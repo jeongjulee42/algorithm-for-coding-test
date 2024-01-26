a2a6e7dfdffdc7ce35316cf6ddeecf6e9753ca1a
jeongjulee42 <ljj3342@naver.com>
2024-01-26 19:32:41 +0900

    cin more

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 string a;
+string b;
 int main() {
-    cin >> a;
+    cin >> a >> b;
     cout << a << '\n';
+    cout << b << '\n';
     return 0;
 }
