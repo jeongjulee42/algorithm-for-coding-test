d6746b93a03589fbe7e0d9f1868613ae0ff65502
jeongjulee42 <ljj3342@naver.com>
2024-02-05 16:57:07 +0900

    &

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,24 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<int, int> pi;
-tuple<int, int, int> tl;
-int a,b,c;
 
 
 
-
-
-
-int main(){
-    pi = {1,2};
-    tl = make_tuple(1,2,3);
-    tie(a, b) = pi;
-    cout << a << " : " << b;
-
-
-
-
+int a[3] = {1, 2, 3}; 
+int main(){ 
+    int * c = a; 
+    cout << c << "\n"; 
+    cout << &a[0] << "\n"; 
+    cout << *a[0] << "\n"; 
+    cout << c + 1 << "\n"; 
+    cout << &a[1] << "\n";
 
 
 
