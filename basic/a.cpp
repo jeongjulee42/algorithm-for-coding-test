3231f5d0c1491b60663f567bb5b652c0cbe5445d
jeongjulee42 <ljj3342@naver.com>
2024-02-09 00:07:10 +0900

    split

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<string> split (const string& input, string delimiter){
+    vector<string> result;
+    auto start = 0;
+    auto end = input.find(delimiter);
+    while(end != string::npos){
+        result.push_back(input.substr(start, end - start));
+        start = end + delimiter.size();
+        end = input.find(delimiter, start);
+    }
+    result.push_back(input.substr(start));
+    return result;
+}
 
 
-
-
-int a[3] = {1, 2, 3}; 
 int main(){ 
-    int * c = a; 
-    cout << c << "\n"; 
-    cout << &a[0] << "\n"; 
-    cout << *a << "\n"; 
-    cout << c + 1 << "\n"; 
-    cout << &a[1] << "\n";
-
-
-
+    string s = "안녕하세요 큰돌이는 킹갓제너럴 천재입니다 정말이에요!", d = " ";
+    vector<string> a = split(s, d);
+    for(string b : a) cout << b << '\n';
 
     return 0;
 }
