7891a893477b20b614a95b445dd5833cb8aa4efb
jeongjulee42 <ljj3342@naver.com>
2024-02-18 19:16:37 +0900

    rotate

--- basic/a.cpp
+++ basic/a.cpp
@@ -4,8 +4,8 @@ using namespace std;
 vector<int> v;
 
 int main(){ 
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-    int sum = accumulate(v.begin(), v.end(), 10); 
-    cout << sum << '\n'; // 55
+    vector<int> v = {1, 2, 3, 4, 5, 6}; 
+    rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5); 
+    for(int i : v) cout << i << ' ';
     return 0;
 }
