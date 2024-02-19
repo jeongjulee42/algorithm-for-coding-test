e221d1de1c2eb03a9e3c05f311588175590718b5
jeongjulee42 <ljj3342@naver.com>
2024-02-19 20:54:13 +0900

    timer

--- basic/a.cpp
+++ basic/a.cpp
@@ -5,7 +5,7 @@ vector<int> v;
 
 int main(){ 
     vector<int> v = {1, 2, 3, 4, 5, 6}; 
-    rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5); 
+    rotate(v.rbegin(), v.rbegin() + 1, v.rend()); 
     for(int i : v) cout << i << ' ';
     return 0;
 }
