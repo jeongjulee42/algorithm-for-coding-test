ab13b2c3ef2d6c0d0310a9b9ddff7a64057e9224
jeongjulee42 <ljj3342@naver.com>
2024-02-26 16:34:48 +0900

    check

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int max_n = 40;
-bool che[max_n + 1];
-
-vector<int> era(int mx_n){
-    vector<int> v;
-    for(int i = 2; i <= mx_n; i++){
-        if(che[i]) continue;
-        for(int j = 2*i; j <mx_n; j += i){
-            che[j] = 1;
-        }
+bool check(int n){
+    if(n <= 1) return 0;
+    if(n == 2) return 1;
+    if(n % 2 == 0) return 0;
+    for(int i = 3; i * i <= n; i++){
+        if(n % i == 0) return 0;
     }
-    for(int i = 2; i <= mx_n; i++) if(che[i] == 0)v.push_back(i);
-    return v;
-    
+    return 1;
 }
 
+
 int main(){ 
-    vector<int> a = era(max_n);
-    for(int i : a)cout << i << " ";
+    for( int i = 1; i <= 20; i++){
+        if(check(i)){
+            cout << i << "는 소수입니다.\n";
+        }
+    }
     return 0;
 }
