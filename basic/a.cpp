6efadae9faed60bdb2baca62dd94933a51af59fb
jeongjulee42 <ljj3342@naver.com>
2024-02-26 17:44:12 +0900

    대칭

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,50 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int n = 3;
-const int m = 4;
-
-void rotate_left_90(vector<vector<int>> & key){
-    int n = key.size();
-    int m = key[0].size();
-    vector<vector<int>> temp(m, vector<int>(n, 0));
-
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            temp[i][j] = key[j][m - i - 1];
-        }
-    }
-    key.resize(m);
-    key[0].resize(n);
-
-    key = temp;
-    return;
-}
-void rotate_right_90(vector<vector<int>> &key){
-    int n = key.size();
-    int m = key[0].size();
-    vector<vector<int>> temp(m, vector<int>(n, 0));
-
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            temp[i][j] = key[n - j - 1][i];
-        }
-    }
-    key.resize(m);
-    key[0].resize(n);
-    key = temp;
-    return;
-}
+vector<vector<int>> v = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+int b[3][3];
 
 
 int main(){ 
-    vector<vector<int>> a = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, }; 
-    rotate_right_90(a); 
-    for(int i = 0; i < m; i++){ 
-        for(int j = 0; j < n; j++){ 
-            cout << a[i][j] << " ";
-        } 
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            b[j][i] = v[i][j];
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << b[i][j] << " ";
+        }
         cout << '\n';
-    } 
+    }
     return 0;
 }
