dc2c2769badbb01332e2b20bf9579c669723bd4f
jeongjulee42 <ljj3342@naver.com>
2024-03-06 16:42:27 +0900

    seven

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,22 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-int a[100004], b, c, psum[100004], n, m;
+int a[9];
+pair<int, int> b;
 
 int main(){
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	cin >> n >> m;
-	for(int i = 1; i <= n; i++){
+	int sum = 0;
+	for(int i = 0; i < 9; i++ ){
 		cin >> a[i];
-		psum[i] = psum[i-1] + a[i];
+		sum += a[i];
 	}
-	for(int i = 0; i < m; i++){
-		cin >> b >> c;
-		cout << psum[c] - psum[b - 1] << '\n';
+	for(int i = 0; i < 9; i++){
+		for(int j = i + 1; j < 9; j++){
+			if(sum - a[i] - a[j] == 100){
+				b.first = a[i];
+				b.second = a[j];
+			}
+		}
 	}
+	sort(a, a+9);
+	for(int i : a){
+		if(i == b.first || i == b.second) continue;
+		cout << i << '\n';
+	}
+
 	return 0;
 }
