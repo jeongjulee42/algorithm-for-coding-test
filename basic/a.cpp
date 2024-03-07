718b71453b34564558b43ac3dce5121dfc831e97
jeongjulee42 <ljj3342@naver.com>
2024-03-07 15:58:01 +0900

    9996

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-string s;
+int num;
+string file, f_s, f_e, inpt;
 
 int main(){
-	
-	getline(cin, s);
-	for(int i = 0; i < s.size(); i++){
-		if(s[i] >= 65 && s[i] <=90){
-			if(s[i] + 13 > 90) s[i] -= 13;
-			else s[i] += 13;
-		} else if (s[i] >= 97 && s[i] <= 122){
-			if(s[i] + 13 > 122) s[i] -= 13;
-			else s[i] += 13;
-		}
-	}
-	cout << s << '\n';
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	return 0;
+    cin >> num;
+    cin >> file;
+    f_s = file.substr(0, file.find('*'));
+    f_e = file.substr(file.find('*') + 1);
+    
+    vector<string> res;
+    for(int i = 0; i < num; i++){
+        cin >> inpt;
+        if(inpt.size() < f_s.size() + f_e.size()) res.push_back("NE");
+        else if(inpt.substr(0, f_s.size()) != f_s) res.push_back("NE");
+        else if (inpt.substr(inpt.size() - f_e.size()) != f_e) res.push_back("NE");
+        else res.push_back("DA");
+    }
+    for(string s : res) cout << s << '\n';
+    return 0;
 }
