36ac821b296d83396fc8611f61924b80eeacce43
jeongjulee42 <ljj3342@naver.com>
2024-03-09 00:39:44 +0900

    1940

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,43 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int tot, cnt, i, sum;
-vector<int> v;
-vector<int> res;
-map<string, int> mp;
-string name, cloth;
+int num, tgt, temp, res;
+map<int, int> mp;
 
-void combi(int start, vector<int> b){
-    if(b.size() == i){
-        int x = 1;
-        for(int a : b) x *= a;
-        sum += x;
-        return;
-    }
-    for(int j = start + 1; j < v.size(); j++){
-        b.push_back(v[j]);
-        combi(j, b);
-        b.pop_back();
-    }
-}
-
-int main() {
+int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-
-    cin >> tot;
-    for(int j = 0; j < tot; j++){
-        cin >> cnt;
-        mp.clear(); v.clear(); sum = 0;
-        for(int k = 0; k < cnt; k++){
-            cin >> name >> cloth;
-            if(mp.find(cloth) == mp.end()) mp.insert({cloth, 1});
-            else mp[cloth]++;
-        }
-        vector<int> r;
-        for(auto m : mp) v.push_back(m.second);
-        for(i = 1; i <= mp.size(); i++) combi(-1, r);
-        res.push_back(sum);
+    cin >> num;
+    cin >> tgt;
+    for(int i = 0; i < num; i++){
+        cin >> temp;
+        if(mp.find(temp) == mp.end()) mp.insert({temp, 1});
+        else mp[temp]++;
     }
-    for(int a : res) cout << a << '\n';
+    for(auto m : mp){
+        if(mp.find(tgt - m.first) == mp.end()) continue;
+        else {
+            if (m.first == tgt - m.first) res += (m.second / 2);
+            else res += min(m.second, mp[tgt - m.first]);
+            mp[m.first] = 0;
+        }
+    }
+
+    cout << res << '\n';
     return 0;
 }
