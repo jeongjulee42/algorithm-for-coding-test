9ec56bfd2bc8d4fa081c86a3694ff712b4692557
jeongjulee42 <ljj3342@naver.com>
2024-03-09 14:20:12 +0900

    1213

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,23 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int num, res;
-string str;
-stack<char> stk;
+string name, res, temp;
+char pos;
+map<char, int> mp;
+int cnt = 0;
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    cin >> num;
-    for(int i = 0; i < num; i ++){
-        cin >> str;
-        for(char s : str){
-            if(stk.size() == 0 || stk.top() != s) stk.push(s);
-            else stk.pop();
-        }
-        if(stk.size() == 0) res++;
-        while(stk.size() != 0) stk.pop();
+    cin >> name;
+    for(char s : name){
+        if(mp.find(s) == mp.end()) mp.insert({s, 1});
+        else mp[s]++;
     }
+    for(auto m : mp){
+        if(m.second % 2 == 1){
+            cnt ++;
+            if(cnt > 1){
+                cout << "I'm Sorry Hansoo" << '\n';
+                exit(0);
+            }
+            pos = m.first;
+            m.second --; 
+        }
+        for(int i = 0; i < m.second / 2; i++){
+            if(m.second == 0) continue;
+            res += m.first;
+        }
+    }
+    temp = res;
+    reverse(res.begin(), res.end());
+    if(cnt == 1) res = temp + pos + res;
+    else res = temp + res;
     cout << res << '\n';
-
     return 0;
 }
