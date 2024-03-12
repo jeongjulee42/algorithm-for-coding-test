9a07a64b47e46053822138caa5cd75020ec7e1de
jeongjulee42 <ljj3342@naver.com>
2024-03-12 17:36:01 +0900

    연결행렬

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int V = 4;
+const int V = 10;
 vector<int> adj[V];
+int visited[V];
 
+void go(int idx){
+    cout << idx << '\n';
+    visited[idx] = 1;
+    for(int there : adj[idx]){
+        if(visited[there]) continue;
+        go(there);
+    } 
+    return;
+}
 
 
 int main(){
-    adj[0].push_back(1);
-    adj[0].push_back(2);
-    adj[0].push_back(3);
-
-    adj[1].push_back(0);
     adj[1].push_back(2);
-
-    adj[2].push_back(0);
     adj[2].push_back(1);
 
-    adj[3].push_back(0); 
+    adj[1].push_back(3); 
+    adj[3].push_back(1);
+
+    adj[3].push_back(4); 
+    adj[4].push_back(3); 
+
 
     for(int i = 0; i < V; i++){
-        cout << i << "::";
-        for(int there : adj[i]) cout << there << ' ';
-        cout << '\n';
+        if(adj[i].size() && visited[i] == 0) go(i);
     }
 
 
