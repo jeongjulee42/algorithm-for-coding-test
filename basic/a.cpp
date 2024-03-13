d946545492e38e2ffe347789d6bd74974b568a5d
jeongjulee42 <ljj3342@naver.com>
2024-03-13 10:23:12 +0900

    DFS기본코드

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
+const int n = 6; 
+vector<int> adj[n];
+int visited[n];
 
-const int n = 3;
-int a[n][n], visited[n][n];
-
-void go(int y, int x){
-    visited[y][x] = 1;
-    cout << y << " : " << x << '\n';
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny >= n || nx >= n || ny < 0 || nx < 0) continue;
-        if(a[ny][nx] == 0) continue;
-        if(visited[ny][nx]) continue;
-        go(ny, nx); 
+void dfs(int u){
+    visited[u] = 1;
+    cout << u << '\n';
+    for(int v : adj[u]){
+        if(visited[v] == 0) dfs(v);
     }
     return;
 }
 
 int main(){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j];
-        }
-    }
-    go(0,0);
-
+    adj[1].push_back(2);
+    adj[1].push_back(3); 
+    adj[2].push_back(4);  
+    adj[4].push_back(2);  
+    adj[2].push_back(5);   
+    dfs(1); 
 
 
     return 0;
