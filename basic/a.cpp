7c0894ca08d6a01b66b0801419df36f75e09719d
jeongjulee42 <ljj3342@naver.com>
2024-03-14 16:42:32 +0900

    1012

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,44 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int n, m;
+int tot, n, m, ny, nx, y, x, num, ret;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int a[100][100];
-int visited[100][100];
-int y, x, sy, sx, ey, ex;
+int a[50][50];
+bool visited[50][50];
+
+void dfs(int y, int x){
+    visited[y][x] = 1;
+    for(int i = 0; i < 4; i++){
+        ny = dy[i] + y;
+        nx = dx[i] + x;
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+        if(visited[ny][nx]) continue;
+        if(a[ny][nx] == 0) continue;
+        dfs(ny, nx);
+    }
+    return;
+}
 
 int main(){
-    cin >> n >> m;
-    cin >> sy >> sx;
-    cin >> ey >> ex;
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+    cin >> tot;
+    for(int i = 0; i < tot; i++){
+        memset(a, 0, sizeof(a));
+        memset(visited, 0, sizeof(visited));
+        ret = 0;
+        cin >> m >> n >> num;
+        for(int j = 0; j < num; j++){
+            cin >> x >> y;
+            a[y][x] = 1;
         }
-    }
-
-    queue<pair<int, int>> q;
-    visited[sy][sx] = 1;
-    q.push({sy, sx});
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny >= n | nx >= m | ny < 0 | nx < 0) continue;
-            if(!a[ny][nx]) continue;
-            if(visited[ny][nx]) continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
+        for(int j = 0; j < 50; j++){
+            for(int k = 0; k < 50; k++){
+                if(visited[j][k] == 0 && a[j][k]) {
+                    dfs(j, k);
+                    ret++;
+                }
+            }
         }
+        cout << ret << '\n';
     }
-    
-
-    printf("%d\n", visited[ey][ex]); 
-
-
 
 
 
