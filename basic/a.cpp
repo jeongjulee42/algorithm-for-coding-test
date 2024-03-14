44cf7ebc8d9d93d81d92c320b059b21e2e37e75a
jeongjulee42 <ljj3342@naver.com>
2024-03-14 16:42:51 +0900

    2178

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,48 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int tot, n, m, ny, nx, y, x, num, ret;
+
+int a[100][100];
+int visited[100][100];
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int a[50][50];
-bool visited[50][50];
+int ny, nx, y, x;
+int n, m;
 
-void dfs(int y, int x){
-    visited[y][x] = 1;
-    for(int i = 0; i < 4; i++){
-        ny = dy[i] + y;
-        nx = dx[i] + x;
-        if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-        if(visited[ny][nx]) continue;
-        if(a[ny][nx] == 0) continue;
-        dfs(ny, nx);
-    }
-    return;
-}
 
 int main(){
-    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    cin >> tot;
-    for(int i = 0; i < tot; i++){
-        memset(a, 0, sizeof(a));
-        memset(visited, 0, sizeof(visited));
-        ret = 0;
-        cin >> m >> n >> num;
-        for(int j = 0; j < num; j++){
-            cin >> x >> y;
-            a[y][x] = 1;
+
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%1d", &a[i][j]);
         }
-        for(int j = 0; j < 50; j++){
-            for(int k = 0; k < 50; k++){
-                if(visited[j][k] == 0 && a[j][k]) {
-                    dfs(j, k);
-                    ret++;
-                }
-            }
-        }
-        cout << ret << '\n';
     }
 
+    queue<pair<int, int>> q;
+    visited[0][0] = 1;
+    q.push({0, 0});
+    while(q.size()){
+        tie(y,x) = q.front(); q.pop();
+        for(int i = 0; i < 4; i++){
+            ny = y + dy[i];
+            nx = x + dx[i];
+            if(ny < 0 | nx < 0 | ny >= n | nx >= m) continue;
+            if(visited[ny][nx]) continue;
+            if(a[ny][nx] == 0) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
+        }
+    }
 
+    cout << visited[n-1][m-1] << '\n';
 
     return 0;
 }
