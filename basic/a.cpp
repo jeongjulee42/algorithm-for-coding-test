76e8fb245eaa29ca49ee17e67dfedd5fa1525c85
jeongjulee42 <ljj3342@naver.com>
2024-03-14 18:03:08 +0900

    2583

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, ny, nx, ret = 1;
+int m, n, k, ny, nx, res, ret;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 int a[100][100];
-int original[100][100];
-bool visited[100][100];
-set<int> st;
+int visited[100][100];
+vector<int> v;
 
 void dfs(int y, int x){
     visited[y][x] = 1;
+    ret ++;
     for(int i = 0; i < 4; i++){
         ny = y + dy[i];
         nx = x + dx[i];
-        if(ny >= n || nx >= n || nx < 0 || ny < 0)continue;
+        if(ny >= n || nx >= m || ny < 0 || nx < 0) continue;
         if(visited[ny][nx]) continue;
-        if(a[ny][nx] > 0){
+        if(a[ny][nx]){
             dfs(ny, nx);
         }
     }
@@ -25,34 +25,31 @@ void dfs(int y, int x){
 
 int main(){
 
-    cin >> n;
-    st.insert(0);
+    cin >> n >> m >> k;
+    fill(&a[0][0], &a[0][0] + 100 * 100, 1);
+    for(int i = 0; i < k; i++){
+        int sy, sx, ey, ex;
+        cin >> sx >> sy >> ex >> ey;
+        for(int j = sy; j < ey; j++){
+            for(int k = sx; k < ex; k++){
+                a[j][k] = 0;
+            }
+        }
+    }
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j];
-            st.insert(a[i][j]);
-            original[i][j] = a[i][j];
-        }
-    }
-    for(int i : st){
-        int res = 0;
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < n; k++){
-                a[j][k] = original[j][k] - i;
+        for(int j = 0; j < m; j++){
+            if(a[i][j] == 1 & visited[i][j] == 0){
+                ret = 0;
+                dfs(i, j);
+                res ++;
+                v.push_back(ret);
             }
         }
-        memset(visited, 0, sizeof(visited));
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < n; k++){
-                if(visited[j][k] == 0 && a[j][k] > 0){
-                    dfs(j, k);
-                    res ++;
-                }
-            }
-        }
-        ret = max(ret, res);
     }
-    cout << ret << '\n';
+    cout << res << '\n';
+    sort(v.begin(), v.end());
+    for(int i : v) cout << i << " ";
+    cout << '\n';
 
     return 0;
 }
