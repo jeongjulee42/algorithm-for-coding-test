6a128cdfb772921e592b89e83ae945ae130fc819
jeongjulee42 <ljj3342@naver.com>
2024-03-15 20:43:21 +0900

    2910

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,28 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, num, start = 1, finish;
-int cnt;
-int tgt;
 
+struct Str{
+    int number;
+    int order;
+    int size;
+    Str(int number, int order, int size) : number(number), order(order), size(size){}
+    Str(){number = 0; order = 0; size = 0;}
+    bool operator < (const Str & a) const {
+        if(size == a.size) return order < a.order;
+        return size > a.size;
+    }
+};
 
+int n, c, temp;
+vector<Str> v;
+set<int> s;
 
 int main(){
-    cin >> n >> m;
-    cin >> num;
-    for(int i = 0; i < num; i++){
-        cin >> tgt;
-        finish = start + m - 1;
-        while(start > tgt || finish < tgt){
-            if(start > tgt){
-                start --;
-                finish --;
-            }else if(finish < tgt){
-                start ++;
-                finish ++;
+    cin >> n >> c;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        int t = s.size();
+        s.insert(temp);
+        if(t == s.size()){
+            for(int j = 0; j < v.size(); j++){
+                if(v[j].number == temp) v[j].size ++ ;
             }
-            cnt++;
+        }else v.push_back({temp, i, 1});
+    }
+    sort(v.begin(), v.end());
+    for(Str b : v){
+        for(int i = 0; i < b.size; i++){
+            cout << b.number << " ";
         }
     }
-    cout << cnt << '\n';
+
     return 0;
 }
