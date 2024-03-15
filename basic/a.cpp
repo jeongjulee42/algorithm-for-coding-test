436d00877f20040340130412fd609dd4588351ce
jeongjulee42 <ljj3342@naver.com>
2024-03-15 16:56:30 +0900

    2828

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,55 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, m, num, start = 1, finish;
+int cnt;
+int tgt;
 
-int m, n, k, ny, nx, res, ret;
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-int a[100][100];
-int visited[100][100];
-vector<int> v;
 
-void dfs(int y, int x){
-    visited[y][x] = 1;
-    ret ++;
-    for(int i = 0; i < 4; i++){
-        ny = y + dy[i];
-        nx = x + dx[i];
-        if(ny >= n || nx >= m || ny < 0 || nx < 0) continue;
-        if(visited[ny][nx]) continue;
-        if(a[ny][nx]){
-            dfs(ny, nx);
-        }
-    }
-    return;
-}
 
 int main(){
-
-    cin >> n >> m >> k;
-    fill(&a[0][0], &a[0][0] + 100 * 100, 1);
-    for(int i = 0; i < k; i++){
-        int sy, sx, ey, ex;
-        cin >> sx >> sy >> ex >> ey;
-        for(int j = sy; j < ey; j++){
-            for(int k = sx; k < ex; k++){
-                a[j][k] = 0;
+    cin >> n >> m;
+    cin >> num;
+    for(int i = 0; i < num; i++){
+        cin >> tgt;
+        finish = start + m - 1;
+        while(start > tgt || finish < tgt){
+            if(start > tgt){
+                start --;
+                finish --;
+            }else if(finish < tgt){
+                start ++;
+                finish ++;
             }
+            cnt++;
         }
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] == 1 & visited[i][j] == 0){
-                ret = 0;
-                dfs(i, j);
-                res ++;
-                v.push_back(ret);
-            }
-        }
-    }
-    cout << res << '\n';
-    sort(v.begin(), v.end());
-    for(int i : v) cout << i << " ";
-    cout << '\n';
-
+    cout << cnt << '\n';
     return 0;
 }
