fc1b20202900fd133fba2a7c56fac63793370332
jeongjulee42 <ljj3342@naver.com>
2024-03-18 17:06:09 +0900

    2870

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,40 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-string a[64][64];
-int n, ny, nx;
-string ret, temp;
-int dy[] = {0, 0, 1, 1};
-int dx[] = {0, 1, 0, 1};
+int n;
+string inpt;
+int flag, zflag;
+string num;
+vector<string> ret;
 
-string four(int num, int y, int x){  
-    if(num == 1){
-        return a[y][x];
-    }
-    else{
-        string s = "";
-        for(int i = 0; i < 4; i++){
-            ny = y + dy[i] * (num / 2);
-            nx = x + dx[i] * (num / 2);
-            s += four(num / 2, ny, nx);
-        }
-        if(s == "0000") return "0";
-        else if(s == "1111") return "1";
-        else return "(" + s + ")";
-    }
+bool cmp(string& a, string& b){
+    if(a.size() != b.size()) return a.size() < b.size();
+    else return a < b;
 }
 
 int main(){
+    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+
     cin >> n;
     for(int i = 0; i < n; i++){
-        cin >> temp;
-        for(int j = 0; j < n; j++){
-            a[i][j] = temp[j];
+        cin >> inpt;
+        flag = 0; num = ""; zflag = 0;
+        for(int j = 0; j < inpt.size(); j++){
+            if(inpt[j] - '0' < 10){
+                flag = 1;
+                if(inpt[j] == '0' && zflag == 0) num = "";
+                else {
+                    num += inpt[j];
+                    zflag = 1;
+                }
+            } else {
+                if(flag == 1) ret.push_back(num);
+                flag = 0; num = ""; zflag = 0;
+            }
         }
+        if(flag == 1) ret.push_back(num);
     }
+    for(int i = 0; i < ret.size(); i++) if(ret[i] == "") ret[i] = "0";
+    sort(ret.begin(), ret.end(), cmp);
+    for(string i : ret) cout << i << '\n';
     
-    ret = four(n, 0, 0);
-    cout << ret << '\n';
-    
+
     return 0;
 }
