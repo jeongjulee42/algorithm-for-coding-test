d10eece74d3882d81453a548535bebeba1b4c2f6
jeongjulee42 <ljj3342@naver.com>
2024-03-18 16:10:11 +0900

    1992

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,25 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int t;
-int cnt;
-int n, temp;
+string a[64][64];
+int n, ny, nx;
+string ret, temp;
+int dy[] = {0, 0, 1, 1};
+int dx[] = {0, 1, 0, 1};
 
-void go(int num){
-    for(int i = 5; i <= num; i *= 5){
-        cnt += num / i;
+string four(int num, int y, int x){  
+    if(num == 1){
+        return a[y][x];
+    }
+    else{
+        string s = "";
+        for(int i = 0; i < 4; i++){
+            ny = y + dy[i] * (num / 2);
+            nx = x + dx[i] * (num / 2);
+            s += four(num / 2, ny, nx);
+        }
+        if(s == "0000") return "0";
+        else if(s == "1111") return "1";
+        else return "(" + s + ")";
     }
 }
 
 int main(){
-    cin >> t;
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    for(int i = 0; i < t; i++){
-        cin >> n;
-        cnt = 0;
-        go(n);
-        cout << cnt << '\n';
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        for(int j = 0; j < n; j++){
+            a[i][j] = temp[j];
+        }
     }
+    
+    ret = four(n, 0, 0);
+    cout << ret << '\n';
+    
     return 0;
 }
