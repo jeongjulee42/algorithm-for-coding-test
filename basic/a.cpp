a3b33b6d38706f1d18d36a14ced0a992534f5106
jeongjulee42 <ljj3342@naver.com>
2024-03-22 15:55:48 +0900

    16234

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,62 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, r, l, ny, nx, ret, flag, sum;
 int ary[50][50];
 int visited[50][50];
-int n, m, cnt, ny, nx;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-vector<int> ret;
+vector<pair<int, int>> temp;
 
-void bfs(int y, int x){
-    queue<pair<int, int>> q;
+void dfs(int y, int x){
+    temp.push_back({y, x});
+    sum += ary[y][x];
     visited[y][x] = 1;
-    q.push({y, x});
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            ny = y + dy[i];
-            nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if(visited[ny][nx]) continue;
-            if(ary[ny][nx] == 0) continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
-        }
-    }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cnt = max(cnt, visited[i][j]);
-        }
+    for(int i = 0; i < 4; i++){
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny >= n || nx >= n || ny < 0 || nx < 0) continue;
+        if(visited[ny][nx]) continue;
+        if(abs(ary[y][x] - ary[ny][nx]) < r || abs(ary[y][x] - ary[ny][nx]) > l) continue;
+        dfs(ny, nx);
     }
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m;
+    cin >> n >> r >> l;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            char c = ' ';
-            cin >> c;
-            if(c == 'W') ary[i][j] = 0;
-            else if(c == 'L') ary[i][j] = 1;
+        for(int j = 0; j < n; j++){
+            cin >> ary[i][j];
         }
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            memset(visited, 0, sizeof(visited));
-            cnt = 0;
-            if(ary[i][j] == 0) continue;
-            bfs(i, j);
-            ret.push_back(cnt);
+    ret = 0;
+    while(true){
+        memset(visited, 0, sizeof(visited));
+        flag = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(visited[i][j] == 1) continue;
+                sum = 0;
+                temp.clear();
+                dfs(i, j);
+                int val = sum / temp.size();
+                for(auto t : temp){
+                    ary[t.first][t.second] = val;
+                }
+                flag++;
+            }
         }
+        if(flag == n*n) break;
+        ret ++;
     }
-    int temp = 0;
-    for(int a : ret){
-        temp = max(temp, a);
-    }
-    cout << temp - 1 << '\n';
-    
+    cout << ret << '\n';
     return 0;
 }
