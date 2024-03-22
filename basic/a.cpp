128140b43ee40a95a6c5a58259a7a2ea8c86b1c0
jeongjulee42 <ljj3342@naver.com>
2024-03-22 14:26:51 +0900

    15686

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,18 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, cnt = 1;
-int pre, suf;
 
+int n, m;
+int ary[50][50];
+vector<pair<int,int>> house;
+vector<pair<int,int>> chicken;
+vector<int> ret;
+
+void combi(int start, vector<int> b){
+    if(b.size() == m){
+        //치킨거리계산
+        int total = 0;
+        for(auto h : house){
+            int length = 5000;
+            for(auto c : b){
+                int temp = abs(h.first - chicken[c].first) + abs(h.second - chicken[c].second);
+                length = min(length, temp);
+            }
+            total += length;
+        }
+        //치킨거리계산한것 ret에 넣기
+        ret.push_back(total);
+        return;
+    }
+    for(int i = start + 1; i < chicken.size(); i++){
+        b.push_back(i);
+        combi(i, b);
+        b.pop_back();
+    }
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n;
-    int i = 666;
-    for(;; i++){
-        if(to_string(i).find("666") != string::npos)n--;
-        if(n == 0)break;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> ary[i][j];
+            if(ary[i][j] == 1) house.push_back({i,j});
+            else if (ary[i][j] == 2) chicken.push_back({i, j});
+        }
     }
-    cout << i << '\n';
+    vector<int> temp;
+    combi(-1, temp);
+    int t = 5000;
+    for(int i : ret){
+        t = min(t, i);
+    }
+    cout << t << '\n';
+    
     return 0;
 }
