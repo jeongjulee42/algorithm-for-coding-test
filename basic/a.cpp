51c46d011400f92254f3c450f67b24007ede1f32
jeongjulee42 <ljj3342@naver.com>
2024-03-22 15:00:47 +0900

    2589

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
 int ary[50][50];
-vector<pair<int,int>> house;
-vector<pair<int,int>> chicken;
+int visited[50][50];
+int n, m, cnt, ny, nx;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
 vector<int> ret;
 
-void combi(int start, vector<int> b){
-    if(b.size() == m){
-        //치킨거리계산
-        int total = 0;
-        for(auto h : house){
-            int length = 5000;
-            for(auto c : b){
-                int temp = abs(h.first - chicken[c].first) + abs(h.second - chicken[c].second);
-                length = min(length, temp);
-            }
-            total += length;
+void bfs(int y, int x){
+    queue<pair<int, int>> q;
+    visited[y][x] = 1;
+    q.push({y, x});
+    while(q.size()){
+        tie(y, x) = q.front(); q.pop();
+        for(int i = 0; i < 4; i++){
+            ny = y + dy[i];
+            nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if(visited[ny][nx]) continue;
+            if(ary[ny][nx] == 0) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny, nx});
         }
-        //치킨거리계산한것 ret에 넣기
-        ret.push_back(total);
-        return;
     }
-    for(int i = start + 1; i < chicken.size(); i++){
-        b.push_back(i);
-        combi(i, b);
-        b.pop_back();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cnt = max(cnt, visited[i][j]);
+        }
     }
 }
 
@@ -35,19 +36,27 @@ int main(){
 
     cin >> n >> m;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> ary[i][j];
-            if(ary[i][j] == 1) house.push_back({i,j});
-            else if (ary[i][j] == 2) chicken.push_back({i, j});
+        for(int j = 0; j < m; j++){
+            char c = ' ';
+            cin >> c;
+            if(c == 'W') ary[i][j] = 0;
+            else if(c == 'L') ary[i][j] = 1;
         }
     }
-    vector<int> temp;
-    combi(-1, temp);
-    int t = 5000;
-    for(int i : ret){
-        t = min(t, i);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            memset(visited, 0, sizeof(visited));
+            cnt = 0;
+            if(ary[i][j] == 0) continue;
+            bfs(i, j);
+            ret.push_back(cnt);
+        }
     }
-    cout << t << '\n';
+    int temp = 0;
+    for(int a : ret){
+        temp = max(temp, a);
+    }
+    cout << temp - 1 << '\n';
     
     return 0;
 }
