ba58f6e3306b6b5b54253afdd07bff0c64e30130
jeongjulee42 <ljj3342@naver.com>
2024-03-26 19:20:22 +0900

    9934:

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,72 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-char ary[1000][1000];
-int visited[1000][1000];
-int r, c, sy, sx, y, x;
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-vector<pair<int, int>> fire;
-vector<int> ret;
+int k;
+vector<int> ret[10];
+int ary[1024];
 
 int main(){
-    cin >> r >> c;
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            cin >> ary[i][j];
-            if(ary[i][j] == 'F') fire.push_back({i, j});
-            else if(ary[i][j] == 'J'){
-                sy = i;
-                sx = j;
-            }
-        }
-    }
-    visited[sy][sx] = 1;
-    queue<pair<int, int>> q;
-    q.push({sy, sx});
-    int fireCnt = 0;
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        if(fireCnt != visited[y][x]){
-            fireCnt = visited[y][x];
-            vector<pair<int, int>> temp;
-            for(int i = 0; i < fire.size(); i++){
-                for(int j = 0; j < 4; j++){
-                    int fy = fire[i].first + dy[j];
-                    int fx = fire[i].second + dx[j];
-                    if(fy < 0 || fx < 0 || fy >= r || fx >= c) continue;
-                    if(ary[fy][fx] == '#' || ary[fy][fx] == 'F') continue;
-                    ary[fy][fx] = 'F';
-                    temp.push_back({fy, fx});
-                }
-            }
-            fire.clear();
-            for(int i = 0; i < temp.size(); i++){
-                fire.push_back(temp[i]);
-            }
-        }
 
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= r || nx >= c){
-                ret.push_back(visited[y][x]);
-                continue;
-            }
-            if(visited[ny][nx]) continue;
-            if(ary[ny][nx] == 'F' || ary[ny][nx] == '#') continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
+    cin >> k;
+    for(int i = 0; i < int(pow(2, k) - 1); i++){
+        cin >> ary[i];
+    }
+    for(int i = 0; i < k; i++){
+        for(int j = int(pow(2, i)) - 1; j < int(pow(2, k) - 1); j += int(pow(2, i + 1))){
+            ret[i].push_back(ary[j]);
         }
     }
-    
-    int minVal = 1000004;
-    if(!ret.size()) cout << "IMPOSSIBLE" << '\n';
-    else{
-        for(int v : ret){
-            minVal = min(v, minVal);
+
+    for(int i = k - 1; i >= 0; i--){
+        for(int j = 0; j < ret[i].size(); j++){
+            cout << ret[i][j] << " ";
         }
-        cout << minVal << '\n';
+        cout << '\n';
     }
+
     return 0;
 }
