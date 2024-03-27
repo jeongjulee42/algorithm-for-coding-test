37a4888e4ed00832916a4c25ef66d76d135ddd72
jeongjulee42 <ljj3342@naver.com>
2024-03-27 23:10:16 +0900

    16637

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,64 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m, sy, sx, ey, ex, flag, y, x;
-char ary[300][300];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-vector<pair<int, int>> temp;
-vector<pair<int, int>> zero;
-queue<pair<int, int>> q;
+int n;
+int maxVal = INT_MIN;
+vector<char> cal;
+vector<int> num;
 
-void go(){
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny >= n || nx >= m || ny < 0 || nx < 0) continue;
-            if(ary[ny][nx] == '*') continue;
-            if(ary[ny][nx] == '1') {
-                zero.push_back({ny, nx});
-                temp.push_back({y, x});
-                continue;
-            }
-            if(ny == ey && nx == ex){
-                flag = 1;
-                return;
-            }
-            ary[ny][nx] = '*';
-            temp.push_back({ny, nx});
-            q.push({ny, nx});
+int calculate(int a, int b, char c){
+    if(c == '+') return a + b;
+    else if(c == '-') return a - b;
+    else return a * b;
+}
+
+void combi(int start, vector<int> b, int k){
+    if(b.size() == k){
+        int ret = 0;
+        vector<char> calTemp;
+        for(int i = 0; i < cal.size(); i++){
+            calTemp.push_back(cal[i]);
         }
+        vector<int> numTemp;
+        for(int i = 0; i < num.size(); i++){
+            numTemp.push_back(num[i]);
+        }
+        for(int i = 0; i < b.size(); i++){
+            char cl = calTemp[b[i]];
+            int a1 = numTemp[b[i]];
+            int a2 = numTemp[b[i] + 1];
+            int a3 = calculate(a1, a2, cl);
+            calTemp[b[i]] = '+';
+            numTemp[b[i]] = a3;
+            numTemp[b[i] + 1] = 0;
+        }
+        ret = numTemp[0];
+        for(int i = 0; i < calTemp.size(); i++){
+            ret = calculate(ret, numTemp[i + 1], calTemp[i]);
+        }
+        maxVal = max(maxVal, ret);
     }
-    return;
+    for(int i = start + 1; i < cal.size(); i ++){
+        if(b.size() && b.back() + 1 == i) continue;
+        b.push_back(i);
+        combi(i, b, k);
+        b.pop_back();
+    }
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m;
-    cin >> sy >> sx >> ey >> ex;
-    sy -= 1; sx -= 1; ey -= 1; ex -= 1;
+    cin >> n;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> ary[i][j];
+        if(i % 2 == 0){
+            int temp = 0;
+            cin >> temp;
+            num.push_back(temp);
+        }else{
+            char temp = '0';
+            cin >> temp;
+            cal.push_back(temp);
         }
     }
-    int cnt = 0;
-    q.push({sy, sx});
-    while(!flag){
-        go();
-        for(int i = 0; i < zero.size(); i++){
-            ary[zero[i].first][zero[i].second] = '0';
-        }
-        zero.clear();
-        for(int i = 0; i < temp.size(); i++){
-            q.push({temp[i].first, temp[i].second});
-        }
-        temp.clear();
-        cnt++;
+
+    for(int i = 0; i < int(ceil(double(n) / 2)); i++){
+        vector<int> b;
+        combi(-1, b, i);
     }
-    cout << cnt << '\n';
+    cout << maxVal << '\n';
     return 0;
 }
--- basic/b.cpp
+++ basic/b.cpp
@@ -1,70 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
+// 틀림. 이렇게 뽑을수가 없다.
+int n;
+int maxVal = INT_MIN;
+vector<char> cal;
+vector<int> num;
 
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-char ary[1500][1500];
-bool visited[1500][1500];
-int r, c, sy, sx, ey, ex, y, x, flag;
-vector<pair<int, int>> rmv;
-queue<pair<int, int>> q;
+int calculate(int a, int b, string c){
+    if(c == "+") return a + b;
+    else if(c == "-") return a - b;
+    else return a * b;
+}
 
-void bfs(){
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny >= r || nx >= c || ny < 0 || nx < 0) continue;
-            if(visited[ny][nx]) continue;
-            if(ary[ny][nx] == 'X'){
-                rmv.push_back({ny, nx});
-                visited[ny][nx] = 1;
-                continue;
-            }
-            visited[ny][nx] = 1;
-            q.push({ny, nx});
-        }
+void combi(int start, vector<int> b, int k){
+    if(b.size() == k){
+        // int ret = 0;
+        //ㄱㅖ산
+        //먼저 계산
+
+        //나중 계산
+        // ----
+        // for(int i = 0; i < b.size(); i++){
+            
+        // }
+        // maxVal = max(maxVal, ret);
+        for(int i = 0; i < b.size(); i++) cout << b[i] << " ";
+        cout << '\n';
+    }
+    for(int i = start + 1; i < cal.size(); i ++){
+        b.push_back(i);
+        combi(i, b, k);
+        b.pop_back();
     }
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> r >> c;
-    int inputFlag = 0;
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            cin >> ary[i][j];
-            if(ary[i][j] == 'L' && inputFlag == 0){
-                sy = i; sx = j;
-                inputFlag = 1;
-            }else if(ary[i][j] == 'L'){
-                ey = i; ex = j;
-            }
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0){
+            int temp = 0;
+            cin >> temp;
+            num.push_back(temp);
+        }else{
+            char temp = '0';
+            cin >> temp;
+            cal.push_back(temp);
         }
     }
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            if(visited[i][j]) continue;
-            if(ary[i][j] == 'L' || ary[i][j] == '.'){
-                visited[i][j] = 1;
-                q.push({i, j});
-                bfs();
-            }
-        }
+
+    for(int i = 0; i <= cal.size(); i++){
+        vector<int> b;
+        combi(-1, b, i);
     }
-    int cnt = 1;
-    while(!flag){
-        for(int i = 0; i < rmv.size(); i++){
-            ary[rmv[i].first][rmv[i].second] = '.';
-            q.push({rmv[i].first, rmv[i].second});
-        }
-        rmv.clear();
-        bfs();
-        if(flag) break;
-        cnt++;
-    }
-    cout << cnt << '\n';
+    cout << maxVal << '\n';
     return 0;
 }
+i < int(ceil(double(n) / 2))
