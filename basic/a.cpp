41c74e9108396807ad59ce1e26fbc34bdad1abd2
jeongjulee42 <ljj3342@naver.com>
2024-03-29 19:23:10 +0900

    15684

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define prev aaa
-#define next aaaa
-const int max_n = 200004;
-int visited[max_n], prev[max_n], n, k, ret, here, cnt, next;   
-vector<int> v; 
-queue<int> q;  
-int main() { 
-    cin >> n >> k; 
-    visited[n] = 1; 
-    q.push(n);  
-    while(q.size()){
-        here = q.front();  
-        q.pop();
-        if(here == k){
-            ret = visited[k]; 
-            break;
+const int INF = 987654321;
+int n, m, h, a, b, ret = INF, visited[34][34];
+bool check(){
+    for(int i = 1; i <= n; i++){
+        int start = i;
+        for(int j = 1; j <= h; j++){
+            if(visited[j][start])start++;
+            else if(visited[j][start - 1])start--;
         }
-		for(int next : {here + 1, here - 1, here * 2}){
-            if(next >= max_n || next < 0 || visited[next]) continue;  
-            visited[next] = visited[here] + 1; 
-            prev[next] = here; 
-            q.push(next); 
-		} 
-    }   
-    for(int i = k; i != n; i = prev[i]){
-        v.push_back(i);
+        if(start != i) return false;
     }
-    v.push_back(n);
-    cout << ret - 1<< '\n'; 
-    reverse(v.begin(), v.end());
-	for(int i : v) cout << i << ' '; 
-    return 0;
+    return true;
+}
+void go(int here, int cnt){ 
+    if(cnt > 3 || cnt >= ret) return;
+    if(check()){
+        ret = min(ret, cnt); return;
+    }
+    for(int i = here; i <= h; i++){
+        for(int j = 1; j < n; j++){
+            if(visited[i][j] || visited[i][j - 1] || visited[i][j + 1]) continue;
+            visited[i][j] = 1;
+            go(i, cnt + 1);
+            visited[i][j] = 0;
+        }
+    }
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m >> h;
+    for(int i = 0; i < m; i++){
+        cin >> a >> b; visited[a][b] = 1;
+    }
+    go(1, 0);
+    cout << ((ret == INF) ? -1 : ret) << "\n";
+	return 0;
 }
