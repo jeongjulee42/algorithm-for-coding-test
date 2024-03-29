655916b8c8d9e9ccc2a52fb2d049d6c43e72e427
jeongjulee42 <ljj3342@naver.com>
2024-03-29 22:34:06 +0900

    11382

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long a,b,c;
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     cout.tie(NULL);
-    cout << "|\\_/|" << '\n';
-    cout << "|q p|   /}" << '\n';
-    cout << "( 0 )\"\"\"\\" << '\n';
-    cout << "|\"^\"`    |" << '\n';
-    cout << "||_/=\\\\__|" << '\n';
+    cin >> a >> b >> c;
+    cout << a+b+c << '\n';
 
 	return 0;
 }
