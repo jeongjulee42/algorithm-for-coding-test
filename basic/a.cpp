8547541aa861518f4b83c6b25041cfc2c706292c
jeongjulee42 <ljj3342@naver.com>
2024-03-29 22:32:26 +0900

    10172

--- basic/a.cpp
+++ basic/a.cpp
@@ -5,10 +5,11 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     cout.tie(NULL);
-    cout << "\\    /\\" << '\n';
-    cout << " )  ( ')" << '\n';
-    cout << "(  /  )" << '\n';
-    cout << " \\(__)|" << '\n';
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|" << '\n';
 
 	return 0;
 }
