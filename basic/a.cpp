575f9318d1ea4e8f21cfba496508ecfe2ae7d702
jeongjulee42 <ljj3342@naver.com>
2024-03-29 22:17:22 +0900

    10430

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a = 2541 - 1998;
-int b;
+int a,b,c;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> b;
-    cout << b - a <<'\n';
-    
-
+    cin >> a >> b >> c;
+    cout << (a+b)%c << '\n';
+    cout << ((a%c)+(b%c)) % c << '\n';
+    cout << (a*b)%c << '\n';
+    cout << ((a%c)*(b%c)) % c << '\n';
 	return 0;
 }
