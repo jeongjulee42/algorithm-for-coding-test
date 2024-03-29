a1fe0b3e035d936a335de4f61324207a22cb9f4b
jeongjulee42 <ljj3342@naver.com>
2024-03-29 22:07:14 +0900

    1000

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a, b;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     cout.tie(NULL);
 
-    cout << "Hello World!" << '\n';
+    cin >> a >> b;
+    cout << a + b << '\n';
+
 	return 0;
 }
