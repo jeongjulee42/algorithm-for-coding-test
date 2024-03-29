bcea05eebcdbf6e42934371f2883302e60bff603
jeongjulee42 <ljj3342@naver.com>
2024-03-29 22:15:14 +0900

    18108

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a;
+int a = 2541 - 1998;
+int b;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> a;
-    cout << a + "??!" << '\n';
+    cin >> b;
+    cout << b - a <<'\n';
     
 
 	return 0;
