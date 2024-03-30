b64680ba1f074c8336d54e6ac56c7e57ccc8347b
jeongjulee42 <ljj3342@naver.com>
2024-03-30 10:13:28 +0900

    14681

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a;
+int a, b;
 
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> a;
-	if(a % 400 == 0) cout << 1 << '\n';
-	else if (a % 100 == 0) cout << 0 << '\n';
-	else if (a % 4 == 0) cout << 1 << '\n';
-	else cout << 0 << '\n';
+	cin >> a >> b;
+	if(a > 0 && b > 0) cout << 1 << '\n';
+	else if(a > 0 && b < 0) cout << 4 << '\n';
+	else if(a < 0 && b < 0) cout << 3 << '\n';
+	else cout << 2 << '\n';
 
 	return 0;
 }
