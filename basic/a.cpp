e498bce04ffa0b81d42e6ef2ccd2b85c767690c8
jeongjulee42 <ljj3342@naver.com>
2024-03-30 10:22:43 +0900

    10951

--- basic/a.cpp
+++ basic/a.cpp
@@ -7,11 +7,9 @@ int a, b;
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> a >> b;
-	if(a > 0 && b > 0) cout << 1 << '\n';
-	else if(a > 0 && b < 0) cout << 4 << '\n';
-	else if(a < 0 && b < 0) cout << 3 << '\n';
-	else cout << 2 << '\n';
+	while(cin >> a >> b){
+		cout << a + b << '\n';
+	}
 
 	return 0;
 }
