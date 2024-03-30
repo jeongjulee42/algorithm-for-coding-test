191d687ac6db9e1ec78dc0dee04d68ea27661680
jeongjulee42 <ljj3342@naver.com>
2024-03-30 10:02:08 +0900

    9498

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b;
+int a;
+
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> a >> b;
-	if(a > b) cout << '>' << '\n';
-	else if (a == b) cout << "==" << '\n';
-	else cout << '<' <<'\n';
+	cin >> a;
+	if(a >= 90) cout << 'A' << '\n';
+	else if(a >= 80) cout << 'B' << '\n';
+	else if(a >= 70) cout << 'C' << '\n';
+	else if(a >= 60) cout << 'D' << '\n';
+	else cout << 'F' << '\n';
 
 	return 0;
 }
