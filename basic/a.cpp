3f7aa3e50d6073d32600106b9a3a53f12ae153a5
jeongjulee42 <ljj3342@naver.com>
2024-03-30 10:00:08 +0900

    1330

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b, c, d, e;
+int a, b;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> a >> b;
-	c = b / 100;
-	d = b % 100 / 10;
-	e = b % 10;
-
-	cout << a * c <<'\n';
-	cout << a * d <<'\n';
-	cout << a * e <<'\n';
-	cout << a * b <<'\n';
+	if(a > b) cout << '>' << '\n';
+	else if (a == b) cout << "==" << '\n';
+	else cout << '<' <<'\n';
 
 	return 0;
 }
