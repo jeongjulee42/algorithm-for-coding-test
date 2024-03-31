6140035c0e298132a06fce6f646c5c4f4bdeb994
jeongjulee42 <ljj3342@naver.com>
2024-03-31 21:16:05 +0900

    2884

--- basic/a.cpp
+++ basic/a.cpp
@@ -7,8 +7,8 @@ int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> t >> m;
-	cin >> n;
-	cout << ((t * 60 + m + n) / 60) % 24 << " " << (t * 60 + m + n) % 60 << '\n';
+	n = 45;
+	cout << ((((t + 24) * 60) + m - n) / 60) % 24 << " " << ((t + 24) * 60 + m - n) % 60 << '\n';
 
 	return 0;
 }
