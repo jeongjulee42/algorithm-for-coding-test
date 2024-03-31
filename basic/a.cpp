ae48c491df91351b3a65a3423ccdd0c28f458c84
jeongjulee42 <ljj3342@naver.com>
2024-03-31 20:03:25 +0900

    11022

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,18 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a;
-
+int n, a, b;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> a;
-	for(int i = 1; i <= a; i++){
-		for(int j = 0; j < i; j++){
-			cout << "*";
-		}
-		cout << '\n';
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << '\n';
 	}
 
 	return 0;
