7b0fe4909ab8a84492534f55d776adeff9e7c6ed
jeongjulee42 <ljj3342@naver.com>
2024-03-31 20:22:21 +0900

    25304

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,20 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, p, sum;
+int n, sum;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> p;
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		int t = 0;
-		int r = 0;
-		cin >> t >> r;
-		sum += t * r;
+	for(int i = 1; i <= n; i++){
+		sum += i;
 	}
-	if(sum == p) cout << "Yes" << '\n';
-	else cout << "No" << '\n';
+	cout << sum << '\n';
 
 	return 0;
 }
