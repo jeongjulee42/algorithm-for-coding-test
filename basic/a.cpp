d32c2494dcc4f8a86c60a418638e818348a94d13
jeongjulee42 <ljj3342@naver.com>
2024-03-31 20:13:22 +0900

    25314

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a, b;
+int n;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 1; i <= n; i++){
-		cin >> a >> b;
-		cout << a + b << '\n';
+	for(int i = 0; i < n / 4; i++){
+		cout << "long ";
 	}
+	cout << "int" << '\n';
 
 	return 0;
 }
