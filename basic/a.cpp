18de7ba66a87866a926e2eb2de54f464d2eecd50
jeongjulee42 <ljj3342@naver.com>
2024-03-31 21:10:23 +0900

    2525

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ary[3];
-int a, b, c;
+int m, t, n;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	for(int i = 0; i < 3; i++){
-		cin >> ary[i];
-	}
-	sort(ary, ary + 3);
-	a = ary[0]; b = ary[1]; c = ary[2];
-	if(a == b && b == c) cout << 10000 + a * 1000 << '\n';
-	else if(a == b || a == c) cout << 1000 + a * 100 << '\n';
-	else if(b == c) cout << 1000 + b * 100 << '\n';
-	else cout << c * 100 << '\n';
+	cin >> t >> m;
+	cin >> n;
+	cout << ((t * 60 + m + n) / 60) % 24 << " " << (t * 60 + m + n) % 60 << '\n';
 
 	return 0;
 }
