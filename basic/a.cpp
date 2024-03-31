c5b174ba7fc30b11ca3b1e7ca646e5f890d5596a
jeongjulee42 <ljj3342@naver.com>
2024-03-31 20:24:41 +0900

    2739

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a, b;
+int n;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		cout << a + b << '\n';
+	for(int i = 1; i < 10; i++){
+		cout << n << " * " << i << " = " << n*i << '\n';
 	}
 
 	return 0;
