7c194006cf3141787e0e8dd7093ca73a51f59655
jeongjulee42 <ljj3342@naver.com>
2024-03-31 20:23:35 +0900

    10950

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, sum;
+int n, a, b;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
-	for(int i = 1; i <= n; i++){
-		sum += i;
+	for(int i = 0; i < n; i++){
+		cin >> a >> b;
+		cout << a + b << '\n';
 	}
-	cout << sum << '\n';
 
 	return 0;
 }
