90891dac5c623bf0c9469e84018d5b4cb96b4f6f
jeongjulee42 <ljj3342@naver.com>
2024-04-01 13:28:14 +0900

    10807

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b;
+int ary[100];
+int n, m, cnt;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> a >> b;
-	cout.precision(10);
-	cout << double(a) / b << '\n';
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> ary[i];
+	}
+	cin >> m;
+	for(int i = 0; i < n; i++){
+		if(ary[i] == m) cnt++;
+	}
+	cout << cnt << '\n';
 
 	return 0;
 }
