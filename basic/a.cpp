06fea225251e0fbf03727ffd588be2b51c950dd7
jeongjulee42 <ljj3342@naver.com>
2024-04-01 13:30:18 +0900

    10871

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int ary[100];
-int n, m, cnt;
+int ary[10000];
+int n, m;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
+	cin >> n >> m;
 	for(int i = 0; i < n; i++){
 		cin >> ary[i];
 	}
-	cin >> m;
 	for(int i = 0; i < n; i++){
-		if(ary[i] == m) cnt++;
+		if(ary[i] < m) cout << ary[i] << " ";
 	}
-	cout << cnt << '\n';
+	cout << '\n';
 
 	return 0;
 }
