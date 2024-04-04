950b9dee236ad893c7e21f915aab22b9d8366f3b
jeongjulee42 <ljj3342@naver.com>
2024-04-04 21:14:41 +0900

    11723

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,73 +1,58 @@
 #include <bits/stdc++.h>
 #include <bitset>
 using namespace std;
-const int INF = -987654321;
-int dy[] = {0, -1, 0, 1};
-int dx[] = {-1, 0, 1, 0};
-int n, m, maxVal = INF, maxVal2 = INF;
-vector<vector<pair<int, int>>> v;
-vector<pair<int, int>> t;
-int ary[50][50];
-bool visited[50][50];
 
-void go(int y, int x){
-	visited[y][x] = 1;
-	t.push_back({y, x});
-	for(int i = 0; i < 4; i++){
-		if(!(ary[y][x] & (1 << i))){
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(ny >= n || nx >= m || ny < 0 || nx < 0) continue;
-			if(visited[ny][nx]) continue;
-			go(ny, nx);
-		}
+set<int> s;
+int n, inpt;
+string str;
+
+void add(int x){
+	s.insert(x);
+}
+void remove(int x){
+	if(find(s.begin(), s.end(), x) != s.end()){
+		s.erase(x);
 	}
 }
-
-int check(vector<pair<int, int>> a, vector<pair<int, int>> b){
-	int cnt = 0;
-	for(int i = 0; i < a.size(); i++){
-		for(int j = 0; j < b.size(); j++){
-			if(a[i].first == b[j].first && abs(a[i].second - b[j].second) == 1){
-				cnt = a.size() + b.size();
-				return cnt;
-			}else if(a[i].second == b[j].second && abs(a[i].first - b[j].first) == 1){
-				cnt = a.size() + b.size();
-				return cnt;
-			}
-		}
-	}
-	return 0;
+void check(int x){
+	if(find(s.begin(), s.end(), x) != s.end()){
+		cout << 1 << '\n';
+	}else cout << 0 << '\n';
+}
+void toggle(int x){
+	if(find(s.begin(), s.end(), x) != s.end()){
+		s.erase(x);
+	}else s.insert(x);
+}
+void all(){
+	s = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+}
+void empty(){
+	s = {};
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> m >> n;
+	vector<string> vec;
+	
+	cin >> n;
+	string buffer = "";
+	getline(cin, buffer);
 	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cin >> ary[i][j];
+		getline(cin, str);
+		if(str.find(" ") != string::npos){
+			auto end = str.find(" ");
+			inpt = stoi(str.substr(end + 1));
+			str = str.substr(0, end);
 		}
+		if(str == "add") add(inpt);
+		else if(str == "remove") remove(inpt);
+		else if(str == "check") check(inpt);
+		else if(str == "toggle") toggle(inpt);
+		else if(str == "all") all();
+		else empty();
 	}
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			if(visited[i][j]) continue;
-			t.clear();
-			go(i, j);
-			v.push_back(t);
-		}
-	}
-	for(vector<pair<int, int>> z : v){
-		int temp = z.size();
-		maxVal = max(maxVal, temp);
-	}
-	for(int i = 0; i < v.size(); i++){
-		for(int j = i + 1; j < v.size(); j++){
-			maxVal2 = max(maxVal2, check(v[i], v[j]));
-		}
-	}
-
-	cout << v.size() << '\n' << maxVal << '\n' << maxVal2 <<'\n';
 
 	return 0;
 }
