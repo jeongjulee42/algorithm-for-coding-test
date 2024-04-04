fad57a0e7930aa0d2a0b7158fbc1937eb9334be4
jeongjulee42 <ljj3342@naver.com>
2024-04-04 17:57:32 +0900

    14890

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,51 +2,72 @@
 #include <bitset>
 using namespace std;
 
-int n, k, m, maxVal = -987654321;
-vector<char> start = {'a','n','t','i','c'};
-vector<int> v;
+int n, l, ret;
+int ary[100][100];
+vector<stack<int>> v;
 
-void go(int b, int idx, int cnt){
-	if(cnt == k - 5){
-		int num = 0;
-		for(int temp : v){
-			if((temp & b) == temp) num++;
-		}
-		maxVal = max(maxVal, num);
-		return;
-	}
-	for(int i = idx; i < 26; i ++){
-		if(b & (1 << i)) continue;
-		go(b | (1 << i), i + 1, cnt + 1);
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n >> k;
+	cin >> n >> l;
 	for(int i = 0; i < n; i++){
-		string str = "";
-		cin >> str;
-		str = str.substr(4, str.size() - 8);
-		set<char> s;
-		for(int j = 0; j < str.size(); j++){
-			s.insert(str[j]);	
+		for(int j = 0; j < n; j++){
+			cin >> ary[i][j];
 		}
-		int temp = 0;
-		for(char c : s) temp |= (1 << (c - 'a'));
+	}
+	for(int i = 0; i < n; i++){
+		stack<int> temp;
+		for(int j = 0; j < n; j++){
+			temp.push(ary[i][j]);
+		}
 		v.push_back(temp);
 	}
-	if(k < 5) {
-		cout << 0 << '\n';
-		exit(0);
+	for(int i = 0; i < n; i++){
+		stack<int> temp;
+		for(int j = 0; j < n; j++){
+			temp.push(ary[j][i]);
+		}
+		v.push_back(temp);
 	}
-	for(int i = 0; i < 5; i++){
-		m |= (1 << (start[i] - 'a'));
+	for(int i = 0; i < v.size(); i++){
+		stack<int> stk = v[i];
+		int cnt = 1;
+		int here = stk.top(); stk.pop();
+		while(stk.size()){
+			if(here == stk.top()){
+				cnt++;
+				stk.pop();
+			}else{
+				if(abs(here - stk.top()) > 1) break;
+				else if(here < stk.top()){
+					if(cnt < l) break;
+					else{
+						here = stk.top();
+						stk.pop();
+						cnt = 1;
+					}
+				}else if (here > stk.top()){
+					// stk 비어있는경우 생각.
+					if(l > stk.size()) break;
+					here = stk.top(); stk.pop();
+					int flag = 0;
+					for(int j = 1; j < l; j++){
+						int temp = stk.top();
+						if(here != temp) {
+							flag = 1;
+							break;
+						}
+						stk.pop();
+					}
+					if(flag) break;
+					cnt = 0;
+				}	
+			}
+		}
+		if(!stk.size()) ret++;
 	}
-	go(m, 0, 0);
-	
-	cout << maxVal << '\n';
+	cout << ret << '\n';
 	return 0;
 }
 
