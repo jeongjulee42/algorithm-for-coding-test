aa9ccd30ab44edf05e10ccf62d4c3537facb4039
jeongjulee42 <ljj3342@naver.com>
2024-04-04 13:41:37 +0900

    1062

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,40 +1,51 @@
 #include <bits/stdc++.h>
+#include <bitset>
 using namespace std;
 
-int r, c, a;
-char ary[20][20];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-int cnt, maxVal = -987654321;;
+int n, k, m, maxVal = -987654321;
+vector<char> start = {'a','n','t','i','c'};
+vector<int> v;
 
-void go(int y, int x){
-	cnt++;
-	for(int i = 0; i < 4; i++){
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if(ny >= r || nx >= c || ny < 0 || nx < 0) continue;
-		int temp = ary[ny][nx] - 'A';
-		if((1 << temp) & a) continue;
-		a |= (1 << temp);
-		go(ny, nx);
-		a &= ~(1 << temp);
+void go(int b, int idx, int cnt){
+	if(cnt == k - 5){
+		int num = 0;
+		for(int temp : v){
+			if((temp & b) == temp) num++;
+		}
+		maxVal = max(maxVal, num);
+		return;
 	}
-	maxVal = max(maxVal, cnt);
-	cnt--;
+	for(int i = idx; i < 26; i ++){
+		if(b & (1 << i)) continue;
+		go(b | (1 << i), i + 1, cnt + 1);
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> r >> c;
-	for(int i = 0; i < r; i++){
-		for(int j = 0; j < c; j++){
-			cin >> ary[i][j];
+	cin >> n >> k;
+	for(int i = 0; i < n; i++){
+		string str = "";
+		cin >> str;
+		str = str.substr(4, str.size() - 8);
+		set<char> s;
+		for(int j = 0; j < str.size(); j++){
+			s.insert(str[j]);	
 		}
+		int temp = 0;
+		for(char c : s) temp |= (1 << (c - 'a'));
+		v.push_back(temp);
 	}
-	int temp = ary[0][0] - 'A';
-	a |= (1 << temp);
-	go(0,0);
+	if(k < 5) {
+		cout << 0 << '\n';
+		exit(0);
+	}
+	for(int i = 0; i < 5; i++){
+		m |= (1 << (start[i] - 'a'));
+	}
+	go(m, 0, 0);
+	
 	cout << maxVal << '\n';
 	return 0;
 }
--- basic/b.cpp
+++ basic/b.cpp
@@ -1,66 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, mp, mf, ms, mv, idx;
-int minVal = 987654321;
-struct Food{int p; int f; int s; int v; int c;};
-Food ary[15];
-vector<int> ret;
+int n, k, m;
+vector<char> start = {'a','n','t','i','c'};
+vector<string> v;
 
-bool cmp(vector<int> a, vector<int> b){
-	int minVal = min(a.size(), b.size());
-	int cnt = 0;
-	for(int i = 0; i < minVal; i++){
-		if(a[i] < b[i]) return true;
-		else if(a[i] == b[i]) cnt++;
-		else return false;
+void go(int m, int cnt){
+	
+	if(cnt == k - 5){
+		
+		return;
 	}
-	if(cnt == minVal && a.size() > b.size()) return false;
-	return true;
+
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	cin >> mp >> mf >> ms >> mv;
+	cin >> n >> k;
 	for(int i = 0; i < n; i++){
-		int p, f, s, v, c;
-		cin >> p >> f >> s >> v >> c;
-		ary[i] = {p, f, s, v, c};
-	}
-	for(int i = 0; i < (1 << n); i++){
-		int p = 0, f = 0, s = 0, v = 0, c = 0;
-		vector<int> temp;
-		for(int j = 0; j < n; j++){
-			if(i & (1 << j)){
-				p += ary[j].p; f += ary[j].f; s += ary[j].s; v += ary[j].v; c += ary[j].c;
-				temp.push_back(j + 1);
+		string str = "";
+		cin >> str;
+		str = str.substr(4, str.size() - 8);
+		set<char> s;
+		for(int j = 0; j < str.size(); j++){
+			if(find(start.begin(), start.end(), str[j]) == start.end()){
+				s.insert(str[j]);
 			}
 		}
-		if(p >= mp && f >= mf && s >= ms && v >= mv && minVal >= c){
-			if(minVal > c){
-				minVal = c;
-				ret.clear();
-				for(int z : temp) ret.push_back(z);
-			}
-			else{
-				sort(temp.begin(), temp.end());
-				if(ret.size() == 0) {
-					for(int z : temp) ret.push_back(z);
-				}
-				else if(cmp(temp, ret)){
-					ret.clear();
-					for(int z : temp) ret.push_back(z);
-				}
-			}
-		}
+		str = "";
+		for(char c : s) str += c;
+		v.push_back(str);
 	}
-	if(minVal == 987654321) cout << -1;
-	else {
-		cout << minVal << '\n' ;
-		for(int z : ret) cout << z << " ";
+	if(k < 5) {
+		cout << 0 << '\n';
+		exit(0);
 	}
+	for(int i = 0; i < 5; i++){
+		m |= (1 << (start[i] - 'a'));
+	}
+	
+	
 	return 0;
 }
 
