620a6e85cb4bd875beab9b6fc97506c442099458
jeongjulee42 <ljj3342@naver.com>
2024-04-05 15:07:20 +0900

    15926

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
-#include <bitset>
 using namespace std;
 
-string a, b, ret;
-stack<int> stkA, stkB;
-int c;
+bool ary[200004];
+stack<pair<int, char>> stk;
+int n, maxVal, cnt;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> a >> b;
-	for(int i = 0; i < a.size(); i++) stkA.push(a[i]);
-	for(int i = 0; i < b.size(); i++) stkB.push(b[i]);
-	while(stkA.size() || stkB.size()){
-		int a1 = 0; int b1 = 0;
-		if(stkA.size()) {
-			a1 = stkA.top() - '0'; stkA.pop();
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		char c = '0';
+		cin >> c;
+		if(!stk.size()) stk.push({i, c});
+		else{
+			if((stk.top().second == '(') && (c == ')')){
+				ary[stk.top().first] = 1;
+				ary[i] = 1;
+				stk.pop();
+			}else{
+				stk.push({i, c});
+			}
 		}
-		if(stkB.size()) {
-			b1 = stkB.top() - '0'; stkB.pop();
-		}
-		int temp = a1 + b1 + c;
-		c = temp / 10;
-		temp %= 10;
-		ret = to_string(temp) + ret;
 	}
-	if(c) ret = to_string(c) + ret;
-	cout << ret << '\n';
+	for(int i = 0; i < n; i++){
+		cout << ary[i];
+		if(ary[i] == 0){
+			maxVal = max(cnt, maxVal);
+			cnt = 0;
+		}else cnt++;
+	}
+	cout << '\n';
+	cout << max(cnt,maxVal) << '\n';
 	return 0;
 }
 
