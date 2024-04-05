a7b08d21a5bb0b4642719a3ca359fed72669eae5
jeongjulee42 <ljj3342@naver.com>
2024-04-05 22:01:28 +0900

    5430

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,32 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string str;
+int t, n;
+string p, ary;
+vector<string> v;
+vector<string> ret;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> str;
-	if(str.size() < 2) cout << "NO" << '\n';
-	else{
-		for(int i = 1; i < str.size(); i++){
-			if((str[i - 1] == 'p' && str[i] == 'i') || (str[i - 1] == 'k' && str[i] == 'a')){
-				str[i - 1] = '.'; str[i] = '.';
+	cin >> t;
+	for(int i = 0; i < t; i++){
+		v.clear();
+		cin >> p;
+		cin >> n;
+		cin >> ary;
+		int flip = 1; int front = 0; int back = 0;
+		for(int j = 0; j < p.size(); j++){
+			if(p[j] == 'R') flip *= -1;
+			else {
+				if(flip > 0) front ++;
+				else back ++;
 			}
 		}
-		for(int i = 2; i < str.size(); i++){
-			if(str[i - 2] == 'c' && str[i - 1] == 'h' && str[i] == 'u'){
-				str[i-2] = '.'; str[i-1] = '.'; str[i] = '.';
-			}
+		if(n < front + back){
+			ret.push_back("error");
+			continue;
 		}
-		for(int i = 0; i < str.size(); i++){
-			if(str[i] != '.') {
-				cout << "NO" << '\n';
-				exit(0);
+		string temp = "";
+		for(int j = 1; j < ary.size() - 1; j++){
+			if(ary[j] == ',') {
+				v.push_back(temp);
+				temp = "";
 			}
+			else temp += ary[j];
 		}
-		cout << "YES" << '\n';
+		v.push_back(temp);
+		temp = "";
+		if(flip > 0){
+			temp += '[';
+			for(int j = front; j < v.size() - back; j++){
+				temp += v[j];
+				if(j != v.size() - back - 1) temp += ',';
+			}
+			temp += ']';
+		}else{
+			temp += '[';
+			for(int j = v.size() - back - 1; j >= front; j--){
+				temp += v[j];
+				if(j != front) temp += ',';
+			}
+			temp += ']';
+		}
+		ret.push_back(temp);
 	}
+	for(string z : ret) cout << z << '\n';
 	
 	return 0;
 }
