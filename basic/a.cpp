5347a74d256f5a456de52a2cbc51676689b0566e
jeongjulee42 <ljj3342@naver.com>
2024-04-09 21:56:42 +0900

    1644

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, from, to, ret;
-vector<pair<int, int>> v;
+int n;
+vector<int> v;
+bool che[4000001];
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> from >> to;
-		v.push_back({from, to});
+	if(n == 1) {
+		cout << 0 << '\n';
+		exit(0);
 	}
-	sort(v.begin(), v.end());
-	ret = v[0].first + v[0].second;
-	for(int i = 1; i < n; i++){
-		if(v[i].first < ret){
-			ret += v[i].second;
-		}else{
-			ret = v[i].first + v[i].second;
+	for(int i = 2; i <= n; i++){
+		if(che[i]) continue;
+		for(int j = 2 * i; j <= n; j += i){
+			che[j] = 1;
 		}
 	}
-	cout << ret << '\n';
+	for(int i = 2; i <= n; i++) if(che[i] == 0) v.push_back(i);
+	int r = 0, l = 0;
+	int cnt = 0;
+	int num = (v[r] + v[l]) / 2;
+	while(r < v.size()){
+		if(num < n){
+			r++;
+			num += v[r];
+		}else if (num > n){
+			num -= v[l];
+			l++;
+		}else if (num == n){
+			cnt++;
+			r++;
+			num += v[r];
+		}
+	}
+	cout << cnt << '\n';
 	return 0;
 }
 
