e29ed50cdd3d7172ed5daa8d842ede7f0f2ff838
jeongjulee42 <ljj3342@naver.com>
2024-04-09 15:07:00 +0900

    2170

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-double score;
+pair<int, int> L[1000004];
+int n, from, to, l, r, ret;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	priority_queue<double> pq;
 	for(int i = 0; i < n; i++){
-		cin >> score;
-		if(pq.size() == 7){
-			pq.push(score); pq.pop();
-		}else pq.push(score);
+		cin >> from >> to;
+		L[i] = {from, to};
 	}
-	vector<double> temp;
-	while(pq.size()){
-		temp.push_back(pq.top());
-		pq.pop();
+	sort(L, L + n);
+	l = L[0].first; r = L[0].second;
+	for(int i = 1; i < n; i++){
+		if(r < L[i].first){
+			ret += r - l;
+			l = L[i].first;
+			r = L[i].second;
+		}else if(L[i].first <= r && L[i].second >= r){
+			r = L[i].second;
+		}
 	}
-	sort(temp.begin(), temp.end());
-	cout.precision(3);
-	cout << fixed;
-	for(double a : temp) cout << a << '\n';
+	ret += r - l;
+	cout << ret << '\n';
 
 	return 0;
 }
