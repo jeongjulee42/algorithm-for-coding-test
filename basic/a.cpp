0e0176bf28ab9bd62dd0b1c1641212062cb5753b
jeongjulee42 <ljj3342@naver.com>
2024-04-09 18:15:57 +0900

    9935

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,43 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, d, p, ret;
-priority_queue<int, vector<int>, greater<int>> pq;
-vector<pair<int, int>> v;
+string str, fire;
+string ret;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	if(n == 0){
-		cout << 0 << '\n';
-		exit(0);
-	}
-	for(int i = 0; i < n; i++){
-		cin >> p >> d;
-		v.push_back({d, p});
-	}
-	sort(v.begin(), v.end());
-	int day = v[0].first;
-	pq.push(v[0].second);
-	for(int i = 1; i < n; i++){
-		if(day == v[i].first){
-			if(pq.size() < day) pq.push(v[i].second);
-			else{
-				if(pq.top() < v[i].second){
-					pq.push(v[i].second);
-					pq.pop(); 
-				}
-			}
-		}else {
-			day = v[i].first;
-			pq.push(v[i].second);
+	cin >> str;
+	cin >> fire;
+	for(int i = 0; i < str.size(); i++){
+		ret += str[i];
+		if(ret.size() >= fire.size() && ret.substr(ret.size() - fire.size()) == fire){
+			ret.erase(ret.size() - fire.size());
 		}
 	}
-	while(pq.size()){
-		ret += pq.top(); pq.pop();
-	}
-	cout << ret << '\n';
+	if(ret.size() == 0) cout << "FRULA" << '\n';
+	else cout << ret << '\n';
+	
 	return 0;
 }
 
