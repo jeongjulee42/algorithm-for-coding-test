0013f5a6c2e4becf88ef0479b4c9603f37074ec5
jeongjulee42 <ljj3342@naver.com>
2024-04-13 22:29:03 +0900

    11654

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,17 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-string str;
+char c;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> str;
-		cout << str[0] << str[str.size() - 1] << '\n';
-	}
+	cin >> c;
+	cout << int(c) << '\n';
 
 
 	return 0;
