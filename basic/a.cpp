804de99abc72dc3df3ec5e2a592246f4e9c98a59
jeongjulee42 <ljj3342@naver.com>
2024-04-14 21:24:46 +0900

    10809

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char c;
+string str;
+int ary[26];
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> c;
-	cout << int(c) << '\n';
-
+	cin >> str;
+	memset(ary, -1, sizeof(ary));
+	for(int i = 0; i < str.size(); i++){
+		if(ary[str[i] - 'a'] == -1){
+			ary[str[i] - 'a'] = i;
+		}
+	}
+	for(int i = 0; i < 26; i++) cout << ary[i] << ' ';
 
 	return 0;
 }
