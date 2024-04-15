43da7145956a767131f2a2d18563fd8d65ae019e
jeongjulee42 <ljj3342@naver.com>
2024-04-15 16:31:21 +0900

    13144

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string str;
-int ary[26];
+long long idx, temp, n;
+long long visited[100001];
+long long res;
+
+long long pls(long long num){
+	return (num * num + num) / 2 ;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> str;
-	memset(ary, -1, sizeof(ary));
-	for(int i = 0; i < str.size(); i++){
-		if(ary[str[i] - 'a'] == -1){
-			ary[str[i] - 'a'] = i;
+	cin >> n;
+	memset(visited, -1, sizeof(visited));
+	for(long long i = 0; i < n; i++){
+		cin >> temp;
+		if(visited[temp] != -1){
+			res += pls(i - idx);
+			idx = max(visited[temp] + 1, idx);
+			res -= pls(i - idx);
 		}
+		visited[temp] = i;
 	}
-	for(int i = 0; i < 26; i++) cout << ary[i] << ' ';
-
+	res += pls(n - idx);
+	cout << res << '\n';
 	return 0;
 }
 
