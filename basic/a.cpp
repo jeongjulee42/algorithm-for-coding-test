d2939b7a5c19536852e9b90364dcad9e50b319e1
jeongjulee42 <ljj3342@naver.com>
2024-04-16 19:57:55 +0900

    3190

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,79 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = -987654321;
-int n, maxVal = INF;
 
-void mv(vector<vector<int>> & key){
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			if(key[i][j] == 0) continue;
-			for(int k = j + 1; k < n; k++){
-				if(key[i][k] == 0) continue;
-				else if (key[i][j] != key[i][k]) break;
-				else if (key[i][j] == key[i][k]){
-					key[i][j] = key[i][j] + key[i][k];
-					key[i][k] = 0;
-					break;
-				}
-			}
-		}
-		queue<int> q;
-		for(int j = 0; j < n; j++){
-			if(!key[i][j]) continue;
-			q.push(key[i][j]);
-		}
-		for(int j = 0; j < n; j++){
-			if(q.size()){
-				key[i][j] = q.front(); q.pop();
-			}
-			else key[i][j] = 0;
-		}
-	}
-}
-
-void rotate_right(vector<vector<int>> & key){
-	vector<vector<int>> temp(n, vector<int>(n, 0));
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			temp[i][j] = key[n - j - 1][i];
-		}
-	}
-	key = temp;
-	return;
-}
-
-void go(vector<vector<int>> key, int cnt){
-	mv(key);
-	if(cnt >= 5){
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				maxVal = max(maxVal, key[i][j]);
-			}
-		}
-		return;
-	}
-	for(int i = 0; i < 4; i++){
-		go(key, cnt + 1);
-		rotate_right(key);
-	}
-}
+int n, k, l, x, cnt;
+char c;
+vector<pair<int,int>> snake;
+vector<pair<int, int>> road;
+int ary[100][100];
+int dy[] = {0, 1, 0, -1};
+int dx[] = {1, 0, -1, 0};
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	vector<vector<int>> adj(n, vector<int>(n, 0));
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cin >> adj[i][j];
+	cin >> k;
+	for(int i = 0; i < k; i++){
+		int y = 0; int x = 0;
+		cin >> y >> x;
+		ary[y - 1][x - 1] = 1;
+	}
+	cin >> l;
+	vector<pair<int, char>> mv;
+	int temp = 0;
+	for(int i = 0; i < l; i++){
+		cin >> x >> c;
+		mv.push_back({x - temp, c});
+		temp = x;
+	}
+	int ny = 0; int nx = 0;
+	int idx = 0;
+	for(auto m : mv){
+		int num = m.first; char d = m.second;
+		for(int i = 0; i < num; i++){
+			nx += dx[idx]; 
+			ny += dy[idx];
+			road.push_back({ny,nx});
+		}
+		if(d == 'L'){
+			idx--;
+			if(idx < 0) idx = 3;
+		}else if(d == 'D'){
+			idx++;
+			if(idx > 3) idx = 0;
 		}
 	}
-	for(int i = 0; i < 4; i++){
-		go(adj, 1);
-		rotate_right(adj);
+	for(int i = 0; i < n; i++){
+		nx += dx[idx];
+		ny += dy[idx];
+		road.push_back({ny, nx});
 	}
-	cout << maxVal << '\n';
+	snake.push_back({0, 0});
+	int cnt = 0;
+	for(auto m : road){
+		cnt++;
+		ny = m.first, nx = m.second;
+		if(ny < 0 || nx < 0 || ny >= n || nx >= n) break;
 
+		int flag = 0;
+		for(int i = 0; i < snake.size(); i++){
+			if(snake[i].first == ny && snake[i].second == nx) {flag = 1; break;}
+		}
+		if(flag) break;
+
+		snake.push_back({ny, nx});
+		if(!ary[ny][nx]) snake.erase(snake.begin(), snake.begin() + 1);
+		else ary[ny][nx] = 0;
+	}
+	cout << cnt << '\n';
 	return 0;
 }
 
