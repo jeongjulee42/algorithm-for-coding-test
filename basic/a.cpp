969e26f6cff7f04aef02811d9a53bf7001bcf720
jeongjulee42 <ljj3342@naver.com>
2024-04-16 17:13:16 +0900

    12100

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,40 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = -987654321;
+int n, maxVal = INF;
 
-int ary[20][20];
-int n, minVal = 987654321;
-
-int go(vector<int> l, vector<int> r){
-	int left = 0, right = 0;
+void mv(vector<vector<int>> & key){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			if(find(l.begin(), l.end(), i) != l.end() && find(l.begin(), l.end(), j) != l.end()) left += ary[i][j];
-			else if(find(r.begin(), r.end(), i) != r.end() && find(r.begin(), r.end(), j) != r.end()) right += ary[i][j];
+			if(key[i][j] == 0) continue;
+			for(int k = j + 1; k < n; k++){
+				if(key[i][k] == 0) continue;
+				else if (key[i][j] != key[i][k]) break;
+				else if (key[i][j] == key[i][k]){
+					key[i][j] = key[i][j] + key[i][k];
+					key[i][k] = 0;
+					break;
+				}
+			}
+		}
+		queue<int> q;
+		for(int j = 0; j < n; j++){
+			if(!key[i][j]) continue;
+			q.push(key[i][j]);
+		}
+		for(int j = 0; j < n; j++){
+			if(q.size()){
+				key[i][j] = q.front(); q.pop();
+			}
+			else key[i][j] = 0;
 		}
 	}
-	return abs(left - right);
+}
+
+void rotate_right(vector<vector<int>> & key){
+	vector<vector<int>> temp(n, vector<int>(n, 0));
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			temp[i][j] = key[n - j - 1][i];
+		}
+	}
+	key = temp;
+	return;
+}
+
+void go(vector<vector<int>> key, int cnt){
+	mv(key);
+	if(cnt >= 5){
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				maxVal = max(maxVal, key[i][j]);
+			}
+		}
+		return;
+	}
+	for(int i = 0; i < 4; i++){
+		go(key, cnt + 1);
+		rotate_right(key);
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
+	vector<vector<int>> adj(n, vector<int>(n, 0));
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			cin >> ary[i][j];
+			cin >> adj[i][j];
 		}
 	}
-	for(int i = 0; i < (1 << n); i++){
-		if(__builtin_popcount(i) != n / 2)continue;
-		vector<int> l;
-		vector<int> r;
-		for(int j = 0; j < n; j++){
-			if(i & (1 << j)) l.push_back(j);
-			else r.push_back(j);
-		}
-		minVal = min(minVal, go(l, r));
+	for(int i = 0; i < 4; i++){
+		go(adj, 1);
+		rotate_right(adj);
 	}
-	cout << minVal << '\n';
+	cout << maxVal << '\n';
+
 	return 0;
 }
 
