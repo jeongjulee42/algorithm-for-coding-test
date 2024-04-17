4fc45ed04d63c88e76255f1a2058fbd687e1cb62
jeongjulee42 <ljj3342@naver.com>
2024-04-17 21:24:14 +0900

    15662

--- basic/a.cpp
+++ basic/a.cpp
@@ -12,26 +12,34 @@ int main() {
 	for(int i = 0; i < t; i++){
 		string str = "";
 		cin >> str;
-		for(int j = 0; j < 8; j++) v[i][j] = str[j];
+		for(int j = 0; j < 8; j++) v[i][j] = str[j] - '0';
 	}
 	cin >> k;
 	for(int i = 0; i < k; i++){
 		memset(ary, 0, sizeof(ary));
 		cin >> num >> dir;
-		//num - 1 (현재) 부터 0까지 확인
 		ary[num - 1] = dir;
 		for(int j = num - 1; j > 0; j--){
 			if(v[j][6] != v[j - 1][2]) ary[j - 1] = -1 * ary[j];
 			else if (v[j][6] == v[j - 1][2]) break;
 		}
-		for(int j = num - 1; j < k - 1; j++){
+		for(int j = num - 1; j < t - 1; j++){
 			if(v[j][2] != v[j + 1][6]) ary[j + 1] = -1 * ary[j];
 			else if (v[j][2] == v[j + 1][6]) break;
 		}
-		for(int j = 0; j < k; j++) cout << ary[j] << " ";
-		cout << '\n';
+		for(int j = 0; j < t; j++){
+			if(ary[j] == -1){
+				rotate(v[j].begin(), v[j].begin() + 1, v[j].end());
+			}else if(ary[j] == 1){
+				rotate(v[j].rbegin(), v[j].rbegin() + 1, v[j].rend());
+			}
+		}
 	}
-
+	int cnt = 0;
+	for(int i = 0; i < t; i++){
+		if(v[i][0] == 1) cnt++;
+	}
+	cout << cnt << '\n';
 	return 0;
 }
 
