4a3073b562f2030a6c6409f05895afdbdb241f6d
jeongjulee42 <ljj3342@naver.com>
2024-04-18 20:00:02 +0900

    2632

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,71 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a, r, c, s, d, z, ret;
-struct Shark{int r; int c; int s; int d; int z;};
-vector<Shark> v;
-int ary[100][100];
-int dy[] = {0, -1, 1, 0, 0};
-int dx[] = {0, 0, 0, 1, -1};
-bool cmp(Shark p, Shark q){
-	return p.z > q.z;
-}
+
+int t, m, n, a, b;
+int ary[1000001];
+int bry[1000001];
+vector<int> ap;
+vector<int> bp;
+
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m >> a;
-	for(int i = 0; i < a; i++){
-		cin >> r >> c >> s >> d >> z;
-		v.push_back({r - 1, c - 1, s, d, z});
+
+	cin >> t;
+	cin >> m >> n;
+	for(int i = 0; i < m; i++){
+		cin >> a;
+		ap.push_back(a);
 	}
-	sort(v.begin(), v.end(), cmp);
-	for(auto vv : v) ary[vv.r][vv.c] = vv.z;
-	for(int man = 0; man < m; man++){
-		vector<Shark> temp;
-		int rmv = 0;
-		for(int i = 0; i < n; i++){
-			if(ary[i][man]){
-				rmv = ary[i][man];
-				ret += rmv;
-				break;
-			}
-		}
-		memset(ary, 0, sizeof(ary));
-		for(int k = 0; k < v.size(); k++){
-			Shark sh = v[k];
-			if(sh.z == rmv) continue;
-			int ny = sh.r;
-			int nx = sh.c;
-			if(sh.d < 3){
-				sh.s = sh.s % ((n - 1) * 2);
-			}else{	
-				sh.s = sh.s % ((m - 1) * 2);
-			}
-			for(int i = 0; i < sh.s; i++){
-				ny = sh.r + dy[sh.d];
-				nx = sh.c + dx[sh.d];
-				if(ny < 0) {
-					sh.d ++;
-					ny = sh.r + dy[sh.d];
-				}
-				else if (ny >= n) {
-					sh.d --;
-					ny = sh.r + dy[sh.d];
-				}
-				else if (nx < 0) {
-					sh.d --;
-					nx = sh.c + dx[sh.d];
-				}
-				else if (nx >= m) {
-					sh.d ++;
-					nx = sh.c + dx[sh.d];
-				}
-				sh.r = ny; sh.c = nx;
-			}
-			if(ary[ny][nx]) continue;
-			ary[ny][nx] = sh.z;
-			temp.push_back(sh);
-		}
-		v = temp;
+	for(int i = 0; i < n; i++){
+		cin >> b;
+		bp.push_back(b);
 	}
+	for(int i = 0; i < m - 2; i++) ap.push_back(ap[i]);
+	for(int i = 0; i < n - 2; i++) bp.push_back(bp[i]);
+	int asum = 0; int bsum = 0;
+	for(int i = 0; i < m; i++){
+		int sum = 0;
+		for(int j = i; j < i + m - 1; j++){
+			sum += ap[j];
+			if(sum > t) break;
+			ary[sum] ++;
+		}
+		asum += ap[i];
+	}
+	for(int i = 0; i < n; i++){
+		int sum = 0;
+		for(int j = i; j < i + n - 1; j++){
+			sum += bp[j];
+			if(sum > t) break;
+			bry[sum] ++;
+		}
+		bsum += bp[i];
+	}
+	ary[asum] ++; bry[bsum] ++;
+	int ret = 0;
+	for(int i = 0; i <= t; i++){
+		ret += (ary[i] * bry[t - i]);
+	}
+	ret += (ary[t] + bry[t]);
+	for(int i = 0; i < 10; i++) cout << ary[i] << " ";
+	cout << '\n';
+	for(int i = 0; i < 10; i++) cout << bry[i] << " ";
+	cout << '\n';
 	cout << ret << '\n';
 	return 0;
 }
