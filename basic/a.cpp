f176ae4e83cacee3e3305073f77a032ba9dca9c2
jeongjulee42 <ljj3342@naver.com>
2024-04-24 22:06:16 +0900

    1072

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,52 +2,35 @@
 using namespace std;
 typedef long long ll;
 
-struct Room{ll t; ll a; ll h;};
-Room ary[123456];
-ll atk, n, t, a, h, ret = 9e18;
-
-bool check(ll hp, ll at){
-	bool flag = true;
-	ll maxHp = hp;
-	for(int i = 0; i < n; i++){
-		Room here = ary[i];
-		if(here.t == 2){
-			hp = min(maxHp, hp + here.h);
-			at += here.a;
-		}else{
-			ll num = here.h / at;
-			if(here.h % at) num++;
-			cout << num << ":" << here.h << ":" << at << '\n';
-			hp -= (here.a * (num - 1));
-			if(hp <= 0){
-				flag = false;
-				break;
-			}
-		}
-	}
-	cout << maxHp << ":" << flag << '\n';
-	return flag;
+ll x, y, z, ret = 1000000004;
+bool check(ll mid){
+	ll _x = x + mid;
+	ll _y = y + mid;
+	ll res = ((double)_y * 100 / _x);
+	return z != res;
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n >> atk;
-	ll lo = 0, mid = 0, hi = 0;
-	for(int i = 0; i < n; i++){
-		cin >> t >> a >> h;
-		ary[i] = {t, a, h};
-		if(t == 1){
-			hi += (h / atk + 1) * a;
+	cin >> x >> y;
+	z = ((double)y * 100 / x);
+
+	if(x == y) cout << -1 << '\n';
+	else{
+		ll lo = 1, hi = x, mid = 0;
+		while(lo <= hi){
+			mid = (lo + hi) / 2;
+			if(check(mid)){
+				ret = min(ret, mid);
+				hi = mid - 1;
+			}else{
+				lo = mid + 1;
+			}
 		}
+		if(ret == 1000000004) ret = -1;
+		cout << ret << '\n';
 	}
-	while(lo <= hi){
-		mid = (lo + hi) / 2;
-		if(check(mid, atk)){
-			ret = min(ret, mid);
-			hi = mid - 1;
-		}else lo = mid + 1;
-	}
-	cout << ret << '\n';
+
 	return 0;
 }
