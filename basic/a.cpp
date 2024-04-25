5525fb284b0a8c6d2b267622903bb19f87db8f95
jeongjulee42 <ljj3342@naver.com>
2024-04-25 23:32:41 +0900

    11720

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,35 +2,21 @@
 using namespace std;
 typedef long long ll;
 
-ll x, y, z, ret = 1000000004;
-bool check(ll mid){
-	ll _x = x + mid;
-	ll _y = y + mid;
-	ll res = ((double)_y * 100 / _x);
-	return z != res;
-}
+
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> x >> y;
-	z = ((double)y * 100 / x);
-
-	if(x == y) cout << -1 << '\n';
-	else{
-		ll lo = 1, hi = x, mid = 0;
-		while(lo <= hi){
-			mid = (lo + hi) / 2;
-			if(check(mid)){
-				ret = min(ret, mid);
-				hi = mid - 1;
-			}else{
-				lo = mid + 1;
-			}
-		}
-		if(ret == 1000000004) ret = -1;
-		cout << ret << '\n';
+	string str = "";
+	int n = 0;
+	cin >> n;
+	cin >> str;
+	int sum = 0;
+	for(int i = 0; i < str.size(); i++){
+		int num = str[i] - '0';
+		sum += num;
 	}
+	cout << sum << '\n';
+
 
 	return 0;
 }
--- basic/b.cpp
+++ basic/b.cpp
@@ -1,101 +1,35 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-//인접 조건 다시보기
-int n, m, t, x, d, k, temp;
-vector<vector<pair<int, int>>> v;
+typedef long long ll;
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	cin >> n >> m >> t;
-	vector<pair<int, int>> dummy;
-	v.push_back(dummy);
-	for(int i = 1; i <= n; i++){
-		vector<pair<int, int>> _v;
-		for(int j = 0; j < m; j++){
-			cin >> temp;
-			_v.push_back({temp, 0});
-		}
-		v.push_back(_v);
+ll ary[1000004];
+ll s, c, ret = 0;
+bool check(int mid){
+	cout << mid << '\n';
+	int num = 0;
+	for(int i = 0; i < s; i++){
+		if(ary[i] / mid == 0) return false;
+		num += ary[i] / mid;
 	}
-	for(int i = 0; i < t; i ++){
-		cin >> x >> d >> k;
-		cout << "====before=====" << '\n';
-		for(int j = 1; j <= n; j++){
-			for(int l = 0; l < m; l++){
-				if(v[j][l].second) cout << ' ' << ' ';
-				else cout << v[j][l].first << ' ';
-			}
-			cout << '\n';
-		}
-		for(int j = x; j <= n; j += x){
-			if(d == 1){
-				rotate(v[j].begin(), v[j].begin() + k, v[j].end());
-			}else{
-				rotate(v[j].rbegin(), v[j].rbegin() + k, v[j].rend());
-			}
-		}
-		cout << "====rotate=====" << '\n';
-		for(int j = 1; j <= n; j++){
-			for(int l = 0; l < m; l++){
-				if(v[j][l].second) cout << ' ' << ' ';
-				else cout << v[j][l].first << ' ';
-			}
-			cout << '\n';
-		}
-		int cnt = 0;
-		int cnt2 = 0;
-		int sum = 0;
-		for(int j = 0; j < m; j++){
-			int _te = 1005;
-			for(int l = 1; l <= n; l++){
-				if(v[l][j].second == 1) continue;
-				sum += v[l][j].first;
-				cnt++;
-				if(_te == v[l][j].first){
-					v[l - 1][j].second = 1;
-					v[l][j].second = 1;
-					cnt2++;
-				}else{
-					_te = v[l][j].first;
-				}
-			}
-		}
-		
-		cout << "====remove=====" << '\n';
-		for(int j = 1; j <= n; j++){
-			for(int l = 0; l < m; l++){
-				if(v[j][l].second) cout << ' ' << ' ';
-				else cout << v[j][l].first << ' ';
-			}
-			cout << '\n';
-		}
-		if(!cnt2){
-			sum /= cnt;
-			for(int j = 1; j <= n; j++){
-				for(int l = 0; l < m; l++){
-					if(v[j][l].first > sum) v[j][l].first --;
-					else if(v[j][l].first < sum) v[j][l].first ++;
-				}
-			}
-		}
-		cout << "====" << sum << "=====" << '\n';
-		for(int j = 1; j <= n; j++){
-			for(int l = 0; l < m; l++){
-				if(v[j][l].second) cout << ' ' << ' ';
-				else cout << v[j][l].first << ' ';
-			}
-			cout << '\n';
-		}
-	}
-	int ret = 0;
-	for(int i = 1; i <= n; i++){
-		for(int j = 0; j < m; j++){
-			if(v[i][j].second == 1) continue;
-			ret += v[i][j].first;
-		}
-	}
-	cout << ret << '\n';
-	return 0;
+	return num >= c;
 }
 
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> s >> c;
+	ll sum = 0;
+	ll hi = 0, mid = 0, lo = 1;
+	for(int i = 0; i < s; i++) cin >> ary[i], sum+= ary[i], hi = max(hi, ary[i]);
+	while(lo <= hi){
+		mid = (lo + hi) / 2;
+		if(check(mid)){
+			lo = mid + 1;
+			ret = max(ret, mid);
+		}else{
+			hi = mid - 1;
+		}
+	}
+	cout << sum - (c * ret) << '\n';
+	return 0;
+}
