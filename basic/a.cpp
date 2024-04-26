7ef73a2dd6cfb30e8aab80cb7f33f55b34f0c892
jeongjulee42 <ljj3342@naver.com>
2024-04-26 18:25:59 +0900

    14627

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,21 +2,33 @@
 using namespace std;
 typedef long long ll;
 
+ll ary[1000004];
+ll s, c, ret = 0;
 
+bool check(int mid){
+	int num = 0;
+	for(int i = 0; i < s; i++){
+		num += ary[i] / mid;
+	}
+	return num >= c;
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-	string str = "";
-	int n = 0;
-	cin >> n;
-	cin >> str;
-	int sum = 0;
-	for(int i = 0; i < str.size(); i++){
-		int num = str[i] - '0';
-		sum += num;
+	
+	cin >> s >> c;
+	ll sum = 0;
+	ll hi = 0, mid = 0, lo = 1;
+	for(int i = 0; i < s; i++) cin >> ary[i], sum+= ary[i], hi = max(hi, ary[i]);
+	while(lo <= hi){
+		mid = (lo + hi) / 2;
+		if(check(mid)){
+			lo = mid + 1;
+			ret = max(ret, mid);
+		}else{
+			hi = mid - 1;
+		}
 	}
-	cout << sum << '\n';
-
-
+	cout << sum - (c * ret) << '\n';
 	return 0;
 }
