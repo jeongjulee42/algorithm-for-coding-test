b7bdafa8adc9569304a956523979043b21e16459
jeongjulee42 <ljj3342@naver.com>
2024-04-29 17:25:33 +0900

    11053

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,27 +2,21 @@
 using namespace std;
 typedef long long ll;
 
-double ary[10004], ret;
-int n;
-
+int lis[1004];
+int n, ret, len, num;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++) cin >> ary[i], ret = max(ret, ary[i]);
-	for(int i = 0; i < n; i++){
-		if(ary[i] < 1) continue;
-		double maxVal = 1;
-		for(int j = 0; j <= i; j++){
-			if(maxVal < 1)maxVal = 1;
-			maxVal *= ary[j];
-		}
-		ret = max(ret, maxVal);
+	for(int i = 0; i < n; i++ ){
+		cin >> num;
+		auto lp = lower_bound(lis, lis + len, num);
+		if(*lp == 0) len++;
+		*lp = num;
 	}
-	cout << fixed;
-	cout.precision(3);
-	cout << ret << '\n';
+	cout << len << '\n';
+
 
 	return 0;
 }
