a41fc0f312a421613884dcfa74eb10d03f672f8d
jeongjulee42 <ljj3342@naver.com>
2024-04-29 16:19:29 +0900

    14003

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,23 +2,35 @@
 using namespace std;
 typedef long long ll;
 
-int n, a[1001], cnt[1001], ret;
+int n, lis[1000004], len, num;
+pair<int, int> ans[1000004];
+stack<int> stk;
+const int INF = 1e9 + 4;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
+	fill(lis, lis + 1000004, INF);
 	cin >> n;
-	for(int i = 0; i < n; i++) cin >>a[i];
-
 	for(int i = 0; i < n; i++){
-		int maxVal = 0;
-		for(int j = 0; j < i; j++){
-			if(a[j] < a[i] && maxVal < cnt[j]) maxVal = cnt[j];
-		}
-		cnt[i] = maxVal + 1;
-		ret = max(ret, cnt[i]);
+		cin >> num;
+		auto lowerPos = lower_bound(lis, lis + len, num);
+		int _pos = (int)(lowerPos - lis);
+		if(*lowerPos == INF) len++;
+		*lowerPos = num;
+		ans[i].first = _pos;
+		ans[i].second = num;
 	}
-	cout << ret << '\n';
-	
+	cout << len << '\n';
+	for(int i = n - 1; i >= 0; i--){
+		if(ans[i].first == len - 1){
+			stk.push(ans[i].second); len--;
+		}
+	}
+	while(stk.size()){
+		cout << stk.top() << ' ';
+		stk.pop();
+	}
+
 	return 0;
 }
