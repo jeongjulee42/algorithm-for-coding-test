604078a93f3cffc5ec872b1de5051b2d5b970763
jeongjulee42 <ljj3342@naver.com>
2024-04-30 19:32:24 +0900

    10810

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,37 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-pair<int, int> p[104];
-int lis[104];
-vector<int> temp;
-int n, ret, a, b, len;
+int ary[101];
+int n, m, a, b, c;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		p[i] = {a, b};
-		temp.push_back(b);
-	}
-	sort(temp.begin(), temp.end());
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < temp.size(); j++){
-			if(p[i].second == temp[j]){
-				p[i].second = j;
-			}
+	cin >> n >> m;
+	for(int i = 0; i < m; i++){
+		cin >> a >> b >> c;
+		for(int j = a; j <= b; j++){
+			ary[j] = c;
 		}
 	}
-	sort(p, p + n);
-	for(int i = 0; i < n; i++){
-		int num = p[i].second;
-		auto lp = lower_bound(lis, lis + len, num);
-		if(*lp == 0) len++;
-		*lp = num;
+	for(int i = 1; i <= n; i++){
+		cout << ary[i] << ' ';
 	}
-	cout << n - len << '\n';
 
 	return 0;
 }
