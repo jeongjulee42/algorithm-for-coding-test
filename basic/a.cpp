924dd5a14835ec0507570e9133e895ffe1ec4595
jeongjulee42 <ljj3342@naver.com>
2024-05-01 21:03:08 +0900

    10813

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 
 int ary[101];
-int n, m, a, b, c;
+int n, m, a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> m;
-	for(int i = 0; i < m; i++){
-		cin >> a >> b >> c;
-		for(int j = a; j <= b; j++){
-			ary[j] = c;
-		}
-	}
 	for(int i = 1; i <= n; i++){
-		cout << ary[i] << ' ';
+		ary[i] = i;
 	}
+	for(int i = 0; i < m; i++){
+		cin >> a >> b;
+		swap(ary[a], ary[b]);
+	}
+	for(int i = 1; i <= n; i++) cout << ary[i] << ' ';
 
 	return 0;
 }
