5ce2f74a7f636fac865817afe9b75eacb0f1b535
jeongjulee42 <ljj3342@naver.com>
2024-05-02 15:36:56 +0900

    2240

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[101];
-int n, m, a, b;
+int dp[1004][2][34];
+int n, m, b[1004];
+
+int go(int idx, int tree, int cnt){
+	if(cnt < 0) return -1e9;
+	if(idx == n) return cnt == 0 ? 0 : -1e9;
+	int &ret = dp[idx][tree][cnt];
+	if(~ret) return ret;
+	return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
+	memset(dp, -1, sizeof(dp));
 	cin >> n >> m;
-	for(int i = 1; i <= n; i++){
-		ary[i] = i;
+	for(int i = 0; i < n; i++){
+		cin >> b[i];
 	}
-	for(int i = 0; i < m; i++){
-		cin >> a >> b;
-		swap(ary[a], ary[b]);
-	}
-	for(int i = 1; i <= n; i++) cout << ary[i] << ' ';
+	cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
 
 	return 0;
 }
