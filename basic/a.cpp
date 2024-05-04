c2833a887d6201caf8e7a29f62cf0b62728b9694
jeongjulee42 <ljj3342@naver.com>
2024-05-04 23:14:39 +0900

    2908

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,26 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dp[1004][2][34];
-int n, m, b[1004];
-
-int go(int idx, int tree, int cnt){
-	if(cnt < 0) return -1e9;
-	if(idx == n) return cnt == 0 ? 0 : -1e9;
-	int &ret = dp[idx][tree][cnt];
-	if(~ret) return ret;
-	return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
-}
+string a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-
-	memset(dp, -1, sizeof(dp));
-	cin >> n >> m;
-	for(int i = 0; i < n; i++){
-		cin >> b[i];
-	}
-	cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
+	
+	cin >> a >> b;
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+	if(a > b) cout << a << '\n';
+	else cout << b << '\n';
 
 	return 0;
 }
