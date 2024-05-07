0a4131574e8b8ed3031b13ba67623bbbf4db9544
jeongjulee42 <ljj3342@naver.com>
2024-05-07 18:16:06 +0900

    12852

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,30 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[26], ret, idx, cnt;
-string str;
+int num;
+int ary[1000004];
+vector<int> ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> str;
-	for(int i = 0; i < str.size(); i++){
-		char c = str[i];
-		if((int)c >= 97) ary[c - 97]++;
-		else ary[c - 65]++;
-	}
-	for(int i = 0; i < 26; i++){
-		if(ret < ary[i]){
-			ret = ary[i];
-			idx = i;
-			cnt = 1;
-		}else if(ret == ary[i]){
-			cnt++;
+	cin >> num;
+	queue<int> q;
+	q.push(num);
+	while(q.size()){
+		int temp = q.front(); q.pop();
+		if(temp == 1) {
+			ret.push_back(1);
+			break;
+		}
+		if(!ary[temp - 1]){
+			q.push(temp - 1);
+			ary[temp - 1] = temp;
+		}
+		if(temp % 2 == 0 && !ary[temp / 2]) {
+			q.push(temp / 2); ary[temp / 2] = temp;
+		}
+		if(temp % 3 == 0 && !ary[temp / 3]){
+			q.push(temp / 3); ary[temp / 3] = temp;
 		}
 	}
-	if(cnt > 1) cout << '?' << '\n';
-	else{
-		cout << (char)(idx + 65) << '\n';
+	int temp = 1;
+	while(ary[temp]){
+		ret.push_back(ary[temp]);
+		temp = ary[temp];
 	}
+	cout << ret.size() - 1 << '\n';
+	reverse(ret.begin(), ret.end());
+	for(int a : ret) cout << a << ' ';
 	return 0;
 }
