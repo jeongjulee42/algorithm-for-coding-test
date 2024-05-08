523aeafb5fc3af851a0c4516146fd638f3adb2c3
jeongjulee42 <ljj3342@naver.com>
2024-05-08 21:48:29 +0900

    12865

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double p, m;
-int c, n, money, price;
-int ary[10004];
+int n, w, v, k;
+int ary[100004];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	while(1){
-		cin >> n >> m;
-		money = (int)(m * 100 + 0.5);
-		if(n == 0) break;
-		for(int i = 0; i < n; i++){
-			cin >> c >> p;
-			price = (int)(p * 100 + 0.5);
-			for(int j = price; j <= money; j++){
-				ary[j] = max(ary[j], ary[j - price] + c);
-			}
+	cin >> n >> k;
+	for(int i = 0; i < n; i++){
+		cin >> w >> v;
+		for(int j = k; j >= w; j--){
+			ary[j] = max(ary[j], v + ary[j - w]);
 		}
-		cout << ary[money] << '\n';
-		memset(ary, 0, sizeof(ary));
 	}
-	
+	cout << ary[k] << '\n';
 	return 0;
 }
