8cecb31fe1169f0cdce3abc85ff5b0dfd3c537b5
jeongjulee42 <ljj3342@naver.com>
2024-05-08 15:35:02 +0900

    2293

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,31 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, k;
-int ary[10004];
-int m[104];
+int n, k, num, ary[10001];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n >> k;
-	for(int i = 0; i < n; i++) cin >> m[i];
-
-	queue<int> q;
-	q.push(0);
-	while(q.size()){
-		int temp = q.front(); q.pop();
-		if(temp == k){
-			cout << ary[temp] << '\n';
-			exit(0);
-		}
-		for(int i = 0; i < n; i++){
-			if(temp + m[i] > k) continue;
-			if(ary[temp + m[i]]) continue;
-			ary[temp + m[i]] = ary[temp] + 1;
-			q.push(temp + m[i]);
+	for(int i = 0; i < n; i++){
+		cin >> num;
+		for(int j = 1; j <= k; j++){
+			int temp = max(0, j - num);
+			ary[j] = ary[j] + ary[temp];
+			if(num == j) ary[j]++;
 		}
 	}
-	cout << -1 << '\n';
+	cout << ary[k] << '\n';
 	return 0;
 }
