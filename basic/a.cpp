acb0346e083e09a6ee7ce3bf1a52ed4d80e9255e
jeongjulee42 <ljj3342@naver.com>
2024-05-09 19:16:01 +0900

    1344

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[104];
-pair<int, int> p[24];
-int n, temp;
+double ary[20][20], bry[20][20];
+int a, b;
+int s[] = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18};
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> p[i].first;
-	}
-	for(int i = 0; i < n; i++){
-		cin >> p[i].second;
-	}
-	for(int i = 0; i < n; i++){
-		int h = p[i].first;
-		int s = p[i].second;
-		for(int j = 100; j >= h; j--){
-			ary[j] = max(ary[j], ary[j - h] + s);
+
+	cin >> a >> b;
+	double c = (double)a / 100;
+	double d = (double)b / 100;
+
+	ary[1][0] = 1 - c; ary[1][1] = c;
+	bry[1][0] = 1 - d; bry[1][1] = d;
+
+	for(int i = 2; i <= 18; i++){
+		for(int j = 0; j <= i; j++){
+			double temp = j - 1 < 0 ? 0 : ary[i - 1][j - 1];
+			double temp2 = j - 1 < 0 ? 0 : bry[i - 1][j - 1];
+			ary[i][j] = (c * temp) + ((1 - c) * ary[i - 1][j]);
+			bry[i][j] = (d * temp2) + ((1 - d) * bry[i - 1][j]);
 		}
 	}
-	cout << ary[99] << '\n';
+
+
+	double reta = 0, retb = 0;
+	for(int i = 0; i < 12; i++){
+		int num = s[i];
+		reta += ary[18][num];
+		retb += bry[18][num];
+	}
+	cout << reta << '\n';
+	cout << 1 - (reta * retb) << '\n';
+
 	return 0;
 }
