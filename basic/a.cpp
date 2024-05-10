c31dc31bbfe048a8b8bd05987824a9c61b45eeda
jeongjulee42 <ljj3342@naver.com>
2024-05-10 22:44:10 +0900

    1316

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,38 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double ary[20][20], bry[20][20];
-int a, b;
-int s[] = {0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18};
+int adj[26];
+stack<char> stk;
+string str;
+int n, cnt;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> a >> b;
-	double c = (double)a / 100;
-	double d = (double)b / 100;
-
-	ary[1][0] = 1 - c; ary[1][1] = c;
-	bry[1][0] = 1 - d; bry[1][1] = d;
-
-	for(int i = 2; i <= 18; i++){
-		for(int j = 0; j <= i; j++){
-			double temp = j - 1 < 0 ? 0 : ary[i - 1][j - 1];
-			double temp2 = j - 1 < 0 ? 0 : bry[i - 1][j - 1];
-			ary[i][j] = (c * temp) + ((1 - c) * ary[i - 1][j]);
-			bry[i][j] = (d * temp2) + ((1 - d) * bry[i - 1][j]);
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		memset(adj, 0, sizeof(adj));
+		cin >> str;
+		for(int j = 0; j < str.size(); j++){
+			if(stk.size() && stk.top() == str[j]){
+				stk.pop();
+			}
+			stk.push(str[j]);
 		}
+		bool flag = true;
+		while(stk.size()){
+			char c = stk.top(); stk.pop();
+			if(adj[c - 97]) flag = false;
+			adj[c - 97] = 1;			
+		}
+		if(flag) cnt++;
 	}
-
-
-	double reta = 0, retb = 0;
-	for(int i = 0; i < 12; i++){
-		int num = s[i];
-		reta += ary[18][num];
-		retb += bry[18][num];
-	}
-	cout << reta << '\n';
-	cout << 1 - (reta * retb) << '\n';
+	cout << cnt << '\n';
 
 	return 0;
 }
