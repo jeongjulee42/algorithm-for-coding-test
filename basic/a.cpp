6ad95986fe5e3c239824cc1c2356fa0933e8aaac
jeongjulee42 <ljj3342@naver.com>
2024-05-11 22:04:06 +0900

    11718

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[101][101], n, m, temp;
+string str;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cin >> ary[i][j];
-		}
-	}
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cin >> temp;
-			ary[i][j] += temp;
-		}
-	}
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
-			cout << ary[i][j] << ' ';
-		}cout << '\n';
+	while(getline(cin, str)){
+		if(str == "") break;
+		cout << str << '\n';
 	}
 	return 0;
 }
