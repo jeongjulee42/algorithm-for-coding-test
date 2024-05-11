ceff18d3c519e3e1e257fb412a79d327b0f9e934
jeongjulee42 <ljj3342@naver.com>
2024-05-11 22:55:17 +0900

    2941

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,15 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+stack<char> stk;
+string str;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-	cout << "         ,r\'\"7" << '\n';
-	cout << "r`-_   ,\'  ,/" << '\n';
-	cout << " \\. \". L_r\'" << '\n';
-	cout << "   `~\\/" << '\n';
-	cout << "      |" << '\n';
-	cout << "      |" << '\n';
+	cin >> str;
+	for(int i = 0; i < str.size(); i++){
+		char c = str[i];
+		if(c == 'j'){
+			if(stk.size() && (stk.top() == 'l' || stk.top() == 'n'))stk.pop();
+		} else if(c == '='){
+			if(stk.size() && (stk.top() == 's' || stk.top() == 'c')) stk.pop();
+			else if (stk.size() && stk.top() == 'z'){
+				stk.pop();
+				if(stk.size() && stk.top() == 'd'){
+					stk.pop();
+				}
+			}
+		} else if(c == '-'){
+			if(stk.size() && (stk.top() == 'd' || stk.top() == 'c')) stk.pop();
+		}
+		stk.push(c);
+	}
+	cout << stk.size() << '\n';
 	return 0;
 }
