a59cdccb3ed429ce54e077ed03d92ae5af480b1c
jeongjulee42 <ljj3342@naver.com>
2024-05-11 22:15:00 +0900

    5622

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 string str;
+vector<string> v = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	while(getline(cin, str)){
-		if(str == "") break;
-		cout << str << '\n';
+	cin >> str;
+	int sum = 0;
+	for(int i = 0; i < str.size(); i++){
+		char c = str[i];
+		for(int j = 0; j < v.size(); j++){
+			string t = v[j];
+			for(int k = 0; k < t.size(); k++){
+				if(t[k] == c){
+					sum += (3 + j);
+				}
+			}
+		}
 	}
+	cout << sum << '\n';
 	return 0;
 }
