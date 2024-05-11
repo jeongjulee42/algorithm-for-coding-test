345c493667ea52e03822a67584f5962a62aa8205
jeongjulee42 <ljj3342@naver.com>
2024-05-11 21:56:05 +0900

    2738

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,33 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int adj[26];
-stack<char> stk;
-string str;
-int n, cnt;
+int ary[101][101], n, m, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
+	cin >> n >> m;
 	for(int i = 0; i < n; i++){
-		memset(adj, 0, sizeof(adj));
-		cin >> str;
-		for(int j = 0; j < str.size(); j++){
-			if(stk.size() && stk.top() == str[j]){
-				stk.pop();
-			}
-			stk.push(str[j]);
+		for(int j = 0; j < m; j++){
+			cin >> ary[i][j];
 		}
-		bool flag = true;
-		while(stk.size()){
-			char c = stk.top(); stk.pop();
-			if(adj[c - 97]) flag = false;
-			adj[c - 97] = 1;			
-		}
-		if(flag) cnt++;
 	}
-	cout << cnt << '\n';
-
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cin >> temp;
+			ary[i][j] += temp;
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cout << ary[i][j] << ' ';
+		}cout << '\n';
+	}
 	return 0;
 }
