0b98e694c82b10cd1c113ee6fbeffa43afd08193
jeongjulee42 <ljj3342@naver.com>
2024-05-11 22:20:33 +0900

    25083

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,25 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string str;
-vector<string> v = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-
-	cin >> str;
-	int sum = 0;
-	for(int i = 0; i < str.size(); i++){
-		char c = str[i];
-		for(int j = 0; j < v.size(); j++){
-			string t = v[j];
-			for(int k = 0; k < t.size(); k++){
-				if(t[k] == c){
-					sum += (3 + j);
-				}
-			}
-		}
-	}
-	cout << sum << '\n';
+	cout << "         ,r\'\"7" << '\n';
+	cout << "r`-_   ,\'  ,/" << '\n';
+	cout << " \\. \". L_r\'" << '\n';
+	cout << "   `~\\/" << '\n';
+	cout << "      |" << '\n';
+	cout << "      |" << '\n';
 	return 0;
 }
