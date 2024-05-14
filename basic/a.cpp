a691584ccf9779330eefc47f3f743ac60a65e29f
jeongjulee42 <ljj3342@naver.com>
2024-05-14 13:17:31 +0900

    3687

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,25 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll n, dp[31][31];
+int number[] = {6,2,5,5,4,5,6,3,7,6};
+string minAry[101];
+string maxAry[101];
+int n, m;
 
-ll go(int whole, int not_whole){
-	if(whole == 0 && not_whole == 0) return 1;
-	if(dp[whole][not_whole]) return dp[whole][not_whole];
-	ll &ret = dp[whole][not_whole];
-	if(whole > 0) ret += go(whole - 1, not_whole + 1);
-	if(not_whole > 0) ret += go(whole, not_whole - 1);
-	return ret;
+bool cmp(string a, string b){
+	if(a.size() > b.size()) return true;
+	else if (a.size() < b.size()) return false;
+	for(int i = 0; i < a.size(); i++){
+		if(a[i] > b[i]) return true;
+		else if(a[i] < b[i]) return false;
+	}
+	return true;
+}
+
+string makeMin(string s, string a){
+	if(a == "0"){
+		if(s == "") return "6";
+		return s[0] + a + s.substr(1);
+	}
+	return a + s;
+}
+
+string makeMax(string s, string a){
+	return s + a;
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    while(true){
-        cin >> n; if(n == 0)break; 
-        cout << go(n, 0) << "\n";
-    }
 
+	for(int i = 9; i >= 0; i--){
+		int num = number[i];
+		for(int j = num; j <= 100; j++){
+			if(j - num != 0 && minAry[j - num] == "") continue;
+			string temp = makeMin(minAry[j - num], to_string(i));
+			if(minAry[j] == "") minAry[j] = temp;
+			else minAry[j] = cmp(minAry[j], temp) ? temp : minAry[j];
+
+			string temp2 = makeMax(maxAry[j - num], to_string(i));
+			maxAry[j] = cmp(maxAry[j], temp2) ? maxAry[j] : temp2;
+		}
+	}
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> m;
+		cout << minAry[m] << ' ' << maxAry[m] << '\n';
+	}
 
 	return 0;
 }
