a5a6450d86d18139776f5c14e2e94adc6fc6a0bc
jeongjulee42 <ljj3342@naver.com>
2024-05-15 19:42:59 +0900

    3687

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,53 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int number[] = {6,2,5,5,4,5,6,3,7,6};
-string minAry[101];
-string maxAry[101];
-int n, m;
-
-bool cmp(string a, string b){
-	if(a.size() > b.size()) return true;
-	else if (a.size() < b.size()) return false;
-	for(int i = 0; i < a.size(); i++){
-		if(a[i] > b[i]) return true;
-		else if(a[i] < b[i]) return false;
-	}
-	return true;
-}
-
-string makeMin(string s, string a){
-	if(a == "0"){
-		if(s == "") return "6";
-		return s[0] + a + s.substr(1);
-	}
-	return a + s;
-}
-
-string makeMax(string s, string a){
-	return s + a;
-}
+int ary[14][204];
+int b[14];
+int n, m, c;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	for(int i = 9; i >= 0; i--){
-		int num = number[i];
-		for(int j = num; j <= 100; j++){
-			if(j - num != 0 && minAry[j - num] == "") continue;
-			string temp = makeMin(minAry[j - num], to_string(i));
-			if(minAry[j] == "") minAry[j] = temp;
-			else minAry[j] = cmp(minAry[j], temp) ? temp : minAry[j];
+	cin >> n >> m >> c;
+	for(int i = 1; i <= n; i++) cin >> b[i];
 
-			string temp2 = makeMax(maxAry[j - num], to_string(i));
-			maxAry[j] = cmp(maxAry[j], temp2) ? maxAry[j] : temp2;
+	for(int i = 1; i <= n; i++){
+		for(int j = b[i]; j <= m * c; j++){
+			if(j % 5 - b[i] < 0) ary[i][j] = ary[i - 1][j];
+			else ary[i][j] = max(ary[i-1][j-b[i]] + 1, ary[i-1][j]);
 		}
+		for(int j = 0; j <= m*c; j++)cout << ary[i][j] << ' ';
+		cout << '\n';
 	}
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> m;
-		cout << minAry[m] << ' ' << maxAry[m] << '\n';
-	}
+
 
 	return 0;
 }
