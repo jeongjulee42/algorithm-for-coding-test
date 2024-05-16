57ca11745ff005965824b960bdcf06b496d864b3
jeongjulee42 <ljj3342@naver.com>
2024-05-16 22:17:30 +0900

    2563

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,30 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-char ary[5][16];
-int v[5][16];
-int m;
+int ary[104][104];
+int n, m, num;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	for(int i = 0; i < 5; i++){
-		string s;
-		cin >> s;
-		for(int j = 0; j < s.size(); j++){
-			ary[i][j] = s[j];
-			v[i][j] = 1;
+	cin >> num;
+	for(int i = 0; i < num; i++){
+		cin >> n >> m;
+		for(int j = n - 1; j < n + 9; j++){
+			for(int k = m - 1; k < m + 9; k++){
+				ary[j][k] = 1;
+			}
 		}
 	}
-	string ret = "";
-	for(int i = 0; i < 16; i++){
-		for(int j = 0; j < 5; j++){
-			if(!v[j][i]) continue;
-			ret += ary[j][i];
+	int ret = 0;
+	for(int i = 0; i <= 100; i++){
+		for(int j = 0; j <= 100; j++){
+			if(ary[i][j]) ret++;
 		}
 	}
 	cout << ret << '\n';
-
 	return 0;
 }
 
