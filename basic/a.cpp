76c64a9d33013e09fa9e18573aae8b19d3dad5d0
jeongjulee42 <ljj3342@naver.com>
2024-05-17 17:51:03 +0900

    10815

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,28 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[104][104];
-int n, m, num;
+map<int, int> mp;
+int n, m, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> num;
-	for(int i = 0; i < num; i++){
-		cin >> n >> m;
-		for(int j = n - 1; j < n + 9; j++){
-			for(int k = m - 1; k < m + 9; k++){
-				ary[j][k] = 1;
-			}
-		}
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		mp[temp] = 1;
 	}
-	int ret = 0;
-	for(int i = 0; i <= 100; i++){
-		for(int j = 0; j <= 100; j++){
-			if(ary[i][j]) ret++;
-		}
+	cin >> m;
+	for(int i = 0; i < m; i++){
+		cin >> temp;
+		if(mp.find(temp) == mp.end()) cout << 0 << ' ';
+		else cout << 1 << ' ';
 	}
-	cout << ret << '\n';
+
 	return 0;
 }
 
