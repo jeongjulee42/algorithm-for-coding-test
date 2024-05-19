6a016f1951178e2671b3f32235c50b852903bc66
jeongjulee42 <ljj3342@naver.com>
2024-05-19 22:30:55 +0900

    2750

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,25 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-map<string, int> mp;
+int n;
+vector<int> v;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	for(int i = 0; i < n; i++) {
-		string str = "";
-		cin >> str;
-		mp.insert({str, 1});
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		int temp = 0;
+		cin >> temp;
+		v.push_back(temp);
 	}
-	int ret = 0;
-	for(int i = 0; i < m; i++){
-		string str = "";
-		cin >> str;
-		if(mp.find(str) != mp.end()) ret++;
-	}	
-	cout << ret << '\n';
+	sort(v.begin(), v.end());
+	for(int a : v) cout << a << '\n';
 
 	return 0;
 }
