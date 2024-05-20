14ddacac4c31fc47015008b665aa476f27f068d0
jeongjulee42 <ljj3342@naver.com>
2024-05-20 21:07:28 +0900

    25305

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
+int n, m;
 vector<int> v;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-
-	cin >> n;
+	
+	cin >> n >> m;
 	for(int i = 0; i < n; i++){
 		int temp = 0;
 		cin >> temp;
 		v.push_back(temp);
 	}
-	sort(v.begin(), v.end());
-	for(int a : v) cout << a << '\n';
-
+	sort(v.begin(), v.end(), greater<int>());
+	cout << v[m - 1] << '\n';	
 	return 0;
 }
 
