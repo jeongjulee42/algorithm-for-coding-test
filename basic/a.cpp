8aa59711442765bb3ea33a036fb2becdef069e8e
jeongjulee42 <ljj3342@naver.com>
2024-05-26 22:26:49 +0900

    11279

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,20 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-queue<int> q;
-int n;
+priority_queue<int> pq;
+int n, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	for(int i = 1; i <= n; i++) q.push(i);
-	while(q.size() != 1){
-		q.pop();
-		q.push(q.front());
-		q.pop();
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		if(temp == 0){
+			if(pq.size() == 0) cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else{
+			pq.push(temp);
+		}
 	}
-	cout << q.front() << '\n';
+	
 	return 0;
 }
 
