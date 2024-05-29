3d5d7a0027312902ba1e9b11166ecb8361d4513a
jeongjulee42 <ljj3342@naver.com>
2024-05-29 21:58:45 +0900

    27323

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,42 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-queue<int> q;
-int n;
-
+int n, m;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		string str = "";
-		cin >> str;
-		if(str == "push"){
-			int temp = 0;
-			cin >> temp;
-			q.push(temp);
-		} else if(str == "pop"){
-			if(!q.size()){
-				cout << -1 << '\n';
-			}
-			else {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-		} else if(str == "size"){
-			cout << q.size() << '\n';
-		} else if(str == "empty"){
-			if(!q.size()) cout << 1 << '\n';
-			else cout << 0 << '\n';
-		} else if(str == "front"){
-			if(!q.size()) cout << -1 << '\n';
-			else cout << q.front() << '\n';
-		} else if(str == "back"){
-			if(!q.size()) cout << -1 << '\n';
-			else cout << q.back() << '\n';
-		}
-	}
+	cin >> n >> m;
+	cout << n * m << '\n';
 	
 	return 0;
 }
