5f7dce59480886abc6688cf0b1ffd27090af745a
jeongjulee42 <ljj3342@naver.com>
2024-06-01 14:01:26 +0900

    10872

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
+int n, ret = 1;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	cout << pow(2, n) << '\n';
-	
+	for(int i = n; i >= 2; i--){
+		ret *= i;
+	}
+	cout << ret << '\n';
 	return 0;
 }
 
