101d3dbb3ef566e1e18bd183a483d21c39f520e5
jeongjulee42 <ljj3342@naver.com>
2024-06-03 20:03:58 +0900

    11650

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,14 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string n;
+int n;
+pair<int, int> p[100004];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	sort(n.begin(), n.end(), greater<int>());
-	cout << n << '\n';
+	for(int i = 0; i < n; i++){
+		cin >> p[i].first >> p[i].second;
+	}
+	sort(p, p + n);
+	for(int i = 0; i < n; i++){
+		cout << p[i].first << ' ' << p[i].second << '\n';
+	}
+
 	return 0;
 }
 
