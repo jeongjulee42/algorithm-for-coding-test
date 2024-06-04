eefba5f8805bfbae80631814acc92286562e6e4a
jeongjulee42 <ljj3342@naver.com>
2024-06-04 21:12:38 +0900

    1085

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,21 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
-pair<int, int> p[100004];
+int x, y, w, h;
+int ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> p[i].first >> p[i].second;
-	}
-	sort(p, p + n);
-	for(int i = 0; i < n; i++){
-		cout << p[i].first << ' ' << p[i].second << '\n';
-	}
-
+	cin >> x >> y >> w >> h;
+	ret = min(min(x, w - x), min(y, h - y));
+	cout << ret << '\n';
 	return 0;
 }
 
