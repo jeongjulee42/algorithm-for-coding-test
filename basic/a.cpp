5009cc3a8aa569efcd28f592e2bb53a16ad3b32f
jeongjulee42 <ljj3342@naver.com>
2024-06-05 22:29:28 +0900

    3009

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int x, y, w, h;
-int ret;
+int ary[1004], bry[1004];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> x >> y >> w >> h;
-	ret = min(min(x, w - x), min(y, h - y));
-	cout << ret << '\n';
+	for(int i = 0; i < 3; i++){
+		cin >> x >> y;
+		ary[x]++;
+		bry[y]++;
+	}
+	for(int i = 0; i < 1001; i++){
+		if(ary[i] == 1) w = i;
+		if(bry[i] == 1) h = i;
+	}
+	cout << w << ' ' << h << '\n';
+	
 	return 0;
 }
 
