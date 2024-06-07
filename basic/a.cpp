cc3ad2fb976da34b440c703cac888c667ff33d17
jeongjulee42 <ljj3342@naver.com>
2024-06-07 20:40:39 +0900

    15894

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,22 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long x1 = 100004, x2 = -100004, y = 100004, y2 = -100004;
 int n;
-long long a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		x1 = min(x1, a);
-		x2 = max(x2, a);
-		y = min(y, b);
-		y2 = max(y2, b);
-	}
-	cout << (x2 - x1) * (y2 - y) << '\n';
+	cout << n * 4 << '\n';
+
 	return 0;
 }
 
