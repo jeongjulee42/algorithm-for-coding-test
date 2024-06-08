1d28038e3af77526c4e9113ef33a32c1aa1f699d
jeongjulee42 <ljj3342@naver.com>
2024-06-08 23:15:47 +0900

    10101

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,13 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
+int a, b, c;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	cin >> n;
-	cout << n * 4 << '\n';
+	cin >> a >> b >> c;
+	if(a + b + c != 180) cout << "Error" << '\n';
+	else if(a != b && b != c && c != a) cout << "Scalene" << '\n';
+	else if(a == b && b == c) cout << "Equilateral" << '\n';
+	else cout << "Isosceles" << '\n';
 
 	return 0;
 }
