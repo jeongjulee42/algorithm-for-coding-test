3e2d965316aae3688c5739cc244410f09d33aa06
jeongjulee42 <ljj3342@naver.com>
2024-06-10 18:12:38 +0900

    14215

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,22 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a, b, c;
 int ary[3];
+int a, b, c;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	
-	while(1){
-		cin >> ary[0] >> ary[1] >> ary[2];
-		if(ary[0] == 0 && ary[1] == 0 && ary[2] == 0) break;
-		a = ary[0]; b = ary[1]; c = ary[2];
-		sort(ary, ary + 3);
-		if(a + b <= c) cout << "Invalid" << '\n';
-		else if(a != b && b != c && c != a) cout << "Scalene" << '\n';
-		else if(a == b && b == c) cout << "Equilateral" << '\n';
-		else cout << "Isosceles" << '\n';
+	cin >> ary[0] >> ary[1] >> ary[2];
+	sort(ary, ary + 3);
+	a = ary[0]; b = ary[1]; c = ary[2];
+	if(a + b <= c){
+		c = a + b - 1;
 	}
+	cout << a + b + c << '\n';
 	return 0;
 }
 
