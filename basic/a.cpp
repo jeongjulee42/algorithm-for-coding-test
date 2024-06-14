d78894cb9baf3981d21dfeae5c93eb890bd2e207
jeongjulee42 <ljj3342@naver.com>
2024-06-14 21:22:17 +0900

    5557

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,33 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-vector<int> ary[2004];
-int str[2004];
-int s, e, n, m, a, b;
-
-int check(int start, int last){
-	if(start == last) return 1;
-	if(str[start] == str[last]){
-		if(last - 1 <= start + 1) return 1;
-		if(ary[last - 1][start + 1]) return 1;
-	}
-	return 0;
-}
+ll ret;
+ll ary[104][24];
+int num[104];
+int n;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-	
+
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> str[i];
-		for(int j = 0; j <= i; j++){
-			ary[i].push_back(check(j, i));
+		cin >> num[i];
+	}
+	ary[0][num[0]] = 1;
+	for(int i = 1; i < n; i++){
+		for(int j = 0; j <= 20; j++){
+			if(ary[i - 1][j]){
+				int a = j - num[i];
+				int b = j + num[i];
+				if(a >= 0 && a <= 20){
+					ary[i][a] += ary[i - 1][j];
+				}
+				if(b >= 0 && b <= 20){
+					ary[i][b] += ary[i - 1][j];
+				}
+			}
 		}
 	}
-	cin >> m;
-	for(int i = 0; i < m; i++){
-		cin >> a >> b;
-		cout << ary[b-1][a-1] << '\n';
-	}
+	cout << ary[n - 2][num[n - 1]];
+	
 	return 0;
 }
