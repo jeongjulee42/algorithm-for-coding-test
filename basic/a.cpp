4c5f132b349641cef7809ce01e6e3c496be374e0
jeongjulee42 <ljj3342@naver.com>
2024-06-15 22:34:34 +0900

    11651

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,35 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll ret;
-ll ary[104][24];
-int num[104];
-int n;
+pair<int, int> ary[100004];
+int n, a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> num[i];
+		cin >> a >> b;
+		ary[i] = {b, a};
 	}
-	ary[0][num[0]] = 1;
-	for(int i = 1; i < n; i++){
-		for(int j = 0; j <= 20; j++){
-			if(ary[i - 1][j]){
-				int a = j - num[i];
-				int b = j + num[i];
-				if(a >= 0 && a <= 20){
-					ary[i][a] += ary[i - 1][j];
-				}
-				if(b >= 0 && b <= 20){
-					ary[i][b] += ary[i - 1][j];
-				}
-			}
-		}
-	}
-	cout << ary[n - 2][num[n - 1]];
-	
+	sort(ary, ary + n);
+	for(int i = 0; i < n; i++) cout << ary[i].second << ' ' << ary[i].first << '\n';
+
 	return 0;
 }
