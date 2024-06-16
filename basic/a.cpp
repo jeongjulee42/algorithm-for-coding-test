0ab76de298fc7b4bc88a3c70a384512c6bd2a6b7
jeongjulee42 <ljj3342@naver.com>
2024-06-16 21:17:38 +0900

    1181

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,19 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-pair<int, int> ary[100004];
-int n, a, b;
+int num;
+string str[20004];
+
+bool cmp(string a, string b){
+	if(a.size() < b.size()) return true;
+	else if(a.size() > b.size()) return false;
+	else return a < b;
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		ary[i] = {b, a};
+	cin >> num;
+	for(int i = 0; i < num; i++){
+		cin >> str[i];
 	}
-	sort(ary, ary + n);
-	for(int i = 0; i < n; i++) cout << ary[i].second << ' ' << ary[i].first << '\n';
+	sort(str, str + num, cmp);
+	cout << str[0] << '\n';
+
+	for(int i = 1; i < num; i++) {
+		if(str[i - 1] == str[i]) continue;
+		cout << str[i] << '\n';
+	}
 
 	return 0;
 }
