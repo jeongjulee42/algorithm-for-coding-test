8d8d68d22f572612fd366819f8f2f65ad225e860
jeongjulee42 <ljj3342@naver.com>
2024-06-19 10:26:28 +0900

    1753

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,21 +2,41 @@
 using namespace std;
 typedef long long ll;
 
-priority_queue<int> pq;
-
+int V, E, K, u, v, w;
+vector<pair<int, int>> adj[20004];
+int dist[20001];
+const int INF = 987654321;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	pq.push(1);
-	pq.push(0);
-	pq.push(3);
-	cout << pq.top() << '\n';
-	pq.pop();
-		cout << pq.top() << '\n';
-	pq.pop();
-		cout << pq.top() << '\n';
-	pq.pop();
+	cin >> V >> E >> K;
+	fill(dist, dist + 20001, INF);
+	for(int i = 0; i < E; i++){
+		cin >> u >> v >> w;
+		adj[u].push_back({w, v});
+	}
+	dist[K] = 0;
+	pq.push({0, K});
+	while(pq.size()){
+		int here = pq.top().second;
+		int here_dist = pq.top().first;
+		pq.pop();
+		if(dist[here] != here_dist) continue;
+		for(auto there : adj[here]){
+			int _dist = there.first;
+			int _there = there.second;
+			if(dist[_there] > dist[here] + _dist){
+				dist[_there] = dist[here] + _dist;
+				pq.push({dist[_there], _there});
+			}
+		}
+	}
+	for(int i = 1; i <= V; i++){
+		if(dist[i] == INF) cout << "INF" << '\n';
+		else cout << dist[i] << '\n';
+	}
 
 	return 0;
 }
