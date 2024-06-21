60f7395834d622a5e1378b2c12fabc7836f75756
jeongjulee42 <ljj3342@naver.com>
2024-06-22 06:27:06 +0900

    11047:

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 typedef long long ll;
 
-int n, k;
-int u = 1, d = 1;
+int n, k, temp, ret;
+int ary[10];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-
 	cin >> n >> k;
-	for(int i = n - k + 1; i <= n; i++){
-		u *= i;
+	for(int i = 0; i < n; i++){
+		cin >> ary[i];
 	}
-	for(int i = 1; i <= k; i++){
-		d *= i;
+	temp = n - 1;
+	for(int i = 0; i < n; i++){
+		if(ary[i] > k){
+			temp = i - 1;
+			break;
+		} else if(ary[i] == k) temp = i;
 	}
-	cout << u / d << '\n';
-
+	while(temp >= 0){
+		ret += (k / ary[temp]);
+		k %= ary[temp];
+		temp--;
+	}
+	cout << ret << '\n';
 	return 0;
 }
