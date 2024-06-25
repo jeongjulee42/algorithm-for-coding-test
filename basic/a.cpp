346f724bd0f290f8afc5ce48fc77b291b97c18a5
jeongjulee42 <ljj3342@naver.com>
2024-06-25 20:47:08 +0900

    7785

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,36 +2,27 @@
 using namespace std;
 typedef long long ll;
 
-
-struct cmp{
-	bool operator()(int a, int b){
-		if(abs(a) > abs(b)) return true;
-		else if(abs(a) < abs(b)) return false;
-		else{
-			if(a > b) return true;
-			else return false;
-		}
-	}
-};
-priority_queue<int, vector<int>, cmp> pq;
-int n, x;
+string name, come;
+int n;
+map<string, int> mp;
+vector<string> ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> x;
-		if(x == 0){
-			if(pq.size()){
-				cout << pq.top() << '\n';
-				pq.pop();
-			} else{
-				cout << 0 << '\n';
-			}
-		}else{
-			pq.push(x);
+		cin >> name >> come;
+		if(come == "enter") mp[name] = 1;
+		else{
+			mp.erase(name);
 		}
+	}
+	for(auto m : mp){
+		ret.push_back(m.first);
+	}
+	for(int i = ret.size() - 1; i >= 0; i--){
+		cout << ret[i] << '\n';
 	}
 
 	return 0;
