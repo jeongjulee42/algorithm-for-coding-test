5174b772d25fc11725675472a1f448b18d84a579
jeongjulee42 <ljj3342@naver.com>
2024-06-26 22:26:58 +0900

    10816

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,27 +2,31 @@
 using namespace std;
 typedef long long ll;
 
-string name, come;
-int n;
-map<string, int> mp;
-vector<string> ret;
+map<int, int> mp;
+int n, m, num;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
 	for(int i = 0; i < n; i++){
-		cin >> name >> come;
-		if(come == "enter") mp[name] = 1;
+		cin >> num;
+		if(mp.find(num) == mp.end()){
+			mp.insert({num, 1});
+		}
 		else{
-			mp.erase(name);
+			mp[num]++;
 		}
 	}
-	for(auto m : mp){
-		ret.push_back(m.first);
-	}
-	for(int i = ret.size() - 1; i >= 0; i--){
-		cout << ret[i] << '\n';
+	cin >> m;
+	for(int i = 0; i < m; i++){
+		cin >> num;
+		if(mp.find(num) == mp.end()){
+			cout << 0 << ' ';
+		}
+		else{
+			cout << mp[num] << ' ';
+		}
 	}
 
 	return 0;
