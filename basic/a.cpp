d9f623b9affd1d140cef6511214a5ff44ee6581c
jeongjulee42 <ljj3342@naver.com>
2024-06-28 16:27:51 +0900

    27433

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,28 +2,18 @@
 using namespace std;
 typedef long long ll;
 
-string s;
-int n, m;
-map<string, int> mp;
-vector<string> ret;
+long long n;
+
+long long go(long long n){
+	if(n <= 1) return 1;
+	return go(n - 1) * n;
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	for(int i = 0; i < n; i++){
-		cin >> s;
-		mp[s] = 0;
-	}
-	for(int i = 0; i < m; i++){
-		cin >> s;
-		if(mp.find(s) != mp.end()){
-			mp[s]++;
-		}
-	}
-	for(auto a : mp) if(a.second != 0) ret.push_back(a.first);
-	cout << ret.size() << '\n';
-	for(auto a : ret) cout << a << '\n';
+	cin >> n;
+	cout << go(n) << '\n';
 
 	return 0;
 }
