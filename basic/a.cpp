f70ae3f14054c1050066a2337b4075af98539fe9
jeongjulee42 <ljj3342@naver.com>
2024-06-29 21:38:46 +0900

    11478

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 typedef long long ll;
 
-long long n;
-
-long long go(long long n){
-	if(n <= 1) return 1;
-	return go(n - 1) * n;
-}
+string s;
+set<string> st;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	cout << go(n) << '\n';
+	cin >> s;
+	for(int i = 0; i < s.size(); i++){
+		string temp = "";
+		for(int j = i; j < s.size(); j++){
+			temp += s[j];
+			cout << temp << '\n';
+			st.insert(temp);
+		}
+	}
+	cout << st.size() << '\n';
 
 	return 0;
 }
