b5782141516c4103ca44a2aab78b853d0bcd3be5
jeongjulee42 <ljj3342@naver.com>
2024-07-01 19:58:21 +0900

    2501

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 typedef long long ll;
 
-int n, m;
+int p, q;
+vector<int> v;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	while(1){
-		cin >> n >> m;
-		if(n == 0 && m == 0) break;
-		if(m % n == 0) cout << "factor" << '\n';
-		else if(n % m == 0) cout << "multiple" << '\n';
-		else cout << "neither" << '\n';
+	cin >> p >> q;
+	for(int i = 1; i <= p; i++){
+		if(p % i == 0) v.push_back(i);
 	}
+	if(v.size() < q - 1) cout << '0' << '\n';
+	else cout << v[q - 1] << '\n'; 
 
 	return 0;
 }
