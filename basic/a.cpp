9b0e41a6a83019c6ff0c0f0d94e186fe32483d4c
jeongjulee42 <ljj3342@naver.com>
2024-07-02 19:54:18 +0900

    1978

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 typedef long long ll;
 
-int p, q;
-vector<int> v;
+bool ary[1001];
+int n, temp, ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> p >> q;
-	for(int i = 1; i <= p; i++){
-		if(p % i == 0) v.push_back(i);
+	cin >> n;
+	ary[1] = true;
+	for(int i = 3; i <= 1000; i++){
+		for(int j = 2; j < i; j++){
+			if(i % j == 0){
+				ary[i] = true;
+				break;
+			}
+		}
 	}
-	if(v.size() < q - 1) cout << '0' << '\n';
-	else cout << v[q - 1] << '\n'; 
-
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		if(!ary[temp]) ret++;
+	}
+	cout << ret << '\n';
 	return 0;
 }
