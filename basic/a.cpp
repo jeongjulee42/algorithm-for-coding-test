37fb16160516f376e9520a0db17def24977a0e08
jeongjulee42 <ljj3342@naver.com>
2024-07-04 17:31:08 +0900

    11653

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,34 +2,21 @@
 using namespace std;
 typedef long long ll;
 
-
-int n, m, minVal = 987654321, ret;
-bool che[10004];
-
-void era(){
-	for(int i = 2; i <= 10001; i++){
-		if(che[i]) continue;
-		for(int j = i * 2; j <= 10001; j += i){
-			che[j] = 1;
-		}
-	}
-}
+vector<int> ret;
+int n;
 
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> m >> n;
-	era();
-	che[1] = 1;
-	for(int i = m; i <= n; i++){
-		if(che[i] == 0){
-			minVal = min(minVal, i);
-			ret += i;
-		}
+	cin >> n;
+	int i = 2;
+	while(n != 1){
+		if(n % i == 0){
+			n /= i;
+			ret.push_back(i);
+		} else i++;
 	}
-	if(ret == 0) cout << -1 << '\n';
-	else cout << ret << '\n' << minVal << '\n';
-	
+	for(int r : ret) cout << r << '\n';
 	return 0;
 }
