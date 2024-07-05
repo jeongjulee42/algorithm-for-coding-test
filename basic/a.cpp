26ffc2a249b1df0392e3ef988927944ceac68c5d
jeongjulee42 <ljj3342@naver.com>
2024-07-05 11:54:52 +0900

    9506

--- basic/a.cpp
+++ basic/a.cpp
@@ -3,20 +3,31 @@ using namespace std;
 typedef long long ll;
 
 vector<int> ret;
-int n;
-
+int n, sum;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	int i = 2;
-	while(n != 1){
-		if(n % i == 0){
-			n /= i;
-			ret.push_back(i);
-		} else i++;
+	while(1){
+		cin >> n;
+		if(n == -1) break;
+		ret.clear();
+		sum = 0;
+		for(int i = 1; i < n; i++){
+			if(n % i == 0) ret.push_back(i);
+		}
+		for(int r : ret) sum += r;
+		if(sum == n){
+			cout << n << " = ";
+			for(int i = 0; i < ret.size() - 1; i++){
+				cout << ret[i] << " + ";
+			}
+			cout << ret[ret.size() - 1] << '\n';
+
+		} else {
+			cout << n << " is NOT perfect." << '\n';
+		}
 	}
-	for(int r : ret) cout << r << '\n';
+
 	return 0;
 }
