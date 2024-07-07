eda6da28017debc90ae5c913cc554f066264d478
jeongjulee42 <ljj3342@naver.com>
2024-07-07 19:28:19 +0900

    10989

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 typedef long long ll;
 
-int n;
+map<int, int> mp;
+int num, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	int temp = n * 2 - 1;
-	for(int i = 1; i <= temp; i++){
-		int j = abs(n - i);
-		for(int k = 1; k <= j; k++) cout << ' ';
-		for(int k = 1; k <= (n - j) * 2 - 1; k++) cout << '*';
-		cout << '\n';
+	cin >> num;
+	for(int i = 0; i < num; i++){
+		cin >> temp;
+		mp[temp]++;
+	}
+	for(auto m : mp){
+		if(!m.second) continue;
+		for(int i = 0; i < m.second; i++){
+			cout << m.first << '\n';
+		}
 	}
 
 	return 0;
