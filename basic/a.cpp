f2720c85593878dfbfd668c2c4cb86cf5d81a71e
jeongjulee42 <ljj3342@naver.com>
2024-07-11 18:20:09 +0900

    2292

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 typedef long long ll;
 
-int n, tot, start = 2;
-//초기 4-0 4개 | 1번 3 5-4 9개 | 2번 5 16-9 25개 | 3번 9 25- 81개 | 
+int n, ret = 1, start = 1, i = 1;
 
+//6 12 18 24 30...
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		start += start - 1;
-		tot = start * start;
+	while(n > start){
+		ret ++;
+		start += i * 6;
+		i++;
 	}
-	cout << tot << '\n';
+	cout << ret << '\n';
 
 	return 0;
 }
