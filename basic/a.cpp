666e4140f3afb7aae27b0ebcd14a725f012c2a52
jeongjulee42 <ljj3342@naver.com>
2024-07-13 13:59:20 +0900

    10870

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,32 +2,20 @@
 using namespace std;
 typedef long long ll;
 
-int n, k;
-queue<int> q;
-queue<int> ret;
+int n;
+int go(int n){
+	if(n <= 2) return 1;
+	return go(n - 1) + go(n - 2);
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> k;
-	for(int i = 1; i <= n; i++) q.push(i);
-	while(q.size()){
-		for(int i = 1; i < k; i++){
-			int temp = q.front();
-			q.pop();
-			q.push(temp);
-		}
-		int temp2 = q.front();
-		q.pop();
-		ret.push(temp2);
+	cin >> n;
+	if(n == 0 || n == 1) cout << n << '\n';
+	else{
+		cout << go(n) << '\n';
 	}
-	cout << '<' << ret.front();
-	ret.pop();
-	while(ret.size()){
-		cout << ", " << ret.front();
-		ret.pop();
-	}
-	cout << '>' << '\n';
 
 	return 0;
 }
