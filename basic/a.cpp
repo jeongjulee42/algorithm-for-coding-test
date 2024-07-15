a094c29fd42b5917c752839cadcf85be1b70e2a2
jeongjulee42 <ljj3342@naver.com>
2024-07-15 21:25:25 +0900

    12789

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,23 +2,40 @@
 using namespace std;
 typedef long long ll;
 
-int gcd(int a, int b){
-	if(a == 0) return b;
-	return gcd(b % a, a);
-}
-int lcm(int a, int b){
-	return (a * b) / gcd(a, b);
-}
-int a, b, n;
+stack<int> stk, temp;
+int ary[1001];
+int n;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		cout << lcm(min(a, b), max(a, b)) << '\n';
+	for(int i = 0; i < n; i++) cin >> ary[i];
+	for(int i = n - 1; i >= 0; i--) stk.push(ary[i]);
+	int start = 1;
+	while(stk.size()){
+		if(stk.top() == start){
+			stk.pop();
+			start++;
+		}else if(temp.size() && temp.top() == start){
+			temp.pop();
+			start++;
+		}else{
+			temp.push(stk.top());
+			stk.pop();
+		}
 	}
+	while(temp.size()){
+		if(temp.top() == start){
+			temp.pop();
+			start++;
+		} else{
+			break;
+		}
+	}
+	if(temp.size()) cout << "Sad" << '\n';
+	else cout << "Nice" << '\n';
+
 
 	return 0;
 }
