f258eb6aba4844e57bd1a0748f9abbd14e9d6f6c
jeongjulee42 <ljj3342@naver.com>
2024-07-19 20:37:25 +0900

    24265

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 typedef long long ll;
 
-int n;
+ll n;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 	cin >> n;
-	cout << n * n << '\n' << 2 << '\n';
+	cout << (n * n - n) / 2 << '\n' << 2 << '\n';
 
 
 	return 0;
