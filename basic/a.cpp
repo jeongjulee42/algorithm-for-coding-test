1c93229c5e24bf032957fc8d3e941f1c99996d5c
jeongjulee42 <ljj3342@naver.com>
2024-07-26 21:59:43 +0900

    13241

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,26 +2,20 @@
 using namespace std;
 typedef long long ll;
 
-int n;
+ll n, m;
+ll gcd(ll a, ll b){
+	if(a == 0) return b;
+	return gcd(b % a, a);
+}
+ll lcm(ll a, ll b){
+	return (a * b) / gcd(a, b);
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 1; i < n; i++){
-		int temp = 0;
-		int num = i;
-		while(num != 0){
-			temp += num % 10;
-			num /= 10;
-		}
-		temp += i;
-		if(temp == n){
-			cout << i << '\n';
-			exit(0);
-		}
-	}
-	cout << 0 << '\n';
-	
+	cin >> n >> m;
+	cout << lcm(n, m) << '\n';
+
 	return 0;
 }
