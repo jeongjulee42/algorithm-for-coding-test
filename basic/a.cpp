af4ac9020cf7201ea58fd49fa7d789c63cd4b7c2
jeongjulee42 <ljj3342@naver.com>
2024-07-29 19:58:35 +0900

    11659

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,26 +2,20 @@
 using namespace std;
 typedef long long ll;
 
-int n;
-map<string, bool> mp;
-string a, b;
+int ary[100001], n, m, temp, a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> a >> b;
-		if(a == "ChongChong"){
-			mp.insert({b, true});
-		}else if (b == "ChongChong"){
-			mp.insert({a, true});
-		}else if(mp.find(a) != mp.end()){
-			mp.insert({b, true});
-		}else if(mp.find(b) != mp.end()){
-			mp.insert({a, true});
-		}
+	cin >> n >> m;
+	for(int i = 1; i <= n; i++){
+		cin >> temp;
+		ary[i] = ary[i - 1] + temp;
 	}
-	cout << mp.size() + 1 << '\n';
+	for(int i = 0; i < m; i++){
+		cin >> a >> b;
+		cout << ary[b] - ary[a - 1] << '\n';
+	}
+
 	return 0;
 }
