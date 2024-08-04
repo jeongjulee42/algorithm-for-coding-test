132b6ef447f93448905a711f2d25a68c4306ed04
jeongjulee42 <ljj3342@naver.com>
2024-08-04 21:10:33 +0900

    2667

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,25 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll n, m, l, k;
-ll gcd(ll a, ll b){
-	if(a == 0) return b;
-	return gcd(b % a, a);
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int n;
+int ary[25][25];
+bool visited[25][25];
+vector<int> ret;
+
+int go(int y, int x){
+	visited[y][x] = 1;
+	int t = 0;
+	for(int i = 0; i < 4; i++){
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
+		if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+		if(visited[ny][nx]) continue;
+		if(!ary[ny][nx]) continue;
+		t += go(ny, nx);
+	}
+	return t + 1;
 }
-ll lcm(ll a, ll b){
-	return (a * b) / gcd(a, b);
-}
+
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	cin >> l >> k;
-	int tp = n * k + m * l;
-	int bt = m * k;
-	int div = gcd(tp, bt);
-	cout << tp / div << ' ' << bt / div << '\n';
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		string str = "";
+		cin >> str;
+		for(int j = 0; j < n; j++){
+			ary[i][j] = str[j] - '0';
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(visited[i][j]) continue;
+			if(ary[i][j] == 0) continue;
+			int temp = go(i, j);
+			ret.push_back(temp);
+		}
+	}
+	sort(ret.begin(), ret.end());
+	cout << ret.size() << '\n';
+	for(int r : ret) cout << r << '\n';
 
 	return 0;
 }
