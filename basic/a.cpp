39eca2e2d2442d8d0d93837f2cbcca17ee1cb5c5
jeongjulee42 <ljj3342@naver.com>
2024-08-06 17:13:41 +0900

    18870

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,17 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int x, start = 1, n;
-// 2-1, 3-2, 4-3, 5-4,
+vector<pair<int, int>> v;
+int ret[1000001];
+int n, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> x;
-	while(n < x){
-		n += start;
-		start++;
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		v.push_back({temp, i});
 	}
-	cout << "n:" << n << " start:" << start << '\n';
+	sort(v.begin(), v.end());
+	int cnt = 0;
+	for(int i = 0; i < n; i++){
+		ret[v[i].second] = cnt;
+		if(i < n - 1 && v[i].first == v[i + 1].first) continue;
+		cnt++;
+	}
+	for(int i = 0; i < n; i++) cout << ret[i] << ' ';
+	cout << '\n';
+
 	return 0;
 }
