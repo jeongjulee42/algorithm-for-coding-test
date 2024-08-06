6d7ebae9a13fc81b5da7a6d0320d3b2ba81f4eb6
jeongjulee42 <ljj3342@naver.com>
2024-08-06 16:27:37 +0900

    17299

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,29 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[1000001], num[1000001], n;
-stack<int> stk;
-vector<int> ret;
+int x, start = 1, n;
+// 2-1, 3-2, 4-3, 5-4,
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> ary[i];
-		num[ary[i]]++;
+	cin >> x;
+	while(n < x){
+		n += start;
+		start++;
 	}
-	for(int i = n - 1; i >= 0; i--){
-		int t = ary[i];
-		while(stk.size() && num[stk.top()] <= num[t]){
-			stk.pop();
-		}
-		if(stk.size() == 0) ret.push_back(-1);
-		else{
-			ret.push_back(stk.top());
-		}
-		stk.push(t);
-	}
-	for(int i = ret.size() - 1; i >= 0; i--) cout << ret[i] << ' ';
+	cout << "n:" << n << " start:" << start << '\n';
 	return 0;
 }
