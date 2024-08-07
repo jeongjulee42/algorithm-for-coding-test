c2da7e3667859885dcb881759f70ef7796ded567
jeongjulee42 <ljj3342@naver.com>
2024-08-07 21:31:22 +0900

    11725

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> v;
-int ret[1000001];
-int n, temp;
+int n;
+int ret[100001];
+vector<int> ary[100001];
+int a, b;
+
+void go(int from){
+	for(int i = 0; i < ary[from].size(); i++){
+		if(ret[ary[from][i]] != 0) continue;
+		ret[ary[from][i]] = from;
+		go(ary[from][i]);
+	}
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		cin >> temp;
-		v.push_back({temp, i});
+	for(int i = 1; i < n; i++){
+		cin >> a >> b;
+		ary[a].push_back(b);
+		ary[b].push_back(a);
 	}
-	sort(v.begin(), v.end());
-	int cnt = 0;
-	for(int i = 0; i < n; i++){
-		ret[v[i].second] = cnt;
-		if(i < n - 1 && v[i].first == v[i + 1].first) continue;
-		cnt++;
+	go(1);
+	for(int i = 2; i <= n; i++){
+		cout << ret[i] << '\n';
 	}
-	for(int i = 0; i < n; i++) cout << ret[i] << ' ';
-	cout << '\n';
 
 	return 0;
 }
