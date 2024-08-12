5d390f8de2a78990abda43e8550525e4f7a480d6
jeongjulee42 <ljj3342@naver.com>
2024-08-12 21:04:54 +0900

    15650

--- basic/a.cpp
+++ basic/a.cpp
@@ -5,28 +5,25 @@ int n, m;
 bool visited[9];
 vector<int> r;
 
-void go(){
+void go(int num){
 	if(r.size() == m){
 		for(int i = 0; i < r.size(); i++ ) cout << r[i] << ' ';
 		cout << '\n';
 		return;
 	}
-	for(int i = 1; i <= n; i++){
-		if(visited[i]) continue;
+	for(int i = num; i <= n; i++){
 		r.push_back(i);
-		//visited[i] = 1;
-		go();
+		go(i + 1);
 		r.pop_back();
-		//visited[i] = 0;
 	}
-	
 }
+
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n >> m;
-	go();
+	go(1);
 
 	return 0;
 }
