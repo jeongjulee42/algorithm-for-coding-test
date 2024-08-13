bf42ed28142101690c418542a16e330907f5a9b7
jeongjulee42 <ljj3342@naver.com>
2024-08-13 18:41:58 +0900

    2485

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,29 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-bool visited[9];
-vector<int> r;
+int n, start, g, temp;
 
-void go(int num){
-	if(r.size() == m){
-		for(int i = 0; i < r.size(); i++ ) cout << r[i] << ' ';
-		cout << '\n';
-		return;
-	}
-	for(int i = num; i <= n; i++){
-		r.push_back(i);
-		go(i + 1);
-		r.pop_back();
-	}
+int gcd(int a, int b){
+	if(a == 0) return b;
+	return gcd(b % a, a);
 }
-
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	go(1);
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> temp;
+		if(i == 0){ 
+			start = temp;
+		}else if(i == 0){
+			g = temp - start;
+		}
+		else{
+			g = gcd(g, temp - start);
+		}
+	}
+
+	cout << (temp - start) / g - n + 1 << '\n';
+	
 
 	return 0;
 }
