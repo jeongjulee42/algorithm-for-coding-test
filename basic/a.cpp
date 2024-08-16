d4a20ff30684bd8b40b5e2382740e80ea36e4d8b
jeongjulee42 <ljj3342@naver.com>
2024-08-16 20:54:00 +0900

    24444

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,23 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> adj[100001];
-int visited[100001];
-int n, a, b, m, r;
+queue<int> q;
 int ret[100001];
-int num = 1;
-
-void dfs(int here){
-	visited[here] = 1;
-	ret[here] = num;
-	for(int i = adj[here].size() - 1; i >= 0; i--){
-		int there = adj[here][i];
-		if(visited[there]) continue;
-		num++;
-		dfs(there);
-	}
-	return;
-}
+bool visited[100001];
+vector<int> v[100001];
+int n, num = 1, m, r, a, b;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
@@ -25,13 +13,29 @@ int main(){
 	cin >> n >> m >> r;
 	for(int i = 0; i < m; i++){
 		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+		v[a].push_back(b);
+		v[b].push_back(a);
 	}
 	for(int i = 1; i <= n; i++){
-		sort(adj[i].begin(), adj[i].end());
+		if(v[i].size() <= 0) continue;
+		sort(v[i].begin(), v[i].end());
 	}
-	dfs(r);
+	q.push(r);
+	visited[r] = 1;
+	ret[r] = num;
+	while(q.size()){
+		int there = q.front();
+		q.pop();
+		for(int i : v[there]){
+			if(!visited[i]) {
+				num++;
+				ret[i] = num;
+				visited[i] = 1;
+				q.push(i);
+			}
+		}
+	}
+
 	for(int i = 1; i <= n; i++) cout << ret[i] << '\n';
 
 	return 0;
