4d7dccb3cd0f16dda309d10a1e48f5247d036a8f
jeongjulee42 <ljj3342@naver.com>
2024-08-19 22:03:53 +0900

    1697

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,50 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m, v, a, b;
-bool visited[10001];
-vector<int> ary[10001];
-
-void dfs(int here){
-	visited[here] = 1;
-	cout << here << ' ';
-	for(int i : ary[here]){
-		if(visited[i]) continue;
-		dfs(i);
-	}
-}
-
-void bfs(int start){
-	queue<int> q;
-	visited[start] = 1;
-	q.push(start);
-	while(q.size()){
-		int next = q.front();
-		q.pop();
-		cout << next << ' ';
-		for(int i : ary[next]){
-			if(visited[i]) continue;
-			visited[i] = 1;
-			q.push(i);
-		}
-	}
-}
+int n, k;
+queue<int> q;
+int visited[100001];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m >> v;
-	for(int i = 0; i < m; i++){
-		cin >> a >> b;
-		ary[a].push_back(b);
-		ary[b].push_back(a);
+	cin >> n >> k;
+	visited[n] = 1;
+	q.push(n);
+	while(q.size()){
+		int num = q.front();
+		q.pop();
+		if(num - 1 >= 0 && !visited[num - 1]){
+			visited[num - 1] = visited[num] + 1;
+			if(num - 1 == k)break;
+			q.push(num - 1);
+		}
+		if(num + 1 <= 100000 && !visited[num + 1]){
+			visited[num + 1] = visited[num] + 1;
+			if(num + 1 == k)break;
+			q.push(num + 1);
+		}
+		if(num * 2 <= 100000 && !visited[num * 2]){
+			visited[num * 2] = visited[num] + 1;
+			if(num * 2 == k)break;
+			q.push(num * 2);
+		}
 	}
-	for(int i = 1; i <= n; i++) sort(ary[i].begin(), ary[i].end());
-	dfs(v);
-	memset(visited, 0, sizeof(visited));
-	cout << '\n';
-	bfs(v);
-
-
+	cout << visited[k] - 1 << '\n';
 	return 0;
 }
