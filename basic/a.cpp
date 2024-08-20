a3288dc2bebf25391abce152de8a7756677bf874
jeongjulee42 <ljj3342@naver.com>
2024-08-20 19:20:04 +0900

    7562

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,35 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, k;
-queue<int> q;
-int visited[100001];
+int dy[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int dx[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+int n, l, sy, sx, ey, ex;
+queue<pair<int, int>> q;
+int visited[300][300];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> k;
-	visited[n] = 1;
-	q.push(n);
-	while(q.size()){
-		int num = q.front();
-		q.pop();
-		if(num - 1 >= 0 && !visited[num - 1]){
-			visited[num - 1] = visited[num] + 1;
-			if(num - 1 == k)break;
-			q.push(num - 1);
-		}
-		if(num + 1 <= 100000 && !visited[num + 1]){
-			visited[num + 1] = visited[num] + 1;
-			if(num + 1 == k)break;
-			q.push(num + 1);
-		}
-		if(num * 2 <= 100000 && !visited[num * 2]){
-			visited[num * 2] = visited[num] + 1;
-			if(num * 2 == k)break;
-			q.push(num * 2);
+	cin >> n;
+	for(int k = 0; k < n; k++){
+		memset(visited, 0, sizeof(visited));
+		cin >> l;
+		cin >> sy >> sx;
+		cin >> ey >> ex;
+		visited[sy][sx] = 1;
+		q.push({sy, sx});
+		while(q.size()){
+			int ty = q.front().first;
+			int tx = q.front().second;
+			q.pop();
+			for(int i = 0; i < 8; i++){
+				int ny = ty + dy[i];
+				int nx = tx + dx[i];
+				if(ny < 0 || nx < 0 || ny >= l || nx >= l) continue;
+				if(visited[ny][nx]) continue;
+				visited[ny][nx] = visited[ty][tx] + 1;
+				q.push({ny, nx});
+			}
+			for(int i = 0; i < l; i++){
+				for(int j = 0; j < l; j++){
+					cout << visited[i][j] << ' ';
+				} cout << '\n';
+			}
+			cout << "----" << '\n';
+			if(visited[ey][ex]){ 
+				cout << visited[ey][ex] - 1 << '\n';
+				while(q.size()){
+					q.pop();
+				}
+				break;
+			}
 		}
 	}
-	cout << visited[k] - 1 << '\n';
+
 	return 0;
 }
