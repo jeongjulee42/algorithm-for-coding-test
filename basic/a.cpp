c1946226ea66cbe2944971ba0e640882ebf447c3
jeongjulee42 <ljj3342@naver.com>
2024-08-21 20:59:39 +0900

    7576

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,44 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dy[] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dx[] = {-1, -2, -2, -1, 1, 2, 2, 1};
-int n, l, sy, sx, ey, ex;
+int ary[1004][1004];
+int visited[1004][1004];
+int n, m, ret;
 queue<pair<int, int>> q;
-int visited[300][300];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	for(int k = 0; k < n; k++){
-		memset(visited, 0, sizeof(visited));
-		cin >> l;
-		cin >> sy >> sx;
-		cin >> ey >> ex;
-		visited[sy][sx] = 1;
-		q.push({sy, sx});
-		while(q.size()){
-			int ty = q.front().first;
-			int tx = q.front().second;
-			q.pop();
-			for(int i = 0; i < 8; i++){
-				int ny = ty + dy[i];
-				int nx = tx + dx[i];
-				if(ny < 0 || nx < 0 || ny >= l || nx >= l) continue;
-				if(visited[ny][nx]) continue;
-				visited[ny][nx] = visited[ty][tx] + 1;
-				q.push({ny, nx});
+	cin >> m >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cin >> ary[i][j];
+			if(ary[i][j] == 1) {
+				visited[i][j] = 1;
+				q.push({i, j});
 			}
-			if(visited[ey][ex]){ 
-				cout << visited[ey][ex] - 1 << '\n';
-				while(q.size()){
-					q.pop();
-				}
-				break;
-			}
+			if(ary[i][j] == -1) visited[i][j] = -1;
 		}
 	}
-
+	while(q.size()){
+		int _y = q.front().first;
+		int _x = q.front().second;
+		q.pop();
+		for(int i = 0; i < 4; i++){
+			int ny = _y + dy[i];
+			int nx = _x + dx[i];
+			if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if(visited[ny][nx]) continue;
+			if(ary[ny][nx] == -1) continue;
+			q.push({ny, nx});
+			visited[ny][nx] = visited[_y][_x] + 1;
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(visited[i][j] == 0){
+				cout << -1 << '\n';
+				exit(0);
+			}
+			ret = max(ret, visited[i][j]);
+		}
+	}
+	cout << ret - 1 << '\n';
 	return 0;
 }
