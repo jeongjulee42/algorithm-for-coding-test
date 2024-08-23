66610d62f9725e824c6be980dc6c8ff9f74097b8
jeongjulee42 <ljj3342@naver.com>
2024-08-23 21:02:00 +0900

    24416

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,31 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-int n, m, t;
-int ary[100004];
-
-int bs(int target, int startn, int endn){
-	while(startn <= endn){
-		int mid = (startn + endn) / 2;
-		if(ary[mid] == target) return 1;
-		else if(ary[mid] > target) endn = mid - 1;
-		else startn = mid + 1;
-	}
-	return 0;
-}
+int ary[41];
+int n;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++) cin >> ary[i];
-	sort(ary, ary + n);
-	cin >> m;
-	for(int i = 0; i < m; i++){
-		cin >> t;
-		cout << bs(t, 0, n - 1) << '\n';
+	ary[1] = 1; ary[2] = 1;
+	for(int i = 3; i <= 40; i++){
+		ary[i] = ary[i-1] + ary[i-2];
 	}
+	cout << ary[n] << ' ' << n - 2;
 
 	return 0;
 }
