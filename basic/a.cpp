4ffd76a531d1e6b3650308ffc2bd95f6c9f6ab34
jeongjulee42 <ljj3342@naver.com>
2024-08-27 19:44:28 +0900

    1149

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[504][504];
-int dp[504][504];
-int n, ret;
+int ary[1000][3];
+int dp[1000][3];
+int n, ret = 987654321;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= i; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < 3; j++){
 			cin >> ary[i][j];
 		}
 	}
-	dp[1][1] = ary[1][1];
-	for(int i = 2; i <= n; i++){
-		for(int j = 1; j <= i; j++){
-			dp[i][j] = ary[i][j] + max(dp[i-1][j], dp[i-1][j-1]);
-		}
+	dp[0][0] = ary[0][0];
+	dp[0][1] = ary[0][1];
+	dp[0][2] = ary[0][2];
+	for(int i = 1; i < n; i++){
+		dp[i][0] = ary[i][0] + min(dp[i-1][1], dp[i-1][2]);
+		dp[i][1] = ary[i][1] + min(dp[i-1][0], dp[i-1][2]);
+		dp[i][2] = ary[i][2] + min(dp[i-1][0], dp[i-1][1]);
 	}
-	for(int i = 1; i <= n; i++){
-		ret = max(dp[n][i], ret);
+	for(int i = 0; i < 3; i++){
+		ret = min(ret, dp[n-1][i]);
 	}
 	cout << ret << '\n';
 	return 0;
