4f8979028e5e8af5d97f11f2583c8e3026626cb2
jeongjulee42 <ljj3342@naver.com>
2024-08-28 19:24:38 +0900

    2579

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,30 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ary[1000][3];
-int dp[1000][3];
-int n, ret = 987654321;
+int n, stair[304];
+pair<int, int> ary[304][2];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < 3; j++){
-			cin >> ary[i][j];
+	for(int i = 1; i <= n; i++) cin >> stair[i];
+	ary[1][0].first = stair[1]; ary[1][0].second = 1;
+	ary[1][1].first = 0; ary[1][1].second = 0;
+	for(int i = 2; i <= n; i++){
+		if(ary[i-1][0].second == 2) {
+			ary[i][0].first = ary[i-1][1].first + stair[i];
+			ary[i][0].second = ary[i-1][1].second + 1;
+		}else if(ary[i-1][0].first > ary[i-1][1].first){
+			ary[i][0].first = ary[i-1][0].first + stair[i];
+			ary[i][0].second = ary[i-1][0].second + 1;
+		}else{
+			ary[i][0].first = ary[i-1][1].first + stair[i];
+			ary[i][0].second = ary[i-1][1].second + 1;
 		}
+		ary[i][1].first = max(ary[i-2][0].first, ary[i-2][1].first) + stair[i];
+		ary[i][1].second = 1;
 	}
-	dp[0][0] = ary[0][0];
-	dp[0][1] = ary[0][1];
-	dp[0][2] = ary[0][2];
-	for(int i = 1; i < n; i++){
-		dp[i][0] = ary[i][0] + min(dp[i-1][1], dp[i-1][2]);
-		dp[i][1] = ary[i][1] + min(dp[i-1][0], dp[i-1][2]);
-		dp[i][2] = ary[i][2] + min(dp[i-1][0], dp[i-1][1]);
-	}
-	for(int i = 0; i < 3; i++){
-		ret = min(ret, dp[n-1][i]);
-	}
-	cout << ret << '\n';
+	cout << max(ary[n][0].first, ary[n][1].first) << '\n';
+
 	return 0;
 }
