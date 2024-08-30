914291614e1cfa8d9aa511bc685c4a686602f2fb
jeongjulee42 <ljj3342@naver.com>
2024-08-30 19:21:18 +0900

    1463

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,34 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int dp[102][102][102];
+int dp[1000001];
+int n;
 
-int w(int a, int b, int c){
-	if(a <= 0 || b <= 0 || c <= 0) return 1;
-	if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
-	if(a < b && b < c){
-		if(!dp[a][b][c-1]) dp[a][b][c-1] = w(a, b, c-1);
-		if(!dp[a][b-1][c-1]) dp[a][b-1][c-1] = w(a, b-1, c-1);
-		if(!dp[a][b-1][c]) dp[a][b-1][c] = w(a, b-1, c);
-		return dp[a][b][c-1] + dp[a][b-1][c-1] - dp[a][b-1][c];
+void go(){
+	queue<int> q;
+	q.push(1);
+	while(q.size()){
+		int num = q.front();
+		q.pop();
+		if(num + 1 <= 1000000 && !dp[num + 1]){
+			dp[num + 1] = dp[num] + 1;
+			q.push(num + 1);
+		}
+		if(num * 2 <= 1000000 && !dp[num * 2]){
+			dp[num * 2] = dp[num] + 1;
+			q.push(num * 2);
+		}
+		if(num * 3 <= 1000000 && !dp[num * 3]){
+			dp[num * 3] = dp[num] + 1;
+			q.push(num * 3);
+		}
 	}
-	if(!dp[a-1][b][c]) dp[a-1][b][c] = w(a-1, b, c);
-	if(!dp[a-1][b-1][c]) dp[a-1][b-1][c] = w(a-1, b-1, c);
-	if(!dp[a-1][b][c-1]) dp[a-1][b][c-1] = w(a-1, b, c-1);
-	if(!dp[a-1][b-1][c-1]) dp[a-1][b-1][c-1] = w(a-1, b-1, c-1);
-	return dp[a-1][b][c] + dp[a-1][b-1][c] + dp[a-1][b][c-1] - dp[a-1][b-1][c-1];
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	
-	while(1){
-		int a = 0, b = 0, c = 0;
-		cin >> a >> b >> c;
-		if(a == -1 && b == -1 && c == -1) break;
-		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
-	}
+	cin >> n;
+	go();
+	cout << dp[n] << '\n';
+
 
 	return 0;
 }
