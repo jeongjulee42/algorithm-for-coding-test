31c2a37fe337cdf7e797c931ba3914d52dfde8f7
jeongjulee42 <ljj3342@naver.com>
2024-09-01 19:53:56 +0900

    4779

--- basic/a.cpp
+++ basic/a.cpp
@@ -3,37 +3,28 @@ using namespace std;
 
 int n;
 
-void go(vector<vector<char>> &adj, int num, int y, int x){
+void go(vector<char> & v, int s, int num){
 	if(num == 1) return;
-	int nextNum = num / 3;
-	int sy = y + nextNum;
-	int sx = x + nextNum;
-	int ey = sy + nextNum;
-	int ex = sx + nextNum;
-	for(int i = sy; i < ey; i++){
-		for(int j = sx; j < ex; j++){
-			adj[i][j] = ' ';
-		}
+	int sx = s + (num / 3);
+	for(int i = sx; i < sx + (num / 3); i++){
+		v[i] = ' ';
 	}
-	for(int i = y; i < y + num; i += nextNum){
-		for(int j = x; j < x + num; j += nextNum){
-			go(adj, nextNum, i, j);
-		}
+	for(int i = s; i < s + num; i += (num / 3)){
+		go(v, i, num/3);
 	}
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
-	vector<vector<char>> adj(n, vector<char>(n, '*'));
-	
-	go(adj, n, 0, 0);
+	while(cin >> n){
+		int num = pow(3, n);
+		vector<char> adj(num, '-');
 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cout << adj[i][j];
-		}cout << '\n';
+		go(adj, 0, num);
+
+		for(int i = 0; i < num; i++) cout << adj[i];
+		cout << '\n';
 	}
 	
 	return 0;
