8e938661f41e116c440c44eeeaa34c4a4f1b12ec
jeongjulee42 <ljj3342@naver.com>
2024-09-02 19:41:05 +0900

    1806

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,31 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const ll MAXVAL = 987654321;
 
-int n;
-
-void go(vector<char> & v, int s, int num){
-	if(num == 1) return;
-	int sx = s + (num / 3);
-	for(int i = sx; i < sx + (num / 3); i++){
-		v[i] = ' ';
-	}
-	for(int i = s; i < s + num; i += (num / 3)){
-		go(v, i, num/3);
-	}
-}
+ll n, s, sum, ret = MAXVAL, l, r = 1;
+ll ary[100001];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	while(cin >> n){
-		int num = pow(3, n);
-		vector<char> adj(num, '-');
-
-		go(adj, 0, num);
-
-		for(int i = 0; i < num; i++) cout << adj[i];
-		cout << '\n';
+	cin >> n >> s;
+	for(int i = 0; i < n; i++) {
+		cin >> ary[i];
+		if(ary[i] >= s) ret = 1;
 	}
-	
+	sum = ary[0] + ary[1];
+	while(r != n){
+		if(sum >= s){
+			ret = min(ret, r - l + 1);
+			sum -= ary[l];
+			l++;
+		} else{
+			r++;
+			sum += ary[r];
+		}
+	}
+	if(ret == MAXVAL) cout << 0 << '\n';
+	else cout << ret << '\n';
+
 	return 0;
 }
