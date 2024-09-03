036cb5622926e9ce25a664d6f2ba1b79f1dd9e38
jeongjulee42 <ljj3342@naver.com>
2024-09-03 20:00:01 +0900

    1991

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,32 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll MAXVAL = 987654321;
 
-ll n, s, sum, ret = MAXVAL, l, r = 1;
-ll ary[100001];
+
+char a, b, c;
+int n;
+char ary[27][3];
+
+void prego(int num){
+	cout << ary[num][0];
+	for(int i = 1; i <= 2; i++){
+		if(ary[num][i] == '.') continue;
+		prego(int(ary[num][i] - 'A' + 1));
+	}
+}
+
+void midgo(int num){
+	if(ary[num][1] != '.'){
+		midgo(ary[num][1] - 'A' + 1);
+	}
+	cout << ary[num][0];
+	if(ary[num][2] != '.'){
+		midgo(ary[num][2] - 'A' + 1);
+	}
+}
+
+void surgo(int num){
+	if(ary[num][1] != '.'){
+		surgo(ary[num][1] - 'A' + 1);
+	}
+	if(ary[num][2] != '.'){
+		surgo(ary[num][2] - 'A' + 1);
+	}
+	cout << ary[num][0];
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> s;
-	for(int i = 0; i < n; i++) {
-		cin >> ary[i];
-		if(ary[i] >= s) ret = 1;
+	cin >> n;
+	for(int i = 1; i <= n; i++){
+		cin >> a >> b >> c;
+		ary[a - 'A' + 1][0] = a;
+		ary[a - 'A' + 1][1] = b;
+		ary[a - 'A' + 1][2] = c;
 	}
-	sum = ary[0] + ary[1];
-	while(r != n){
-		if(sum >= s){
-			ret = min(ret, r - l + 1);
-			sum -= ary[l];
-			l++;
-		} else{
-			r++;
-			sum += ary[r];
-		}
-	}
-	if(ret == MAXVAL) cout << 0 << '\n';
-	else cout << ret << '\n';
+	prego(1);
+	cout << '\n';
+	midgo(1);
+	cout << '\n';
+	surgo(1);
 
 	return 0;
 }
