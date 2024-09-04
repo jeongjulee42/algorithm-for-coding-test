f3782fada9370bf46efbcd5f5cbaa4f73ec5ee57
jeongjulee42 <ljj3342@naver.com>
2024-09-04 20:28:33 +0900

    2630

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,54 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-char a, b, c;
 int n;
-char ary[27][3];
+int ary[128][128];
+int white, blue;
 
-void prego(int num){
-	cout << ary[num][0];
-	for(int i = 1; i <= 2; i++){
-		if(ary[num][i] == '.') continue;
-		prego(int(ary[num][i] - 'A' + 1));
+void go(int y, int x, int num){
+	int temp = 0;
+	for(int i = y; i < y + num; i++){
+		for(int j = x; j < x + num; j++){
+			temp += ary[i][j];
+		}
 	}
-}
-
-void midgo(int num){
-	if(ary[num][1] != '.'){
-		midgo(ary[num][1] - 'A' + 1);
+	if(temp == num * num){
+		blue++;
+	}else if(temp == 0){
+		white++;
+	}else{
+		for(int i = y; i < y + num; i += num / 2){
+			for(int j = x; j < x + num; j += num / 2){
+				go(i, j, num / 2);
+			}
+		}
 	}
-	cout << ary[num][0];
-	if(ary[num][2] != '.'){
-		midgo(ary[num][2] - 'A' + 1);
-	}
-}
-
-void surgo(int num){
-	if(ary[num][1] != '.'){
-		surgo(ary[num][1] - 'A' + 1);
-	}
-	if(ary[num][2] != '.'){
-		surgo(ary[num][2] - 'A' + 1);
-	}
-	cout << ary[num][0];
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 1; i <= n; i++){
-		cin >> a >> b >> c;
-		ary[a - 'A' + 1][0] = a;
-		ary[a - 'A' + 1][1] = b;
-		ary[a - 'A' + 1][2] = c;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++) cin >> ary[i][j];
 	}
-	prego(1);
-	cout << '\n';
-	midgo(1);
-	cout << '\n';
-	surgo(1);
+	go(0, 0, n);
+	cout << white << '\n' << blue << '\n';
 
 	return 0;
 }
