1f983541e050db8029823bb23d463f7e16ee5cb1
jeongjulee42 <ljj3342@naver.com>
2024-09-07 20:40:54 +0900

    2869

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,28 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string str;
-int ary[200004][26];
-int n, s, e;
-char c;
+int a, b, v, ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
+    cin >> a >> b >> v;
+    ret = (v-a) / (a - b);
+    int t = (v-a) % (a - b);
+    if(t == 0) cout << ret + 1 << '\n';
+    else cout << ret + 2 << '\n';
 
-
-
-    cin >> str;
-    str = ' ' + str;
-    for(int i = 1; i < str.size(); i++){
-        for(int j = 0; j < 26; j++) ary[i][j] = ary[i - 1][j];
-        ary[i][int(str[i] - 'a')]++;
-    }
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> c >> s >> e;
-        cout << ary[e + 1][int(c - 'a')] - ary[s][int(c - 'a')] << '\n';
-    }
 
 	return 0;
 }
