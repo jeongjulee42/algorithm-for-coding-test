225bd6f9019bdce97e71c5f31fa30b91a5415e16
jeongjulee42 <ljj3342@naver.com>
2024-09-08 20:55:41 +0900

    11660

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,17 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int a, b, v, ret;
+int n, m, sx, sy, ex, ey;
+int ary[1025][1025];
+int adj[1025][1025];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> a >> b >> v;
-    ret = (v-a) / (a - b);
-    int t = (v-a) % (a - b);
-    if(t == 0) cout << ret + 1 << '\n';
-    else cout << ret + 2 << '\n';
-
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> ary[i][j];
+            adj[i][j] = ary[i][j] + adj[i-1][j] + adj[i][j-1] - adj[i-1][j-1];
+        }
+    }
+    for(int i = 0; i < m; i++){
+        cin >> sy >> sx >> ey >> ex;
+        cout << adj[ey][ex] - adj[ey][sx - 1] - adj[sy-1][ex] + adj[sy-1][sx-1] << '\n';
+    }
 
 	return 0;
 }
