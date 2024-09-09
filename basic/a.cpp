806ffa36c6ff15491c5d95168841e11d8bef3862
jeongjulee42 <ljj3342@naver.com>
2024-09-09 19:42:57 +0900

    13909

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m, sx, sy, ex, ey;
-int ary[1025][1025];
-int adj[1025][1025];
+typedef long long ll;
+ll n, t = 1;
+int ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            cin >> ary[i][j];
-            adj[i][j] = ary[i][j] + adj[i-1][j] + adj[i][j-1] - adj[i-1][j-1];
-        }
+    cin >> n;
+    while(t*t <= n){
+        ret++;
+        t++;
     }
-    for(int i = 0; i < m; i++){
-        cin >> sy >> sx >> ey >> ex;
-        cout << adj[ey][ex] - adj[ey][sx - 1] - adj[sy-1][ex] + adj[sy-1][sx-1] << '\n';
-    }
+    cout << ret << '\n';
 
 	return 0;
 }
