faf15a1b12894151a0d8e18f59cd5b0da6560470
jeongjulee42 <ljj3342@naver.com>
2024-09-11 06:59:35 +0900

    1018

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,33 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool che[1000001];
-int t, n;
-
-void go(){
-    che[1] = 1;
-    for(int i = 2; i <= 1000000; i++){
-        if(che[i]) continue;
-        for(int j = i * 2; j <= 1000000; j += i){
-            che[j] = 1;
-        }
-    }
-}
+int n, m;
+string _a = "WBWBWBWB";
+string _b = "BWBWBWBW";
+char ary[51][51];
+char a[8][8];
+char b[8][8];
+int ret = 987654321;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    go();
-    cin >> t;
-    for(int z = 0; z < t; z++){
-        cin >> n;
-        int ret = 0;
-        for(int i = 2; i <= n / 2; i++){
-            if(che[i]) continue;
-            if(che[i] == 0 && che[n - i] == 0) ret++;
+    for(int i = 0; i < 8; i++){
+        if(i % 2 == 0){
+            for(int j = 0; j < 8; j++){
+                a[i][j] = _a[j];
+            }
+        }else {
+            for(int j = 0; j < 8; j++){
+                a[i][j] = _b[j];
+            }
         }
-        cout << ret << '\n';
+    }
+    for(int i = 0; i < 8; i++){
+        if(i % 2 == 0){
+            for(int j = 0; j < 8; j++){
+                b[i][j] = _b[j];
+            }
+        }else {
+            for(int j = 0; j < 8; j++){
+                b[i][j] = _a[j];
+            }
+        }
     }
 
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++) cin >> ary[i][j];
+    }
+
+    for(int i = 0; i <= n - 8; i++){
+        for(int j = 0; j <= m - 8; j++){
+            int temp = 0;
+            for(int k = 0; k < 8; k++){
+                for(int l = 0; l < 8; l++){
+                    if(ary[i + k][j + l] != a[k][l]) temp++;
+                }
+            }
+            ret = min(ret, temp);
+            temp = 0;
+            for(int k = 0; k < 8; k++){
+                for(int l = 0; l < 8; l++){
+                    if(ary[i + k][j + l] != b[k][l]) temp++;
+                }
+            }
+            ret = min(ret, temp);
+        }
+    }
+    cout << ret << '\n';
 	return 0;
 }
