32f07cbb613a63605bd2a0dbb4a6d7adfbd0c31f
jeongjulee42 <ljj3342@naver.com>
2024-09-12 16:41:47 +0900

    9663

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,63 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-string _a = "WBWBWBWB";
-string _b = "BWBWBWBW";
-char ary[51][51];
-char a[8][8];
-char b[8][8];
-int ret = 987654321;
+vector<pair<int, int>> v;
+int n, ret;
+
+//검사 - 상, 하, 대각선
+bool check(int y, int x){
+    for(int i = 0; i < v.size(); i++){
+        int vy = v[i].first;
+        int vx = v[i].second;
+        if(y == vy || x == vx || (abs(y - vy) == abs(x - vx))) return false;
+    }
+    return true;
+}
+
+void go(int y, int x){
+    if(!check(y, x)) return;
+    v.push_back({y, x});
+    if(y >= n - 1){
+        ret++;
+    }else{
+        for(int i = 0; i < n; i++){
+            go(y + 1, i);
+        }
+    }
+    v.pop_back();
+}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    for(int i = 0; i < 8; i++){
-        if(i % 2 == 0){
-            for(int j = 0; j < 8; j++){
-                a[i][j] = _a[j];
-            }
-        }else {
-            for(int j = 0; j < 8; j++){
-                a[i][j] = _b[j];
-            }
-        }
-    }
-    for(int i = 0; i < 8; i++){
-        if(i % 2 == 0){
-            for(int j = 0; j < 8; j++){
-                b[i][j] = _b[j];
-            }
-        }else {
-            for(int j = 0; j < 8; j++){
-                b[i][j] = _a[j];
-            }
-        }
-    }
-
-    cin >> n >> m;
+    cin >> n;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++) cin >> ary[i][j];
-    }
-
-    for(int i = 0; i <= n - 8; i++){
-        for(int j = 0; j <= m - 8; j++){
-            int temp = 0;
-            for(int k = 0; k < 8; k++){
-                for(int l = 0; l < 8; l++){
-                    if(ary[i + k][j + l] != a[k][l]) temp++;
-                }
-            }
-            ret = min(ret, temp);
-            temp = 0;
-            for(int k = 0; k < 8; k++){
-                for(int l = 0; l < 8; l++){
-                    if(ary[i + k][j + l] != b[k][l]) temp++;
-                }
-            }
-            ret = min(ret, temp);
-        }
+        go(0, i);
     }
     cout << ret << '\n';
+
 	return 0;
 }
+
