daecd00dc201c87d67d1798082009154112c092f
jeongjulee42 <ljj3342@naver.com>
2024-09-13 20:00:09 +0900

    9663

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,39 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> v;
-int n, ret;
+int n, start = 1, tot;
+bool flag;
 
-bool check(int y, int x){
-    for(int i = 0; i < v.size(); i++){
-        int vy = v[i].first;
-        int vx = v[i].second;
-        if(y == vy || x == vx || (abs(y - vy) == abs(x - vx))) return false;
-    }
-    return true;
-}
-
-void go(int y, int x){
-    if(!check(y, x)) return;
-    v.push_back({y, x});
-    if(y >= n - 1){
-        ret++;
-    }else{
-        for(int i = 0; i < n; i++){
-            go(y + 1, i);
-        }
-    }
-    v.pop_back();
-}
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int i = 0; i < n; i++){
-        go(0, i);
+    while(tot + start < n){
+        tot += start;
+        start++;
+        flag = !flag;
     }
-    cout << ret << '\n';
+    cout << start ;
+
 
 	return 0;
 }
