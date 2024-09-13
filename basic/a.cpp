72dbc6f2a91e3f0fd06f1e4606167ed551046e2d
jeongjulee42 <ljj3342@naver.com>
2024-09-13 20:14:45 +0900

    1193

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, start = 1, tot;
+int n, start = 1, tot, a, b;
 bool flag;
 
 
@@ -14,8 +14,14 @@ int main(){
         start++;
         flag = !flag;
     }
-    cout << start ;
-
+    if(flag){
+        a = n - tot;
+        b = start + 1 - a;
+    }else {
+        a = start + 1 - (n - tot);
+        b = n - tot;
+    }
+    cout << a << "/" << b;
 
 	return 0;
 }
