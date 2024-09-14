92696d605a16f5a4a9495d8a5e02012fe466d7bc
jeongjulee42 <ljj3342@naver.com>
2024-09-14 19:38:17 +0900

    28279

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,27 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n, start = 1, tot, a, b;
-bool flag;
-
+deque<int> dq;
+int n, c, num;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    while(tot + start < n){
-        tot += start;
-        start++;
-        flag = !flag;
+    for(int i = 0; i < n; i++){
+        cin >> c;
+        if(c == 1){
+            cin >> num;
+            dq.push_front(num);
+        }else if (c == 2){
+            cin >> num;
+            dq.push_back(num);
+        }else if (c == 3){
+            if(dq.size()){
+                cout << dq.front() << '\n';
+                dq.pop_front();
+            }else{
+                cout << -1 << '\n';
+            }
+        }else if (c == 4){
+            if(dq.size()){
+                cout << dq.back() << '\n';
+                dq.pop_back();
+            }else{
+                cout << -1 << '\n';
+            }
+        }else if (c == 5){
+            cout << dq.size() << '\n';
+        }else if (c == 6){
+            if(dq.size()){
+                cout << 0 << '\n';
+            }else{
+                cout << 1 << '\n';
+            }
+        }else if (c == 7){
+            if(dq.size()){
+                cout << dq.front() << '\n';
+            }else{
+                cout << -1 << '\n';
+            }
+        }else{
+            if(dq.size()){
+                cout << dq.back() << '\n';
+            }else{
+                cout << -1 << '\n';
+            }
+        }
     }
-    if(flag){
-        a = n - tot;
-        b = start + 1 - a;
-    }else {
-        a = start + 1 - (n - tot);
-        b = n - tot;
-    }
-    cout << a << "/" << b;
 
 	return 0;
 }
