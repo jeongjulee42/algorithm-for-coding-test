02a378518ae6a1d83c78fea8ab3f2c92271647a4
jeongjulee42 <ljj3342@naver.com>
2024-09-15 20:13:43 +0900

    2346

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,53 +2,33 @@
 using namespace std;
 
 deque<int> dq;
-int n, c, num;
+int ary[1000];
+int n, temp;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> c;
-        if(c == 1){
-            cin >> num;
-            dq.push_front(num);
-        }else if (c == 2){
-            cin >> num;
-            dq.push_back(num);
-        }else if (c == 3){
-            if(dq.size()){
-                cout << dq.front() << '\n';
+    for(int i = 0; i < n; i++ ){
+        cin >> ary[i];
+        dq.push_back(i);
+    }
+    
+    while(dq.size()){
+        temp = dq.front();
+        cout << temp + 1 << ' ';
+        dq.pop_front();
+        if(ary[temp] >= 0){
+            for(int i = 1; i < ary[temp]; i++){
+                int num = dq.front();
                 dq.pop_front();
-            }else{
-                cout << -1 << '\n';
+                dq.push_back(num);
             }
-        }else if (c == 4){
-            if(dq.size()){
-                cout << dq.back() << '\n';
+        }else{ 
+            for(int i = 0; i > ary[temp]; i--){
+                int num = dq.back();
                 dq.pop_back();
-            }else{
-                cout << -1 << '\n';
-            }
-        }else if (c == 5){
-            cout << dq.size() << '\n';
-        }else if (c == 6){
-            if(dq.size()){
-                cout << 0 << '\n';
-            }else{
-                cout << 1 << '\n';
-            }
-        }else if (c == 7){
-            if(dq.size()){
-                cout << dq.front() << '\n';
-            }else{
-                cout << -1 << '\n';
-            }
-        }else{
-            if(dq.size()){
-                cout << dq.back() << '\n';
-            }else{
-                cout << -1 << '\n';
+                dq.push_front(num);
             }
         }
     }
