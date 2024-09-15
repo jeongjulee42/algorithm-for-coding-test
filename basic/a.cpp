40bbee28934169ba0fa8eeb0c984e89676e9ab7c
jeongjulee42 <ljj3342@naver.com>
2024-09-16 00:25:33 +0900

    24511

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,36 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-deque<int> dq;
-int ary[1000];
-int n, temp;
+int n, b, m, c;
+int ary[100004];
+int bry[100004];
+queue<int> q;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int i = 0; i < n; i++ ){
+    for(int i = 0; i < n; i++){
         cin >> ary[i];
-        dq.push_back(i);
     }
-    
-    while(dq.size()){
-        temp = dq.front();
-        cout << temp + 1 << ' ';
-        dq.pop_front();
-        if(ary[temp] >= 0){
-            for(int i = 1; i < ary[temp]; i++){
-                int num = dq.front();
-                dq.pop_front();
-                dq.push_back(num);
-            }
-        }else{ 
-            for(int i = 0; i > ary[temp]; i--){
-                int num = dq.back();
-                dq.pop_back();
-                dq.push_front(num);
-            }
-        }
+    for(int i = 0; i < n; i++){
+        cin >> bry[i];
+    }
+    for(int i = n-1; i >= 0; i--){
+        if(ary[i] == 0) q.push(bry[i]);
+    }
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        cin >> c;
+        q.push(c);
+
+        cout << q.front() << ' ';
+        q.pop();
     }
 
 	return 0;
