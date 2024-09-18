21c6b9ea807214fed038c571f9ba53cec3caf6de
jeongjulee42 <ljj3342@naver.com>
2024-09-18 17:13:46 +0900

    2108

--- basic/a.cpp
+++ basic/a.cpp
@@ -1,38 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, num, sq;
-bool flag = 0;
+
+int n, num, minNum = 987654321, maxNum = -987654321, sum, mid, cnt;
+vector<int> v;
+map<int, int> mp;
+
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int z = 0; z < n; z++){    
+    for(int i = 0; i < n; i++){
         cin >> num;
-        if(num == 0 || num == 1 || num == 2){
-            cout << 2 << '\n';
-            continue;
-        }
-        if(num % 2 == 0) num++;
-        while(1){
-            sq = sqrt(num);
-            int cnt = 0;
-            for(int i = 2; i <= sq; i ++){
-                if(num % i == 0) {
-                    cnt++;
-                    break;
-                }
-            }
-            if(cnt){
-                num += 2;
-            }
-            else{
-                cout << num << '\n';
-                break;
-            }
+        mp[num]++;
+        minNum = min(minNum, num);
+        maxNum = max(maxNum, num);
+        sum += num;
+    }
+    cout << round(sum / n + 0.0) << '\n';
+    
+    bool flag = 0;
+    for(auto m : mp){
+        mid = max(mid, m.second);
+        cnt += m.second;
+        if(cnt >= n / 2 && !flag){
+            flag = 1;
+            cout << m.first << '\n';
         }
     }
+
+
+    for(auto m : mp){
+        if(m.second == mid) v.push_back(m.first);
+    }
+    if(v.size() >= 2) cout << v[1] << '\n';
+    else cout << v[0] << '\n';
+
+    cout << maxNum - minNum << '\n';
 
 	return 0;
 }
