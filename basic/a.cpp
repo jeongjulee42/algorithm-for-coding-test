3c58290fc92c1aa94fc70257fe1ba5e34a8a7317
jeongjulee42 <ljj3342@naver.com>
2024-09-25 11:15:42 +0900

    11054

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 typedef long long ll;
 
-int n, p[10004];
-int ary[10004][3];
+int n, a[1001], cnt[1001], cnt2[1001], ret;
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n;
-    for(int i = 1; i <= n; i++) cin >> p[i];
-    for(int i = 1; i <= n; i++){
-        ary[i][0] = max({ary[i-1][0], ary[i-1][1], ary[i-1][2]});
-        ary[i][1] = ary[i-1][0] + p[i];
-        ary[i][2] = max(ary[i-1][0], ary[i-1][1]) + p[i];
+    for(int i = 0; i < n; i++)cin >> a[i];
+    for(int i = 0; i < n; i++){
+        int maxVal = 0;
+        for(int j = 0; j < i; j++){
+            if(a[j] < a[i] && maxVal < cnt[j]) maxVal = cnt[j];
+        }
+        cnt[i] = maxVal + 1;
     }
-    cout << max({ary[n][0], ary[n][1], ary[n][2]}) << '\n';
+    for(int i = n-1; i >= 0; i--){
+        int minVal = 0;
+        for(int j = n - 1; j > i; j--){
+            if(a[j] < a[i] && minVal < cnt2[j]) minVal = cnt2[j];
+        }
+        cnt2[i] = minVal + 1;
+        ret = max(ret, cnt[i] + cnt2[i] - 1);
+    }
+
+    cout << ret << '\n';
 
 	return 0;
 }
