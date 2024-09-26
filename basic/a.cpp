e52c31ea076c95151e286a865cf70a20f3cb106f
jeongjulee42 <ljj3342@naver.com>
2024-09-26 09:59:01 +0900

    9251

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 typedef long long ll;
 
-int n, a[1001], cnt[1001], cnt2[1001], ret;
+string a, b;
+int lcs[1004][1004];
 
 int main(){
 	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n;
-    for(int i = 0; i < n; i++)cin >> a[i];
-    for(int i = 0; i < n; i++){
-        int maxVal = 0;
-        for(int j = 0; j < i; j++){
-            if(a[j] < a[i] && maxVal < cnt[j]) maxVal = cnt[j];
+    cin >> a;
+    cin >> b;
+    for(int i = 1; i <= a.size(); i++){
+        for(int j = 1; j <= b.size(); j++){
+            if(a[i-1] == b[j-1]){
+                lcs[i][j] = lcs[i-1][j-1] + 1;
+            }else{
+                lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
+            }
         }
-        cnt[i] = maxVal + 1;
     }
-    for(int i = n-1; i >= 0; i--){
-        int minVal = 0;
-        for(int j = n - 1; j > i; j--){
-            if(a[j] < a[i] && minVal < cnt2[j]) minVal = cnt2[j];
-        }
-        cnt2[i] = minVal + 1;
-        ret = max(ret, cnt[i] + cnt2[i] - 1);
-    }
-
-    cout << ret << '\n';
+    cout << lcs[a.size()][b.size()] << '\n';
 
 	return 0;
 }
