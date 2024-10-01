4672f833d93e79681242255ebb4dc0e0b117ff2d
jeongjulee42 <ljj3342@naver.com>
2024-10-01 12:50:51 +0900

    13305

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,33 +2,28 @@
 using namespace std;
 typedef long long ll;
 
-ll ary[100004];
-ll len[100004];
-ll n, lenSum, ret = 1e18, temp;
-
-void go(int num, int money, int here){
-    if(num == n - 1) ret = min(ret, money);
-    for(int i = len[num]; i <= lensum; i++){
-        //go(num + 1, money + ((i - here) * ary[num]), i);
-        cout << num + 1 << ":" << money + ((i - here) * ary[num]) << ":" << i << '\n';
-    }
-}
+int n, ary[100004], len[100004];
+ll money;
 
 int main(){
-	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+   ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n;
-    for(int i = 0; i < n - 1; i++){
-        cin >> temp;
-        lenSum += temp;
-        if(i == 0) len[i] = temp;
-        else{
-            len[i] = len[i-1] + temp;
-        }
-    }
-    for(int i = 0; i < n; i++) cin >> ary[i];
+   cin >> n;
+   for(int i = 0; i < n-1; i++) cin >> len[i];
+   for(int i = 0; i < n; i++) cin >> ary[i];
+   ll lenSum = len[0];
+   ll start = ary[0];
+   for(int i = 1; i < n - 1; i++){
+      if(ary[i] < start){
+         money += lenSum * start;
+         lenSum = len[i];
+         start = ary[i];
+      } else {
+         lenSum += len[i];
+      }
+   }
+   money += lenSum * start;
+   cout << money << '\n';
 
-    go(0, 0, 0);
-
-	return 0;
+   return 0;
 }
