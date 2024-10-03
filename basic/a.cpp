e51475262bc7259d3629b5906216af1469421867
jeongjulee42 <ljj3342@naver.com>
2024-10-03 19:56:02 +0900

    16928

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,55 +2,43 @@
 using namespace std;
 typedef long long ll;
 
-struct Pos{int z; int y; int x;};
-int ary[104][104][104], visited[104][104][104], m, n, h, ret;
-queue<Pos> q;
-int dz[] = {1, -1, 0, 0, 0, 0};
-int dy[] = {0, 0, 1, -1, 0, 0};
-int dx[] = {0, 0, 0, 0, 1, -1};
+int ary[10][10], visited[10][10], n, m, x, y, u, v;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> m >> n >> h;
-   for(int i = 0; i < h; i++){
-      for(int j = 0; j < n; j++){
-         for(int k = 0; k < m; k++){
-            cin >> ary[i][j][k];
-            if(ary[i][j][k] == 1) {
-               q.push({i, j, k});
-               visited[i][j][k] = 1;   
-            }
-            else if(ary[i][j][k] == -1) visited[i][j][k] = -1;
-         }
-      }
+   cin >> n >> m;
+   for(int i = 0; i < n; i++){
+      cin >> x >> y;
+      ary[(x-1)/10][(x-1)%10] = y-1;
    }
+   for(int i = 0; i < m; i++){
+      cin >> u >> v;
+      ary[(u-1)/10][(u-1)%10] = v-1;
+   }
+   queue<int> q;
+   q.push(0);
    while(q.size()){
-      int tz = q.front().z;
-      int ty = q.front().y;
-      int tx = q.front().x;
+      int num = q.front();
       q.pop();
-      for(int i = 0; i < 6; i++){
-         int nz = tz + dz[i];
-         int ny = ty + dy[i];
-         int nx = tx + dx[i];
-         if(nz < 0 || ny < 0 || nx < 0 || nz >= h || ny >= n || nx >= m || visited[nz][ny][nx]) continue;
-         q.push({nz, ny, nx});
-         visited[nz][ny][nx] = visited[tz][ty][tx] + 1;
-      }
-   }
-   for(int i = 0; i < h; i++){
-      for(int j = 0; j < n;  j++){
-         for(int k = 0; k < m; k++){
-            if(visited[i][j][k] == 0){
-               cout << -1 << '\n';
-               exit(0);
-            }
-            ret = max(ret, visited[i][j][k]);
+      for(int i = 1; i <= 6; i++){
+         int dn = num + i;
+         if(dn >= 100) continue;
+         if(visited[dn/10][dn%10]) continue;
+         if(ary[dn/10][dn%10]){
+            int temp = ary[dn/10][dn%10];
+            if(visited[temp/10][temp%10]) continue;
+            visited[dn/10][dn%10] = visited[num/10][num%10] + 1;
+            visited[temp/10][temp%10] = visited[dn/10][dn%10];
+            q.push(temp);
+         }else{
+            visited[dn/10][dn%10] = visited[num/10][num%10] + 1;
+            q.push(dn);
          }
       }
    }
-   cout << ret - 1 << '\n';
+
+   cout << visited[9][9] << '\n';
 
    return 0;
 }
