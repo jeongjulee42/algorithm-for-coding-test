f96619d2cfe46721276326db77dd973b17d33f50
jeongjulee42 <ljj3342@naver.com>
2024-10-06 19:08:16 +0900

    1100

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,54 +2,19 @@
 using namespace std;
 typedef long long ll;
 
-int n, ret, a, b, c, maxVal;
-int visited[10004];
-vector<pair<int, int>> v[10004];
+char t;
+int ret;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> n;
-   for(int i = 0; i < n - 1; i++){
-      cin >> a >> b >> c;
-      v[a].push_back({b, c});
-      v[b].push_back({a, c});
-   }
-   queue<int> q;
-   q.push(1);
-   while(q.size()){
-      int num = q.front();
-      q.pop();
-      for(auto t : v[num]){
-         if(visited[t.first] || t.first == 1) continue;
-         visited[t.first] = visited[num] + t.second;
-         q.push(t.first);
+   for(int i = 0; i < 8; i++){
+      for(int j = 0; j < 8; j++){
+         cin >> t;
+         if((i + j) % 2 == 0 && t == 'F') ret++;
       }
    }
-   int start = 0;
-   for(int i = 1; i <= n; i++){
-      if(visited[i] > maxVal){
-         maxVal = visited[i];
-         start = i;
-      }
-      visited[i] = 0;
-   }
-   queue<int> q1;
-   q1.push(start);
-   while(q1.size()){
-      int num = q1.front();
-      q1.pop();
-      for(auto t : v[num]){
-         if(visited[t.first] || t.first == start) continue;
-         visited[t.first] = visited[num] + t.second;
-         q1.push(t.first);
-      }
-   }
-   int ret = 0;
-   for(int i = 1; i <= n; i++){
-      ret = max(ret, visited[i]);
-   }
-   cout<< ret << '\n';
+   cout << ret << '\n';
 
    return 0;
 }
