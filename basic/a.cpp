fbc22a216645ed85d6a02222ebf09007f69803d6
jeongjulee42 <ljj3342@naver.com>
2024-10-07 19:23:20 +0900

    1158

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,19 +2,30 @@
 using namespace std;
 typedef long long ll;
 
-char t;
-int ret;
+queue<int> q;
+int n, k;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   for(int i = 0; i < 8; i++){
-      for(int j = 0; j < 8; j++){
-         cin >> t;
-         if((i + j) % 2 == 0 && t == 'F') ret++;
-      }
-   }
-   cout << ret << '\n';
+  cin >> n >> k;
 
-   return 0;
+  queue<int> q;
+
+  cout << '<';
+  for (int i = 1; i <= n; i++)
+    q.push(i);
+
+  while (!q.empty()){
+    for (int i = 0; i < k - 1; i++){
+      q.push(q.front());
+      q.pop();
+    }
+    cout << q.front();
+    q.pop();
+
+    if (q.empty()) cout << '>';
+    else cout << ", ";
+  }
+  return 0;
 }
