042cf2ad4789b693486bbeab1b0a6f20581fef77
jeongjulee42 <ljj3342@naver.com>
2024-10-08 10:57:24 +0900

    1138

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,30 +2,39 @@
 using namespace std;
 typedef long long ll;
 
-queue<int> q;
-int n, k;
+int n;
+vector<int> v;
+int ary[11];
+
+bool check(vector<int> t){
+   for(int i = 0; i < n; i++){
+      if(ary[i] != t[i]) return false;
+   }
+   return true;
+}
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-  cin >> n >> k;
+   cin >> n;
+   for(int i = 0; i < n; i++) cin >> ary[i];
+   for(int i = 0; i < n; i++) v.push_back(i + 1);
+   do{
+      vector<int> temp;
+      for(int i = 1; i <= n; i++){
+         int num = 0;
+         for(int j = 0; j < n; j++){
+            if(v[j] > i) num++;
+            else if(v[j] == i){
+               temp.push_back(num);
+            }
+         }
+      }
+      if(check(temp)){
+         for(int i = 0; i < n; i++) cout << v[i] << ' ';
+         break;
+      }
+   }while(next_permutation(v.begin(), v.end()));
 
-  queue<int> q;
-
-  cout << '<';
-  for (int i = 1; i <= n; i++)
-    q.push(i);
-
-  while (!q.empty()){
-    for (int i = 0; i < k - 1; i++){
-      q.push(q.front());
-      q.pop();
-    }
-    cout << q.front();
-    q.pop();
-
-    if (q.empty()) cout << '>';
-    else cout << ", ";
-  }
-  return 0;
+   return 0;
 }
