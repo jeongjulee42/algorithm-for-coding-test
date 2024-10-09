f550accc00ac2939260ffe3bf4b2925a87f0c866
jeongjulee42 <ljj3342@naver.com>
2024-10-09 21:22:13 +0900

    1032

--- basic/a.cpp
+++ basic/a.cpp
@@ -3,38 +3,26 @@ using namespace std;
 typedef long long ll;
 
 int n;
-vector<int> v;
-int ary[11];
-
-bool check(vector<int> t){
-   for(int i = 0; i < n; i++){
-      if(ary[i] != t[i]) return false;
-   }
-   return true;
-}
+string str;
+map<string, int> mp;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
    cin >> n;
-   for(int i = 0; i < n; i++) cin >> ary[i];
-   for(int i = 0; i < n; i++) v.push_back(i + 1);
-   do{
-      vector<int> temp;
-      for(int i = 1; i <= n; i++){
-         int num = 0;
-         for(int j = 0; j < n; j++){
-            if(v[j] > i) num++;
-            else if(v[j] == i){
-               temp.push_back(num);
-            }
-         }
+   for(int i = 0; i < n; i++){
+      cin >> str;
+      mp[str]++;
+   }
+   int maxnum = 0;
+   string maxname = "";
+   for(auto m : mp){
+      if(maxnum < m.second){
+         maxnum = m.second;
+         maxname = m.first;
       }
-      if(check(temp)){
-         for(int i = 0; i < n; i++) cout << v[i] << ' ';
-         break;
-      }
-   }while(next_permutation(v.begin(), v.end()));
+   }
+   cout << maxname << '\n';
 
    return 0;
 }
