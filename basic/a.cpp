b1d0944f7030c11fc27b1b4de3c8b25217fc5961
jeongjulee42 <ljj3342@naver.com>
2024-10-11 19:52:26 +0900

    2003

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,38 +2,22 @@
 using namespace std;
 typedef long long ll;
 
-int ary[100][100], n, m, k, tx, ty, ret;
-int dy[] = {0, 1, 0, -1};
-int dx[] = {1, 0, -1, 0};
-bool visited[100][100];
-
-int go(int y, int x){
-   visited[y][x] = 1;
-   int t = 0;
-   for(int i = 0; i < 4; i++){
-      int ny = y + dy[i];
-      int nx = x + dx[i];
-      if(ny < 0 || nx < 0 || ny >= 100 || nx >= 100 || visited[ny][nx]) continue;
-      if(ary[ny][nx] == 0) continue;
-      t += go(ny, nx);
-   }
-   return t + 1;
-}
+int ary[10001];
+int sum[10001];
+int n, m, ret;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> n >> m >> k;
-   for(int i = 0; i < k; i++){
-      cin >> ty >> tx;
-      ary[ty-1][tx-1] = 1;
+   cin >> n >> m;
+   for(int i = 1; i <= n; i++){
+      cin >> ary[i];
+      sum[i] = ary[i] + sum[i-1];
    }
-   for(int i = 0; i < n; i++){
-      for(int j = 0; j < m; j++){
-         if(visited[i][j]) continue;
-         if(ary[i][j] == 0) continue;
-         int temp = go(i, j);
-         ret = max(ret, temp);
+   for(int i = 1; i <= n; i++){
+      for(int j = 0; j < i; j++){
+         if(sum[i] - sum[j] == m) ret++;
+         else if(sum[i] - sum[j] < m) continue;
       }
    }
    cout << ret << '\n';
