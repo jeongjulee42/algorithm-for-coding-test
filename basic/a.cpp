2c1e6291e730884efb7a0fd9aba2c9a736d5a04e
jeongjulee42 <ljj3342@naver.com>
2024-10-12 20:04:28 +0900

    2217

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,23 +2,17 @@
 using namespace std;
 typedef long long ll;
 
-int ary[10001];
-int sum[10001];
-int n, m, ret;
+int ary[100004];
+int ret, n;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> n >> m;
-   for(int i = 1; i <= n; i++){
-      cin >> ary[i];
-      sum[i] = ary[i] + sum[i-1];
-   }
-   for(int i = 1; i <= n; i++){
-      for(int j = 0; j < i; j++){
-         if(sum[i] - sum[j] == m) ret++;
-         else if(sum[i] - sum[j] < m) continue;
-      }
+   cin >> n;
+   for(int i = 0; i < n; i++) cin >> ary[i];
+   sort(ary, ary + n);
+   for(int i = 0; i < n; i++){
+      ret = max(ret, ary[i] * (n - i));
    }
    cout << ret << '\n';
 
