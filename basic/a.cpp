854766239389dd46482ef6b011c1c1c5aecb1246
jeongjulee42 <ljj3342@naver.com>
2024-10-14 18:26:50 +0900

    2441

--- basic/a.cpp
+++ basic/a.cpp
@@ -9,6 +9,7 @@ int main(){
 
    cin >> n;
    for(int i = n; i > 0; i--){
+      for(int j = 0; j < n - i; j++) cout << ' ';
       for(int j = i; j > 0; j--) cout << '*';
       cout << '\n';
    }
