1f4893f8cc4c38e3236f52efd606fa09573a370d
jeongjulee42 <ljj3342@naver.com>
2024-10-16 14:19:44 +0900

    2443

--- basic/a.cpp
+++ basic/a.cpp
@@ -8,7 +8,7 @@ int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
    cin >> n;
-   for(int i = 0; i < n; i++){
+   for(int i = n-1; i >= 0; i--){
       for(int j = 0; j < n - i - 1; j++) cout << ' ';
       for(int j = 0; j < i; j++) cout << '*';
       cout << '*';
