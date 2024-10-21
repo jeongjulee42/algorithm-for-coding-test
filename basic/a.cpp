11c8c77688767913295b9fa3e899ab7baf47465c
jeongjulee42 <ljj3342@naver.com>
2024-10-21 18:07:15 +0900

    2476

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,15 +2,24 @@
 using namespace std;
 typedef long long ll;
 
-int a, b, ret, now;
+int n, a, b, c, ret, temp;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   for(int i = 0; i < 4; i++){
-      cin >> a >> b;
-      now = now - a + b;
-      ret = max(ret, now);
+   cin >> n;
+   for(int i = 0; i < n; i++){
+      cin >> a >> b >> c;
+      if(a == b && b == c){
+         temp = 10000 + (a * 1000);
+      }else if(a == b || b == c){
+         temp = 1000 + (b * 100);
+      }else if(a == c){
+         temp = 1000 + (a * 100);
+      }else{
+         temp = max({a, b, c}) * 100;
+      }
+      ret = max(temp, ret);
    }
    cout << ret << '\n';
 
