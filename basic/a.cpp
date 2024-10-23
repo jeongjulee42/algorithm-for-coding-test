9c2ce282cbd73354a9539b857a031d068f922da1
jeongjulee42 <ljj3342@naver.com>
2024-10-23 18:13:40 +0900

    2522

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 typedef long long ll;
 
-int n, num;
+int n;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   for(int i = 0; i < 3; i++){
-      num = 0;
-      for(int j = 0; j < 4; j++) {
-         cin >> n;
-         if(n == 0) num++;
-      }
-      if(num == 0) cout << 'E' << '\n';
-      else if(num == 1) cout << 'A' << '\n';
-      else if(num == 2) cout << 'B' << '\n';
-      else if(num == 3) cout << 'C' << '\n';
-      else cout << 'D' << '\n';
+   cin >> n;
+   for(int i = -1 * (n - 1); i < n; i++){
+      int num = abs(i);
+      for(int j = 0; j < num; j++) cout << ' ';
+      for(int j = 0; j < n - num; j++) cout << '*';
+      cout << '\n';
    }
 
    return 0;
