51acc71e28162b19dd26b7a13eb5c0ec8b46f87c
jeongjulee42 <ljj3342@naver.com>
2024-10-29 20:20:33 +0900

    2742

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 typedef long long ll;
 
-int ary[10], t;
+int n;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> t;
-   for(int i = 0; i < t; i++){
-      for(int j = 0; j < 10; j++) cin >> ary[j];
-      sort(ary, ary + 10);
-      cout << ary[7] << '\n';
-   }
+   cin >> n;
+   for(int i = n; i > 0; i--) cout << i << '\n';
 
    return 0;
 }
