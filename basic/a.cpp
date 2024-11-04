418e6d81799bd9e9aac23873eddf3e9208161f33
jeongjulee42 <ljj3342@naver.com>
2024-11-04 18:55:11 +0900

    2774

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,23 +2,31 @@
 using namespace std;
 typedef long long ll;
 
-int n, sum, ret, num;
+int ary[10];
+int t, x;
+
+int check(int n){
+   for(int i = 0; i < 10; i++){
+      ary[i] = 0;
+   }
+   string temp = to_string(n);
+   for(int i = 0; i < temp.size(); i++){
+      ary[temp[i] - '0']++;
+   }
+   int ret = 0;
+   for(int i = 0; i < 10; i++){
+      if(ary[i]) ret++;
+   }
+   return ret;
+}
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> n;
-   for(int i = 0; i < n; i++){
-      ret = 987654321;
-      sum = 0;
-      for(int j = 0; j < 7; j++){
-         cin >> num;
-         if(num % 2 == 0){
-            sum += num;
-            ret = min(ret, num);
-         }
-      }
-      cout << sum << ' ' << ret << '\n';
+   cin >> t;
+   for(int i = 0; i < t; i++){
+      cin >> x;
+      cout << check(x) << '\n';
    }
 
    return 0;
