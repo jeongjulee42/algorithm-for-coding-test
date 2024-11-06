c39f28a76429fc092ebf695fa1b074fe05198281
jeongjulee42 <ljj3342@naver.com>
2024-11-06 18:40:36 +0900

    2947

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,22 +2,51 @@
 using namespace std;
 typedef long long ll;
 
-string str, ret;
+int ary[5];
 
+void swap(int a, int b){
+   int temp = ary[a];
+   ary[a] = ary[b];
+   ary[b] = temp;
+}
+void print(){
+   for(int i = 0; i < 5; i++) cout << ary[i] << ' ';
+   cout << '\n';
+}
+
+bool go(){
+   bool flag = true;
+   if(ary[0] > ary[1]){
+      swap(0, 1);
+      print();
+   }
+   if(ary[1] > ary[2]){
+      swap(1, 2);
+      print();
+   }
+   if(ary[2] > ary[3]){
+      swap(2, 3);
+      print();
+   }
+   if(ary[3] > ary[4]){
+      swap(3, 4);
+      print();
+   }
+   for(int i = 0; i < 5; i++){
+      if(ary[i] != i + 1) flag = false;
+   }
+   return flag;
+}
 
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
 
-   cin >> str;
-   for(int i = 0; i < str.size(); i++){
-      if(str[i] - 'a' >= 0){
-         cout << char(str[i] - 'a' + 'A');
-      }else{
-         cout << char(str[i] - 'A' + 'a');
-      }
+   for(int i = 0; i < 5; i++) cin >> ary[i];
+   while(1){
+      bool f = go();
+      if(f) break;
    }
-   cout << ret << '\n';
 
    return 0;
 }
