f9f9bf1c37253775f5e66ede3b80e5e3ecbfb105
jeongjulee42 <ljj3342@naver.com>
2024-11-09 19:40:21 +0900

    2965

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,26 +2,13 @@
 using namespace std;
 typedef long long ll;
 
-string str, ret;
-
-bool check(char c){
-   if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
-   return false;
-}
+int a, b, c;
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
    
-   getline(cin, str);
-   for(int i = 0; i < str.size(); i++){
-      if(check(str[i]) && i + 2 < str.size()){
-         if(str[i + 1] == 'p' && str[i + 2] == str[i]){
-            i += 2;
-         }
-      }
-      ret += str[i];
-   }
-   cout << ret << '\n';
+   cin >> a >> b >> c;
+   cout << max({c - b, b - a}) - 1 << '\n'; 
 
    return 0;
 }
