7ef0d9f6dab6bf7f8e9d252dff54f034627d4200
jeongjulee42 <ljj3342@naver.com>
2024-11-13 20:32:35 +0900

    2935

--- basic/a.cpp
+++ basic/a.cpp
@@ -2,16 +2,33 @@
 using namespace std;
 typedef long long ll;
 
-int n, w, h, temp;
+string a, b;
+char c;
+int ary[101];
 
 int main(){
    ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
    
-   cin >> n >> w >> h;
-   for(int i = 0; i < n; i++){
-      cin >> temp;
-      if(temp * temp <= w * w + h * h) cout << "DA" << '\n';
-      else cout << "NE" << '\n';
+   cin >> a >> c >> b;
+   if(c == '*'){
+      cout << 1;
+      for(int i = 0; i < a.size() + b.size() - 2; i++) cout << 0;
+      cout << '\n';
+   }else{
+      reverse(a.begin(), a.end());
+      reverse(b.begin(), b.end());
+      for(int i = 0; i < a.size(); i++){
+         ary[i] += a[i] - '0';
+      }
+      for(int i = 0; i < b.size(); i++){
+         ary[i] += b[i] - '0';
+      }
+      string ret = "";
+      for(int i = 0; i < max(a.size(), b.size()); i++){
+         ret += ary[i] + '0';
+      }
+      reverse(ret.begin(), ret.end());
+      cout << ret << '\n';
    }
 
    return 0;
